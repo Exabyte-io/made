03c31810152a5bef497890a37e6cf87815a53f23
Mohammad Mohammadi <mohammad@exabyte.io>
2019-02-05 19:02:00 -0800

    Readd 'should return same basis' test

--- tests/basis/basis.js
+++ tests/basis/basis.js
@@ -5,6 +5,70 @@ import {Basis} from "../../src/basis/basis";
 
 describe('Basis', function () {
 
+    it('should return same basis', function () {
+        const basis1 = new Basis({
+            elements: [
+                {
+                    id: 1,
+                    value: 'Fe'
+                },
+                {
+                    id: 2,
+                    value: 'Li'
+                },
+                {
+                    id: 3,
+                    value: 'Si'
+                }
+            ],
+            coordinates: [
+                {
+                    id: 1,
+                    value: [0.5, 0.5, 0.5]
+                },
+                {
+                    id: 2,
+                    value: [0.3, 0.3, 0.3]
+                },
+                {
+                    id: 3,
+                    value: [0, 0, 0]
+                }
+            ]
+        });
+        const basis2 = new Basis({
+            elements: [
+                {
+                    id: 2,
+                    value: 'Fe'
+                },
+                {
+                    id: 1,
+                    value: 'Li'
+                },
+                {
+                    id: 3,
+                    value: 'Si'
+                }
+            ],
+            coordinates: [
+                {
+                    id: 2,
+                    value: [0.5, 0.5, 0.5]
+                },
+                {
+                    id: 1,
+                    value: [0.3, 0.3, 0.3]
+                },
+                {
+                    id: 3,
+                    value: [0, 0, 0]
+                }
+            ]
+        });
+        expect(basis1.isEqualTo(basis2)).to.be.ok;
+    });
+
     it('should return proper unitCellFormula', function () {
         const basis = new Basis(Na4Cl4.basis);
         expect(basis.unitCellFormula).to.be.equal('Na4Cl4');
