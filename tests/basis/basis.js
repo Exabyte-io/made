5913b8816b7be5a93a7c15b2c22bdceea5533dac
Mohammad Mohammadi <mohammad@exabyte.io>
2019-02-07 14:55:09 -0800

    Move basis configs to fixtures

--- tests/basis/basis.js
+++ tests/basis/basis.js
@@ -1,71 +1,13 @@
 import {expect} from "chai";
 
-import {Na4Cl4} from "../enums";
 import {Basis} from "../../src/basis/basis";
+import {FeLiSiBasis, LiFeSiBasis, Na4Cl4} from "../enums";
 
 describe('Basis', function () {
 
     it('should return same basis', function () {
-        const basis1 = new Basis({
-            elements: [
-                {
-                    id: 1,
-                    value: 'Fe'
-                },
-                {
-                    id: 2,
-                    value: 'Li'
-                },
-                {
-                    id: 3,
-                    value: 'Si'
-                }
-            ],
-            coordinates: [
-                {
-                    id: 1,
-                    value: [0.5, 0.5, 0.5]
-                },
-                {
-                    id: 2,
-                    value: [0.3, 0.3, 0.3]
-                },
-                {
-                    id: 3,
-                    value: [0, 0, 0]
-                }
-            ]
-        });
-        const basis2 = new Basis({
-            elements: [
-                {
-                    id: 2,
-                    value: 'Fe'
-                },
-                {
-                    id: 1,
-                    value: 'Li'
-                },
-                {
-                    id: 3,
-                    value: 'Si'
-                }
-            ],
-            coordinates: [
-                {
-                    id: 2,
-                    value: [0.5, 0.5, 0.5]
-                },
-                {
-                    id: 1,
-                    value: [0.3, 0.3, 0.3]
-                },
-                {
-                    id: 3,
-                    value: [0, 0, 0]
-                }
-            ]
-        });
+        const basis1 = new Basis(FeLiSiBasis);
+        const basis2 = new Basis(LiFeSiBasis);
         expect(basis1.isEqualTo(basis2)).to.be.ok;
     });
 
--- tests/enums.js
+++ tests/enums.js
@@ -6,3 +6,5 @@ export const Si = readJSONFile(path.join(FIXTURES_DIR, "Si.json"));
 export const Na4Cl4 = readJSONFile(path.join(FIXTURES_DIR, "Na4Cl4.json"));
 export const Na4Cl4Poscar = readFile(path.join(FIXTURES_DIR, "Na4Cl4.poscar"));
 export const SiSupercell = readJSONFile(path.join(FIXTURES_DIR, "Si-supercell.json"));
+export const FeLiSiBasis = readJSONFile(path.join(FIXTURES_DIR, "FeLiSi-basis.json"));
+export const LiFeSiBasis = readJSONFile(path.join(FIXTURES_DIR, "LiFeSi-basis.json"));
