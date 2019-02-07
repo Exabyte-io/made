a7e3a15425e5dea654fe74473f7dc1eefad21d70
Mohammad Mohammadi <mohammad@exabyte.io>
2019-02-07 15:16:31 -0800

    Move basis configs to fixtures

--- tests/enums.js
+++ tests/enums.js
@@ -6,5 +6,10 @@ export const Si = readJSONFile(path.join(FIXTURES_DIR, "Si.json"));
 export const Na4Cl4 = readJSONFile(path.join(FIXTURES_DIR, "Na4Cl4.json"));
 export const Na4Cl4Poscar = readFile(path.join(FIXTURES_DIR, "Na4Cl4.poscar"));
 export const SiSupercell = readJSONFile(path.join(FIXTURES_DIR, "Si-supercell.json"));
+
+export const OSiBasis = readJSONFile(path.join(FIXTURES_DIR, "OSi-basis.json"));
+export const Si2Basis = readJSONFile(path.join(FIXTURES_DIR, "Si2-basis.json"));
+export const Ge2Basis = readJSONFile(path.join(FIXTURES_DIR, "Ge2-basis.json"));
+export const AsGeBasis = readJSONFile(path.join(FIXTURES_DIR, "AsGe-basis.json"));
 export const FeLiSiBasis = readJSONFile(path.join(FIXTURES_DIR, "FeLiSi-basis.json"));
 export const LiFeSiBasis = readJSONFile(path.join(FIXTURES_DIR, "LiFeSi-basis.json"));
