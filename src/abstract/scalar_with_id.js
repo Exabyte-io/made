3b24a08d3f953ea98c44a51c61e6ebdf53ef63bb
Timur Bazhirov <timur@exabyte.io>
2019-02-11 15:34:22 -0800

    Abstract classes added

--- src/abstract/scalar_with_id.js
+++ src/abstract/scalar_with_id.js
@@ -1,13 +1,23 @@
 import _ from "underscore";
 
+/**
+ * Helper class representing a ScalarWithId.
+ * @example {"id" : 0, "value" : "Si" }
+ */
 export class ScalarWithId {
-    constructor(value, id = 0) {
+
+    /**
+     * Create a an array with ids.
+     * @param {Any} valueOrObject - a ScalarWithID, or any other type.
+     * @param {Number} id - numerical id (Integer).
+     */
+    constructor(valueOrObject, id = 0) {
         // if already passing a ScalarWithId => preserve original
-        if (_.isObject(value) && !_.isArray(value)) { // Arrays are Objects too
-            id = value.id;
-            value = value.value;
+        if (_.isObject(valueOrObject) && !_.isArray(valueOrObject)) { // NOTE - Arrays are Objects too
+            id = valueOrObject.id;
+            valueOrObject = valueOrObject.value;
         }
-        this.value = value;
+        this.value = valueOrObject;
         this.id = id;
     }
 
