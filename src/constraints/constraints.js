08d3abef67d553cc0ac3bfe568dda868e4b217ab
Timur Bazhirov <timur@exabyte.io>
2019-02-11 19:46:49 -0800

    Moved generateTranslationCombinations to tools

--- src/constraints/constraints.js
+++ src/constraints/constraints.js
@@ -4,11 +4,29 @@ export class AtomicConstraints {
 
     static fromArray(array) {return new AtomicConstraints({values: array})}
 
+    /**
+     * Create atomic constraints.
+     * @param {Object} config
+     * @param {ArrayWithIds|Array} config.values
+     */
     constructor(config = {}) {
         this.name = 'atomic_constraints';
         this.values = new ArrayWithIds(config.values || []);
     }
 
+    /**
+     * @example As below:
+        [
+            {
+                "id" : 0,
+                "value" : [
+                    1,
+                    1,
+                    1
+                ]
+            }
+        ]
+     */
     toJSON() {
         return {
             name: this.name,
@@ -18,8 +36,12 @@ export class AtomicConstraints {
 
     getByIndex(idx) {return this.values.getArrayElementByIndex(idx) || []}
 
-    getAsStringByIndex(idx, func = (boolean) => boolean ? 1 : 0) {
-        return this.getByIndex(idx).map(func).join(' ');
-    }
+    /**
+     * Get constraints for an atom with index as string.
+     * @param {Number} idx - atom index.
+     * @param {Function} mapFn (OPTIONAL) - a function to be applied to each constraint. By default 0 or 1 is returned.
+     * @return {String[]}
+     */
+    getAsStringByIndex(idx, mapFn = (boolean) => boolean ? 1 : 0) {return this.getByIndex(idx).map(mapFn).join(' ')}
 
 }
