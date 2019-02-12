4c6843931975198e8f75cf347690867115ab7556
Timur Bazhirov <timur@exabyte.io>
2019-02-11 19:46:08 -0800

    Docstrings for Basis

--- src/basis/basis.js
+++ src/basis/basis.js
@@ -7,12 +7,25 @@ import {Lattice} from "../lattice/lattice";
 import {ArrayWithIds} from "../abstract/array_with_ids";
 import {ATOMIC_COORD_UNITS, HASH_TOLERANCE} from "../constants";
 
+/**
+ * A class representing a crystal basis.
+ */
 export class Basis {
+
+    /**
+     * Create a Basis class.
+     * @param {Object} - Config object.
+     * @param {Array|ArrayWithIds} elements - chemical elements for atoms in basis.
+     * @param {Array|ArrayWithIds} coordinates - coordinates for the atoms in basis.
+     * @param {String} units - units for the coordinates (eg. angstrom, crystal).
+     * @param {Cell} cell - crystal cell corresponding to the basis (eg. to convert to crystal coordinates).
+     * @param {Boolean} isEmpty - crystal cell corresponding to the basis (eg. to convert to crystal coordinates).
+     */
     constructor({
                     elements = ["Si"],
                     coordinates = [[0, 0, 0]],
                     units,
-                    cell = Basis.defaultCell,   // by default assume a cubic unary cell
+                    cell = Basis.defaultCell,   // by default, assume a cubic unary cell
                     isEmpty = false,            // whether to generate an empty Basis
                 }) {
         if (!units) {
@@ -41,51 +54,93 @@ export class Basis {
         return new Lattice().vectorArrays;
     }
 
-    toJSON() {
-        return JSON.parse(JSON.stringify(_.pick(this, ["elements", "coordinates", "units", "cell"])));
-    }
+    /**
+     * Serialize class instance to JSON.
+     * @example As below:
+     {
+            "elements" : [
+                {
+                    "id" : 0,
+                    "value" : "Si"
+                },
+                {
+                    "id" : 1,
+                    "value" : "Si"
+                }
+            ],
+            "coordinates" : [
+                {
+                    "id" : 0,
+                    "value" : [
+                        0,
+                        0,
+                        0
+                    ]
+                },
+                {
+                    "id" : 1,
+                    "value" : [
+                        0.25,
+                        0.25,
+                        0.25
+                    ]
+                }
+            ],
+            "units" : "crystal",
+            "cell" : [
+                [
+                    1,
+                    0,
+                    6.12323399573677e-17
+                ],
+                [
+                    1.60812264967664e-16,
+                    1,
+                    6.12323399573677e-17
+                ],
+                [
+                    0,
+                    0,
+                    1
+                ]
+            ]
+        }
+     */
+    toJSON() {return JSON.parse(JSON.stringify(_.pick(this, ["elements", "coordinates", "units", "cell"])))}
 
+    /**
+     * Create an identical copy of the class instance.
+     * @param {Object} extraContext - Extra context to be passed to the new class instance on creation.
+     */
     clone(extraContext) {return new this.constructor(Object.assign({}, this.toJSON(), extraContext))}
 
-    getElementByIndex(idx) {
-        return this._elements.getArrayElementByIndex(idx);
-    }
+    getElementByIndex(idx) {return this._elements.getArrayElementByIndex(idx)}
 
-    getCoordinateByIndex(idx) {
-        return this._coordinates.getArrayElementByIndex(idx);
-    }
+    getCoordinateByIndex(idx) {return this._coordinates.getArrayElementByIndex(idx)}
 
-    get elements() {
-        return this._elements.toJSON();
-    }
+    get elements() {return this._elements.toJSON()}
 
-    get elementsArray() {
-        return this._elements.array;
-    }
+    get elementsArray() {return this._elements.array}
 
-    set elements(elementsArray) {
-        this._elements = new ArrayWithIds(elementsArray);
-    }
+    /**
+     * Set basis elements to passed array.
+     * @param {Array|ArrayWithIds} elementsArray - New elements array.
+     */
+    set elements(elementsArray) {this._elements = new ArrayWithIds(elementsArray)}
 
-    get coordinates() {
-        return this._coordinates.toJSON();
-    }
+    get coordinates() {return this._coordinates.toJSON()}
 
-    set coordinates(coordinatesNestedArray) {
-        this._coordinates = new ArrayWithIds(coordinatesNestedArray);
-    }
+    /**
+     * Set basis elements to passed array.
+     * @param {Array|ArrayWithIds} coordinatesNestedArray - New coordinates array.
+     */
+    set coordinates(coordinatesNestedArray) {this._coordinates = new ArrayWithIds(coordinatesNestedArray)}
 
-    get coordinatesAsArray() {
-        return this._coordinates.array;
-    }
+    get coordinatesAsArray() {return this._coordinates.array}
 
-    get isInCrystalUnits() {
-        return this.units === ATOMIC_COORD_UNITS.crystal;
-    }
+    get isInCrystalUnits() {return this.units === ATOMIC_COORD_UNITS.crystal}
 
-    get isInCartesianUnits() {
-        return this.units === ATOMIC_COORD_UNITS.cartesian;
-    }
+    get isInCartesianUnits() {return this.units === ATOMIC_COORD_UNITS.cartesian}
 
     toCartesian() {
         const unitCell = this.cell;
@@ -101,12 +156,15 @@ export class Basis {
         this.units = ATOMIC_COORD_UNITS.crystal;
     }
 
-    // assert that all coordinates are within 0 and 1 in crystal units
+    /**
+     * Asserts that all coordinates are in standardRepresentation (as explained below).
+     */
     toStandardRepresentation() {
         this.toCrystal();
         this._coordinates.mapArrayInPlace(point => point.map(x => math.mod(x)));
     }
 
+    /** A representation where all coordinates are within 0 and 1 in crystal units */
     get standardRepresentation() {
         const originalUnits = this.units;
 
@@ -119,11 +177,24 @@ export class Basis {
         return result;
     }
 
+    /**
+     * Add atom with a chemical element at coordinate.
+     * @param {Object} config
+     * @param {String} element - Chemical element.
+     * @param {Array} coordinate - 3-dimensional coordinate.
+     */
     addAtom({element = "Si", coordinate = [0.5, 0.5, 0.5]}) {
         this._elements.addElement(element);
         this._coordinates.addElement(coordinate);
     }
 
+    /**
+     * Remove atom with a chemical element at coordinate either by passing the (element AND coordinate) OR id.
+     * @param {Object} config
+     * @param {String} element - Chemical element.
+     * @param {Array} coordinate - 3-dimensional coordinate.
+     * @param {Number} id - numeric id of the element (optional).
+     */
     removeAtom({element, coordinate, id}) {
         if (element && coordinate.length > 0) {
             this._elements.removeElement(element, id);
@@ -131,6 +202,12 @@ export class Basis {
         }
     }
 
+    /**
+     * Remove atom at a particular coordinate.
+     * @param {Object} config
+     * @param {Array} coordinate - 3-dimensional coordinate.
+     * @param {Number} id - numeric id of the element (optional).
+     */
     removeAtomAtCoordinate({coordinate, id}) {
         if (coordinate.length > 0) {
             const index = this._coordinates.getArrayIndexByPredicate(
@@ -142,14 +219,17 @@ export class Basis {
     }
 
     /**
-     * @summary Unique element names used in the material. E.g. `['Si', 'Li']`
+     * Unique names (symbols) of the chemical elements basis. E.g. `['Si', 'Li']`
+     * @return {Array}
      */
     get uniqueElements() {
         return _.unique(this._elements.array);
     }
 
     /**
-     * @summary Returns unit cell formula as object `{ "Fe": 4.0, "O": 8.0, "Li": 2.0}`
+     * Unit cell formula as object `{ "Fe": 4.0, "O": 8.0, "Li": 2.0}`.
+     * Chemical elements are sorted by electronegativity.
+     * @return {Object}
      */
     get elementCounts() {
         return _.chain(this.elements)
@@ -158,8 +238,8 @@ export class Basis {
     }
 
     /**
-     * @summary Returns a reduced formula in IUPAC format. E.g., Na2SO4
-     * TODO: sort elements by electronegativity
+     * Reduced formula in IUPAC format. E.g., Na2SO4
+     * @return {String}
      */
     get formula() {
         const counts = this.elementCounts;
@@ -171,7 +251,8 @@ export class Basis {
     }
 
     /**
-     * @summary Returns unit cell formula as object `{ "Fe": 4.0, "O": 8.0, "Li": 2.0}`
+     * Returns the unit cell formula as object `{ "Fe": 4.0, "O": 8.0, "Li": 2.0}`
+     * @return {Array}
      */
     get unitCellFormula() {
         const counts = this.elementCounts;
@@ -181,8 +262,9 @@ export class Basis {
     }
 
     /**
-     * @summary Returns a nested array with elements and their corresponding coordinates
+     * Returns a nested array with elements and their corresponding coordinates
      * @example Output: [ ["Si", [0,0,0]], ["Si", [0.5,0.5,0.5]] ]
+     * @return {Array}
      */
     get elementsAndCoordinatesArray() {
         const clsInstance = this;
@@ -198,9 +280,10 @@ export class Basis {
      * ```
      *     elements: [{id: 1, value: 'Si'}, {id: 2, value: 'Si'}]
      *     coordinates: [{id: 1, value: [1,0,0]}, {id: 2, value: [0, 1, 0]}]
+     *
      *     result: "Si 0,1,0;Si 1,0,0"
      * ```
-     * This guarantees independence of hash from elements array ordering.
+     * This guarantees the independence from the order in the elements array.
      * @return {String}
      */
     getAsSortedString(inStandardRepresentation = true) {
@@ -215,7 +298,10 @@ export class Basis {
         }).sort().join(';') + ";";
     }
 
-    // returns a string for hash calculation (in crystal units)
+    /**
+     * Returns a string for hash calculation (in crystal units)
+     * @return {String}
+     */
     get hashString() {
         const originallyInCartesianUnits = this.isInCartesianUnits;
 
@@ -229,7 +315,7 @@ export class Basis {
     }
 
     /**
-     * @summary Returns atomic positions array.
+     * Returns an array of strings with chemical elements and their atomic positions.
      * E.g., ``` ['Si 0 0 0', 'Li 0.5 0.5 0.5']```
      * @return {String[]}
      */
@@ -245,9 +331,7 @@ export class Basis {
     /**
      * @summary Returns number of atoms in material
      */
-    get nAtoms() {
-        return this._elements.array.length;
-    }
+    get nAtoms() {return this._elements.array.length}
 
     // helpers
 
