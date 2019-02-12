b0f0e9cf9c4574ecb9d7db4b01f8ce7c867764df
Timur Bazhirov <timur@exabyte.io>
2019-02-11 19:47:15 -0800

    Docstrings for lattice

--- src/lattice/lattice_bravais.js
+++ src/lattice/lattice_bravais.js
@@ -7,6 +7,20 @@ import {LATTICE_TYPE_CONFIGS} from "./types";
  * When stored as class variables units for lengths are always "angstrom"s, angle - "degree"s
  */
 export class LatticeBravais {
+
+    /**
+     * Create a Bravais lattice.
+     * @param {Object} config - Config object.
+     * @param {Number} config.a - lattice constant a.
+     * @param {Number} config.b - lattice constant b.
+     * @param {Number} config.c - lattice constant c.
+     * @param {Number} config.alpha - lattice angle alpha.
+     * @param {Number} config.beta - lattice angle beta.
+     * @param {Number} config.gamma - lattice angle gamma.
+     * @param {Object} config.units - units container.
+     * @param {String} config.units.length - units for length (eg. "angstrom", "crystal").
+     * @param {String} config.units.angle - units for angles (eg. "degree", "radian").
+     */
     constructor(config) {
         const {
             a = 1, // default lattice is cubic with unity in edge sizes
@@ -36,10 +50,19 @@ export class LatticeBravais {
 
     }
 
-    static _roundValue (x) {
-        return math.precise(math.roundToZero(x));
-    }
+    static _roundValue(x) {return math.precise(math.roundToZero(x))}
 
+    /**
+     * Create a Bravais lattice from vectors.
+     * @param {Object} - Config object.
+     * @param {Array} a - vector of the lattice.
+     * @param {Array} b - vector of the lattice.
+     * @param {Array} c - vector of the lattice.
+     * @param {Number} alat - scaling factor for the vector coordinates, defaults to 1.
+     * @param {String} units - units for the coordinates (eg. angstrom, crystal).
+     * @param {String} type - type of the lattice to be created, defaults to TRI when not provided.
+     * @param {Boolean} skipRounding - whether to skip rounding the resulting lattice values, defaults to `false`.
+     */
     static fromVectors({a, b, c, alat = 1, units = 'angstrom', type = 'TRI', skipRounding = false}) {
         const roundValue = skipRounding ? x => x : this._roundValue;
         return new this.prototype.constructor({
@@ -58,6 +81,9 @@ export class LatticeBravais {
         });
     }
 
+    /**
+     * See fromVectors above.
+     */
     static fromVectorArrays(array, type, skipRounding = true) {
         return this.fromVectors({
             a: array[0],
@@ -68,6 +94,11 @@ export class LatticeBravais {
         });
     }
 
+    /**
+     * Get the list of editable keys (eg. 'a', 'alpha') for the current lattice.
+     * @return {Object}
+     * @example {a: true, b: false, c: false, alpha: true, beta: false, gamma: false}
+     */
     get editables() {
         const object = {};
         const editablesList = LATTICE_TYPE_CONFIGS.find(entry => entry.code === this.type).editables;
@@ -80,6 +111,23 @@ export class LatticeBravais {
         return object;
     }
 
+    /**
+     * Serialize class instance to JSON.
+     * @example As below:
+         {
+            "a" : 3.867,
+            "b" : 3.867,
+            "c" : 3.867,
+            "alpha" : 60,
+            "beta" : 60,
+            "gamma" : 60,
+            "units" : {
+                "length" : "angstrom",
+                "angle" : "degree"
+            },
+            "type" : "FCC"
+         }
+     */
     toJSON() {
         return Object.assign({}, this, {
             units: {
