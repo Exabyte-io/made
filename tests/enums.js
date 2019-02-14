d9532ea882a50a9179d523373e8cc5860f6d0f73
Mohammad Mohammadi <mohammad@exabyte.io>
2019-02-14 12:17:00 -0800

    Add Basis tests

--- tests/enums.js
+++ tests/enums.js
@@ -1,9 +1,12 @@
 import path from "path";
 import {readFile, readJSONFile} from "./utils";
 
+export const TOLERANCE = 1e-3;
+
 export const FIXTURES_DIR = path.resolve(__dirname, "./fixtures");
 export const Si = readJSONFile(path.join(FIXTURES_DIR, "Si.json"));
 export const Na4Cl4 = readJSONFile(path.join(FIXTURES_DIR, "Na4Cl4.json"));
+export const Na4Cl4Cartesian = readJSONFile(path.join(FIXTURES_DIR, "Na4Cl4-cartesian.json"));
 export const Na4Cl4Poscar = readFile(path.join(FIXTURES_DIR, "Na4Cl4.poscar"));
 export const SiSupercell = readJSONFile(path.join(FIXTURES_DIR, "Si-supercell.json"));
 
