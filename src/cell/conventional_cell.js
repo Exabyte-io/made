945350630e90a346d481491662637e935886395a
Mohammad Mohammadi <mohammad@exabyte.io>
2019-03-26 19:24:51 -0700

    Add primitive to conventional cell convertor

---
+++ src/cell/conventional_cell.js
@@ -0,0 +1,111 @@
+import math from "../math";
+import {LATTICE_TYPE} from "../lattice/types";
+
+/**
+ * Routines for calculating conventional cell vectors from primitive cell Bravais parameters.
+ * Following Setyawan, W., & Curtarolo, S. (2010). doi:10.1016/j.commatsci.2010.05.010
+ */
+
+const CONVENTIONAL_TO_PRIMITIVE_CELL_MULTIPLIERS = {
+
+    [LATTICE_TYPE.CUB]: [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    ],
+
+    [LATTICE_TYPE.FCC]: [
+        [-1, 1, 1],
+        [1, -1, 1],
+        [1, 1, -1]
+    ],
+
+    [LATTICE_TYPE.BCC]: [
+        [0, 1, 1],
+        [1, 0, 1],
+        [1, 1, 0]
+    ],
+
+    [LATTICE_TYPE.TET]: [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    ],
+
+    [LATTICE_TYPE.BCT]: [
+        [0, 1, 1],
+        [1, 0, 1],
+        [1, 1, 0]
+    ],
+
+    [LATTICE_TYPE.ORC]: [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    ],
+
+    [LATTICE_TYPE.ORCF]: [
+        [-1, 1, 1],
+        [1, -1, 1],
+        [1, 1, -1]
+    ],
+
+    [LATTICE_TYPE.ORCI]: [
+        [0, 1, 1],
+        [1, 0, 1],
+        [1, 1, 0]
+    ],
+
+    [LATTICE_TYPE.ORCC]: [
+        [1, -1, 0],
+        [1, 1, 0],
+        [0, 0, 1]
+    ],
+
+    [LATTICE_TYPE.HEX]: [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    ],
+
+    [LATTICE_TYPE.RHL]: [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    ],
+
+    [LATTICE_TYPE.MCL]: [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    ],
+
+    [LATTICE_TYPE.MCLC]: [
+        [1, -1, 0],
+        [1, 1, 0],
+        [0, 0, 1]
+    ],
+
+    [LATTICE_TYPE.TRI]: [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    ],
+
+    [LATTICE_TYPE.TRI + 'alt']: [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+    ],
+
+};
+
+/**
+ * Returns lattice vectors for a conventional cell for a lattice.
+ * @param {Lattice} lattice - Lattice instance.
+ * @return {Array[]} Cell.vectorsAsArray
+ */
+export function conventionalCell(lattice) {
+    const multiplier = CONVENTIONAL_TO_PRIMITIVE_CELL_MULTIPLIERS[lattice.type || LATTICE_TYPE.TRI];
+    return math.multiply(math.matrix(multiplier), math.matrix(lattice.vectorArrays)).toArray();
+}
