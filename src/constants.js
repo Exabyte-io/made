1b546b9a791cc38078959db451747b54fa4b43e9
Mohammad Mohammadi <mohammad@exabyte.io>
2019-04-14 19:55:41 -0700

    Add coefficients

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,8 @@
 export const coefficients = {
+    EV_TO_RY: 0.0734986176,
     BOHR_TO_ANGSTROM: 0.52917721092,
-    ANGSTROM_TO_BOHR: 1 / 0.52917721092
+    ANGSTROM_TO_BOHR: 1 / 0.52917721092,
+    EV_A_TO_RY_BOHR: 1 / 25.71104309541616,
 };
 
 export const tolerance = {
