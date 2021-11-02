f8b904537b659bc81509d0e7a1667affcbc8fd69
adewyer <adewyer@umich.edu>
2021-11-01 17:21:10 -0700

    added poscar function to count file lines, added test for counting file lines

--- tests/parsers/poscar.js
+++ tests/parsers/poscar.js
@@ -1,7 +1,8 @@
 import {expect} from "chai";
 
 import {Material} from "../../src/material";
-import {Na4Cl4, Na4Cl4Poscar, Zr1H23Zr1H1, Zr1H23Zr1H1Poscar} from "../enums";
+import {lineCount} from "../../src/parsers/poscar";
+import {Na4Cl4, Na4Cl4Poscar, Zr1H23Zr1H1, Zr1H23Zr1H1Poscar, H2O} from "../enums";
 
 describe('Parsers.POSCAR', function () {
     it('should return a valid poscar', function () {
@@ -12,6 +13,10 @@ describe('Parsers.POSCAR', function () {
     it('should return poscar elements line according to id in basis, duplicate entries separate', function () {
         const material = new Material(Zr1H23Zr1H1);
         expect(`${material.getAsPOSCAR()}\n`).to.be.equal(Zr1H23Zr1H1Poscar);
+    });
+
+    it('should return the number of lines in the file', function() {
+        expect(lineCount(H2O)).to.be.equal(11);
     });
 
 });
