f9152edb1b980842ee06f2a18b68d94a0f9ef3be
adewyer <adewyer@umich.edu>
2021-12-06 12:01:04 -0800

    added function to update lattice to material.tools file

--- src/tools/material.js
+++ src/tools/material.js
@@ -1,3 +1,5 @@
+import {Basis} from "../basis/basis";
+import {Lattice} from "../lattice/lattice";
 /**
  * Scales one lattice vector for the given material
  * @param material {Material} The material acted upon.
@@ -18,6 +20,26 @@ function scaleOneLatticeVector(material, key = 'a', factor = 1.0) {
 
 }
 
+function scaleNonPeriodicLattice(material) {
+    const lattice = new Lattice(material.lattice);
+    const basis = new Basis({
+        ...material.basis,
+        cell: lattice.vectorArrays,
+        units: "crystal"
+    })
+    basis.toCartesian();
+    basis.toJSON();
+    //TODO: Change to match the new scaling factor variable from SOF-5214
+    const scalingFactor = 2.0;
+    const newLattice = new Lattice({
+        a: basis.maxPairwiseDistance * scalingFactor,
+        type: "CUB"
+    });
+    newLattice.toJSON();
+    return newLattice;
+}
+
 export default {
     scaleOneLatticeVector,
+    scaleNonPeriodicLattice,
 }
