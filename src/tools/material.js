28909c78c27785ab6d2b92b6e8c39a48ef693e75
adewyer <adewyer@umich.edu>
2021-12-07 15:33:25 -0800

    simplified function to scale the lattice for non-periodic structures

--- src/tools/material.js
+++ src/tools/material.js
@@ -25,24 +25,20 @@ function scaleOneLatticeVector(material, key = 'a', factor = 1.0) {
  * Returns the lattice for a non-periodic structure. The lattice has been scaled to accomodate the size of the
  * non-periodic structure.
  * @param material {Material}
- * @returns lattice
+ * @returns {Object}
  */
 function scaleLatticeToMakeNonPeriodic(material) {
-    const lattice = new Lattice(material.lattice);
     const basis = new Basis({
-        ...material.basis,
-        cell: lattice.vectorArrays,
-        units: "crystal"
+        ...material.Basis.toJSON(),
     })
     basis.toCartesian();
-    basis.toJSON();
     const scalingFactor = nonPeriodicLatticeScalingFactor;
     const newLattice = new Lattice({
         a: basis.maxPairwiseDistance * scalingFactor,
         type: "CUB"
     });
-    newLattice.toJSON();
-    return newLattice;
+
+    return newLattice.toJSON();
 }
 
 export default {
