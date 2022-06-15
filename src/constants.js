55ae17b367ffc55b4df7650deb22d6528334ee64
Timur Bazhirov <timur@exabyte.io>
2022-06-15 13:15:38 -0700

    fix: re-export default constants from code.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
 // TODO: adjust the imports and remove the need for re-exporting
 export * from "@exabyte-io/code.js/dist/constants";
+// eslint-disable-next-line no-restricted-exports
+export { default } from "@exabyte-io/code.js/dist/constants";
