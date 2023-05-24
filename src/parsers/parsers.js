6ce51690e0accccd788b7564f17ba104a9b4a0e6
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-05-24 19:40:33 -0400

    feat: handle native formats (only json now)

--- src/parsers/parsers.js
+++ src/parsers/parsers.js
@@ -2,10 +2,12 @@ import cif from "./cif";
 import espresso from "./espresso";
 import poscar from "./poscar";
 import xyz from "./xyz";
+import convertFromNative from "./native_formats";
 
 export default {
     xyz,
     poscar,
     cif,
     espresso,
+    convertFromNative,
 };
