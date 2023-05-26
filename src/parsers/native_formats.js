592a824b6119a0adbdb1ed59c338acc16b778fc2
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-05-26 16:06:26 -0400

    feat: add native format handler

--- src/parsers/native_formats.js
+++ src/parsers/native_formats.js
@@ -1,17 +1,46 @@
+import Poscar from "./poscar";
+
+// TODO: move this enum to an appropriate file
+export const NATIVE_FORMAT = {
+    JSON: "json",
+    POSCAR: "poscar",
+    CIF: "cif",
+    PWX: "pwx",
+    XYZ: "xyz",
+};
+
+/**
+ * @summary Detects the format of the input string
+ * @throws {Error} - If the input string is unknown format
+ * @param {string} text -  input string to detect format
+ * @returns {NATIVE_FORMAT} - Format of the input string
+ */
+function detectFormat(text) {
+    const jsonRegex = /^\s*\{/;
+    if (jsonRegex.test(text)) return NATIVE_FORMAT.JSON;
+    if (Poscar.isPoscar(text)) return NATIVE_FORMAT.POSCAR;
+
+    throw new Error("Unknown format");
+}
+
 /**
  * @summary Function to handle conversion from native formats
- * @param {String} text - Text to autodetect format and convert
- * @throws {Error} - If the input is not a valid JSON string
+ * @param {String} text - input string to detect format and convert
+ * @throws {Error} - If the input string is of unknown format
  * @return {Object} - Material config
  */
 function convertFromNative(text) {
-    let materialConfig = {};
-    try {
-        materialConfig = JSON.parse(text);
-    } catch (error) {
-        throw new Error("Invalid JSON: " + error.message);
+    const format = detectFormat(text);
+
+    switch (format) {
+        case NATIVE_FORMAT.JSON:
+            return JSON.parse(text);
+        case NATIVE_FORMAT.POSCAR:
+            return Poscar.fromPoscar(text);
+        // TODO:  add more formats
+        default:
+            throw new Error(`Unsupported format: ${format}`);
     }
-    return materialConfig;
 }
 
 export default convertFromNative;
