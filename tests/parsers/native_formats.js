e9403114afd12e852fe5dd8a6def4f15ba75262f
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-06-02 19:01:13 -0400

    fix: change the name of the import

--- tests/parsers/native_formats.js
+++ tests/parsers/native_formats.js
@@ -1,30 +1,30 @@
 import { expect } from "chai";
 
-import nativeFormats from "../../src/parsers/native_format_parsers";
+import nativeFormatParsers from "../../src/parsers/native_format_parsers";
 import { Graphene, GraphenePoscar, NiHex, NiHexPoscar } from "../enums";
 import { assertDeepAlmostEqual } from "../utils";
 
 describe("Parsers.NativeFormat", () => {
     it("should return a material config for graphene from a json", () => {
         const json = JSON.stringify(Graphene);
-        expect(nativeFormats.convertFromNative(json)).to.deep.equal(Graphene);
+        expect(nativeFormatParsers.convertFromNativeFormat(json)).to.deep.equal(Graphene);
     });
 
     it("should return a material config for graphene from a poscar", () => {
         const poscar = GraphenePoscar;
-        const config = nativeFormats.convertFromNative(poscar);
+        const config = nativeFormatParsers.convertFromNativeFormat(poscar);
         expect(config).to.be.deep.almost.equal(Graphene);
     });
 
     it("should return a material config for Ni hex from a poscar", () => {
         const poscar = NiHexPoscar;
-        const config = nativeFormats.convertFromNative(poscar);
+        const config = nativeFormatParsers.convertFromNativeFormat(poscar);
         assertDeepAlmostEqual(config, NiHex, ["lattice"]);
         assertDeepAlmostEqual(config.lattice, NiHex.lattice, ["type"]); // to omit "lattice.type" property
     });
 
     it("should throw an error for unknown format", () => {
         const text = "A\n snippet from an unknown format";
-        expect(() => nativeFormats.convertFromNative(text)).to.throw("Unknown format");
+        expect(() => nativeFormatParsers.convertFromNativeFormat(text)).to.throw("Unknown format");
     });
 });
