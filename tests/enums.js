50d3222757d0ec04b2b7728bb984c49abafc72e6
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-06-07 16:11:02 -0400

    feat: add test for ORC symmetry

--- tests/enums.js
+++ tests/enums.js
@@ -39,3 +39,5 @@ export const NiHex = readJSONFile(path.join(FIXTURES_DIR, "Ni-hex.json"));
 export const NiHexPoscar = readFile(path.join(FIXTURES_DIR, "Ni-hex.poscar"));
 export const NiCub = readJSONFile(path.join(FIXTURES_DIR, "Ni-cub.json"));
 export const NiCubPWSCFInput = readFile(path.join(FIXTURES_DIR, "Ni-cub-pwscf.in"));
+export const Sb2S3Orc = readJSONFile(path.join(FIXTURES_DIR, "Sb2S3-ORC.json"));
+export const Sb2S3OrcPWSCFInput = readFile(path.join(FIXTURES_DIR, "Sb2S3-ORC-pwscf.in"));
