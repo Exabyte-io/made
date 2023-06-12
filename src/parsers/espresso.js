a6d1dff976c4302b4063a84ee60bf76709eafdf9
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-06-12 18:53:13 -0400

    update: improve separation of concerns

--- src/parsers/espresso.js
+++ src/parsers/espresso.js
@@ -2,12 +2,10 @@ import _ from "underscore";
 import s from "underscore.string";
 
 import { ConstrainedBasis } from "../basis/constrained_basis";
-// eslint-disable-next-line no-unused-vars
-import { ATOMIC_COORD_UNITS, coefficients } from "../constants";
 import { Lattice } from "../lattice/lattice";
 import { LATTICE_TYPE } from "../lattice/types";
-import math from "../math";
 import { parseFortranFile } from "./fortran_parser";
+import { regex } from "./utils";
 import xyz from "./xyz";
 
 /**
@@ -37,71 +35,43 @@ function toEspressoFormat(materialOrConfig) {
  * @return {boolean}
  */
 function isEspressoFormat(fileContent) {
-    const regex = /&CONTROL|&SYSTEM|ATOMIC_POSITIONS/i;
-    return regex.test(fileContent);
+    const espressoRegex = /&CONTROL|&SYSTEM|ATOMIC_POSITIONS/i;
+    return espressoRegex.test(fileContent);
 }
-
-// /**
-//  * @summary Function to convert Fortran namelist to JavaScript object.
-//  * @param {String} data - Fortran string.
-//  * @return {Object}
-//  */
-// function parseFortranNameList(data) {
-//     const blocks = data.split(/\/\n+/);
-//     const result = {};
-//
-//     blocks.forEach((block) => {
-//         const lines = block.split("\n");
-//         const blockName = lines[0].trim().toLowerCase();
-//
-//         if (blockName.startsWith("&")) {
-//             const blockContent = {};
-//             for (let i = 1; i < lines.length; i++) {
-//                 const line = lines[i].trim();
-//                 if (line) {
-//                     // Check if the line contains celldm(i)
-//                     const celldmMatch = line.match(/celldm\((\d+)\)\s*=\s*(\d.\d+).*(#.*)?/);
-//                     if (celldmMatch) {
-//                         const index = Number(celldmMatch[1]);
-//                         const value = Number(celldmMatch[2]);
-//                         if (!blockContent.celldm) {
-//                             blockContent.celldm = {}; // Initialize if not present
-//                         }
-//                         blockContent.celldm[index] = value; // Store value at index
-//                     } else {
-//                         const [key, value] = line.split("=").map((str) => str.trim());
-//                         // convert Fortran boolean to JavaScript boolean
-//                         if (value === ".true.") {
-//                             blockContent[key] = true;
-//                         } else if (value === ".false.") {
-//                             blockContent[key] = false;
-//                         } else if (Number.isNaN(Number(value))) {
-//                             // if value is not a number, keep it as a string
-//                             blockContent[key] = value.replace(/'/g, ""); // remove single quotes
-//                         } else {
-//                             blockContent[key] = Number(value); // convert to number
-//                         }
-//                     }
-//                 }
-//             }
-//             result[blockName.slice(1)] = blockContent;
-//         } else {
-//             result.cards = block;
-//         }
-//     });
-//
-//     return result;
-// }
 
 /**
  * @summary Read unit cell parameters from CELL_PARAMETERS card
- * @param {Object} cell
- * @param {Number} alat
- * @returns {cell: Number[][], alat: Number, units: String}
+ * @param {String} text - cards data
+ * @param {Number} alat - lattice parameter
+ * @returns {{vectors: number[][], alat: Number, units: {length: string}}}
  */
-function getCellParameters(cell, alat = null) {
-    let { units } = cell;
-    let cellUnits = 1.0;
+function getCellParameters(text, alat = null) {
+    let vectors = [];
+    let units;
+
+    const cellParameters = text.match(regex.cellParameters);
+
+    if (cellParameters) {
+        vectors = [
+            [
+                parseFloat(cellParameters[2]),
+                parseFloat(cellParameters[3]),
+                parseFloat(cellParameters[4]),
+            ],
+            [
+                parseFloat(cellParameters[5]),
+                parseFloat(cellParameters[6]),
+                parseFloat(cellParameters[7]),
+            ],
+            [
+                parseFloat(cellParameters[8]),
+                parseFloat(cellParameters[9]),
+                parseFloat(cellParameters[10]),
+            ],
+        ];
+        // eslint-disable-next-line prefer-destructuring
+        units = cellParameters[1];
+    }
 
     if (units === "bohr") {
         if (alat !== null) {
@@ -109,258 +79,161 @@ function getCellParameters(cell, alat = null) {
                 "Lattice parameters given in &SYSTEM celldm/A and CELL_PARAMETERS bohr",
             );
         }
-        cellUnits = coefficients.BOHR_TO_ANGSTROM;
     } else if (units === "angstrom") {
         if (alat !== null) {
             throw new Error(
                 "Lattice parameters given in &SYSTEM celldm/A and CELL_PARAMETERS angstrom",
             );
         }
-        cellUnits = 1.0;
     } else if (units === "alat") {
         if (alat === null) {
             throw new Error("Lattice parameters must be set in &SYSTEM for alat units");
-        } else cellUnits = alat * coefficients.BOHR_TO_ANGSTROM;
-    } else if (alat === null) {
-        cellUnits = coefficients.BOHR_TO_ANGSTROM;
-    } else {
-        cellUnits = alat;
-    }
-
-    Object.keys(cell.cell).forEach((key) => {
-        if (Array.isArray(cell.cell[key])) {
-            cell.cell[key] = cell.cell[key].map((value) => value * cellUnits);
         }
-    });
+    }
 
     // eslint-disable-next-line no-param-reassign
     alat = 1; // Supplied data already taken in consideration so here it set to unity
-    // eslint-disable-next-line no-param-reassign
-    units = ATOMIC_COORD_UNITS.angstrom; // Supplied data already taken in consideration
-    cell.alat = alat;
-    return cell;
+    return { vectors, alat, units };
 }
 
-// TODO: move lattice logic to Lattice
+/**
+ * @summary Read atomic positions from ATOMIC_POSITIONS card
+ * @param {String} text - cards data
+ * @returns {{elements: Object[], coordinates: Object[], constraints: Object[], units: String}}
+ */
+function getAtomicPositions(text) {
+    const atomicSpeciesMatches = Array.from(text.matchAll(regex.atomicSpecies));
+    // eslint-disable-next-line no-unused-vars
+    const atomicSpecies = atomicSpeciesMatches.map((match) => ({
+        element: match[1],
+        mass: parseFloat(match[2]),
+        potential: match[3],
+    }));
+    const atomicPositionsMatches = Array.from(text.matchAll(regex.atomicPositions));
+    const elements = atomicPositionsMatches.map((match, index) => ({
+        id: index,
+        value: match[1],
+    }));
+    const coordinates = atomicPositionsMatches.map((match, index) => ({
+        id: index,
+        value: [parseFloat(match[2]), parseFloat(match[3]), parseFloat(match[4])],
+    }));
+    const constraints = atomicPositionsMatches
+        .filter((match) => match[5] && match[6] && match[7]) // Check if all three constrtaints exist
+        .map((match, index) => ({
+            id: index,
+            value: [match[5] === "1", match[6] === "1", match[7] === "1"],
+        }));
+    // eslint-disable-next-line prefer-destructuring
+    const units = text.match(regex.atomicPositionsUnits)[1];
+
+    return { elements, coordinates, constraints, units };
+}
+
 /**
  * @summary Creates cell from ibrav and celldm(i) parameters
- * // https://www.quantum-espresso.org/Doc/INPUT_PW.html#idm224
- * @param {Object} system ???
- * @returns {Number[][], Number, String, String}
+ * @param {Object} system - The system parameters from &SYSTEM namelist
+ * @param {Number} system.ibrav - ibrav parameter
+ * @param {Number[]} [system.celldm] - celldm parameters
+ * @param {Number} [system.a] - a parameter
+ * @param {Number} [system.b] - b parameter
+ * @param {Number} [system.c] - c parameter
+ * @param {Number} [system.cosab] - cosab parameter
+ * @param {Number} [system.cosac] - cosac parameter
+ * @param {Number} [system.cosbc] - cosbc parameter
+ * @returns {Lattice}
  */
 function ibravToCell(system) {
-    let alat = 1.0;
-    let cell,
-        type,
-        sinab,
-        sinac,
-        tx,
-        ty,
-        tz,
-        a_prime,
-        u,
-        v,
-        v3,
-        b_over_a,
-        c_over_a,
-        cosab,
-        cosac,
-        cosbc;
-    if (system.celldm[1] && system.a) {
-        throw new Error("do not specify both celldm and a,b,c!");
-    } else if (system.celldm[1]) {
+    // eslint-disable-next-line no-unused-vars
+    const { ibrav, celldm, a, b, c, cosab, cosac, cosbc } = system;
+    let alat, type, units;
+
+    // celdm(1) = celldm[0]: index begins from 0
+    if (celldm[0] && a) {
+        throw new Error("Both celldm and a,b,c are given");
+    } else if (celldm[0]) {
         // celldm(x) in bohr
         // eslint-disable-next-line prefer-destructuring
-        alat = system.celldm[1] * coefficients.BOHR_TO_ANGSTROM; // this value given in Bohr (a.u.) as per QE specs
-        b_over_a = system.celldm[2] || 0.0;
-        c_over_a = system.celldm[3] || 0.0;
-        cosab = system.celldm[4] || 0.0;
-        cosac = system.celldm[5] || 0.0;
-        cosbc = 0.0;
-        if (system.ibrav === 14) {
-            cosbc = system.celldm[4] || 0.0;
-            cosac = system.celldm[5] || 0.0;
-            cosab = system.celldm[6] || 0.0;
-        }
-    } else if (system.a) {
+        alat = celldm[0];
+        units = "bohr";
+    } else if (a) {
         // a, b, c, cosAB, cosAC, cosBC in Angstrom
-        throw new Error("params_to_cell() does not yet support A/B/C/cosAB/cosAC/cosBC");
+        throw new Error("ibravToCell() does not yet support A/B/C/cosAB/cosAC/cosBC");
     } else {
         throw new Error("Missing celldm(1)");
     }
 
-    switch (system.ibrav) {
+    switch (ibrav) {
         case 1:
-            cell = [
-                [alat, 0, 0],
-                [0, alat, 0],
-                [0, 0, alat],
-            ];
             type = LATTICE_TYPE.CUB;
             break;
         case 2:
-            cell = [
-                [-0.5 * alat, 0, 0.5 * alat],
-                [0, 0.5 * alat, 0.5 * alat],
-                [-0.5 * alat, 0.5 * alat, 0],
-            ];
             type = LATTICE_TYPE.FCC;
             break;
         case 3:
-            cell = [
-                [0.5 * alat, 0.5 * alat, 0.5 * alat],
-                [-0.5 * alat, 0.5 * alat, 0.5 * alat],
-                [-0.5 * alat, -0.5 * alat, 0.5 * alat],
-            ];
             type = LATTICE_TYPE.BCC;
             break;
         case -3:
-            cell = [
-                [-0.5 * alat, 0.5 * alat, 0.5 * alat],
-                [0.5 * alat, -0.5 * alat, 0.5 * alat],
-                [0.5 * alat, 0.5 * alat, -0.5 * alat],
-            ];
             type = LATTICE_TYPE.BCC;
             break;
         case 4:
-            cell = [
-                [alat, 0, 0],
-                [-0.5 * alat, 0.5 * alat * math.sqrt(3), 0],
-                [0, 0, c_over_a * alat],
-            ];
             type = LATTICE_TYPE.HEX;
             break;
         case 5:
-            tx = math.sqrt((1.0 - cosab) / 2.0);
-            ty = math.sqrt((1.0 - cosab) / 6.0);
-            tz = math.sqrt((1 + 2 * cosab) / 3.0);
-            cell = [
-                [tx * alat, -ty * alat, tz * alat],
-                [0, 2 * ty * alat, tz * alat],
-                [-tx * alat, -ty * alat, tz * alat],
-            ];
-            type = LATTICE_TYPE.TRI; // FIXME: NOT TRUE!
+            type = LATTICE_TYPE.RHL;
             break;
         case -5:
-            ty = math.sqrt((1.0 - cosab) / 6.0);
-            tz = math.sqrt((1 + 2 * cosab) / 3.0);
-            a_prime = alat / math.sqrt(3);
-            u = tz - 2 * math.sqrt(2) * ty;
-            v = tz + math.sqrt(2) * ty;
-            cell = [
-                [u * a_prime, v * a_prime, v * a_prime],
-                [v * a_prime, u * a_prime, v * a_prime],
-                [v * a_prime, v * a_prime, u * a_prime],
-            ];
-            type = LATTICE_TYPE.TRI; // FIXME: NOT TRUE!
+            type = LATTICE_TYPE.RHL;
             break;
         case 6:
-            cell = [
-                [alat, 0, 0],
-                [0, alat, 0],
-                [0, 0, c_over_a * alat],
-            ];
             type = LATTICE_TYPE.TET;
             break;
         case 7:
-            cell = [
-                [0.5 * alat, -0.5 * alat, c_over_a],
-                [0.5 * alat, 0.5 * alat, c_over_a],
-                [-0.5 * alat, -0.5 * alat, c_over_a * 0.5 * alat],
-            ];
             type = LATTICE_TYPE.TET;
             break;
         case 8:
-            cell = [
-                [alat, 0, 0],
-                [0, b_over_a * alat, 0],
-                [0, 0, c_over_a * alat],
-            ];
             type = LATTICE_TYPE.ORC;
             break;
         case 9:
-            cell = [
-                [0.5 * alat, (b_over_a * alat) / 2, 0],
-                [-0.5 * alat, (b_over_a * alat) / 2, 0],
-                [0, 0, c_over_a * alat],
-            ];
             type = LATTICE_TYPE.ORCC;
             break;
         case -9:
-            cell = [
-                [0.5 * alat, (-b_over_a * alat) / 2, 0],
-                [0.5 * alat, (b_over_a * alat) / 2, 0],
-                [0, 0, c_over_a * alat],
-            ];
             type = LATTICE_TYPE.ORCC;
             break;
         case 10:
-            cell = [
-                [0.5 * alat, 0, 0.5 * c_over_a],
-                [0.5 * alat, 0.5 * b_over_a * alat, 0],
-                [0, 0.5 * b_over_a * alat, 0.5 * c_over_a * alat],
-            ];
             type = LATTICE_TYPE.ORCF;
             break;
         case 11:
-            cell = [
-                [0.5 * alat, 0.5 * b_over_a * alat, 0.5 * c_over_a * alat],
-                [-0.5 * alat, 0.5 * b_over_a * alat, 0.5 * c_over_a * alat],
-                [-0.5 * alat, -0.5 * b_over_a * alat, 0.5 * c_over_a * alat],
-            ];
             type = LATTICE_TYPE.ORCI;
             break;
         case 12:
-            sinab = math.sqrt(1.0 - cosab ** 2);
-            cell = [
-                [alat, 0, 0],
-                [b_over_a * alat * cosab, b_over_a * alat * sinab, 0],
-                [0, 0, c_over_a * alat],
-            ];
             type = LATTICE_TYPE.MCL; // Monoclinic P, unique axis c
             break;
         case -12:
-            sinac = math.sqrt(1.0 - cosac ** 2);
-            cell = [
-                [alat, 0, 0],
-                [0, b_over_a * alat, 0],
-                [c_over_a * alat * cosac, 0, c_over_a * alat * sinac],
-            ];
             type = LATTICE_TYPE.MCL; // Monoclinic P, unique axis b
             break;
         case 13:
-            sinab = math.sqrt(1.0 - cosab ** 2);
-            cell = [
-                [0.5 * alat, 0, -0.5 * c_over_a * alat],
-                [b_over_a * alat * cosab, b_over_a * alat * sinab, 0],
-                [0.5 * alat, 0, 0.5 * c_over_a * alat],
-            ];
             type = LATTICE_TYPE.MCLC; // Monoclinic P, base centered
             break;
         case 14:
-            sinab = math.sqrt(1.0 - cosab ** 2);
-            v3 = [
-                c_over_a * cosac,
-                (c_over_a * (cosbc - cosac * cosab)) / sinab,
-                (c_over_a *
-                    math.sqrt(
-                        1 + 2 * cosbc * cosac * cosab - cosbc ** 2 - cosac ** 2 - cosab ** 2,
-                    )) /
-                    sinab,
-            ];
-            cell = [
-                [alat, 0, 0],
-                [b_over_a * alat * cosab, b_over_a * alat * sinab, 0],
-                [v3[0] * alat, v3[1] * alat, v3[2] * alat],
-            ];
             type = LATTICE_TYPE.TRI;
             break;
         default:
             throw new Error(`ibrav = ${system.ibrav} not implemented`);
     }
-    const units = ATOMIC_COORD_UNITS.angstrom; // This is passed to the
-    alat = 1.0;
-    return { cell, alat, units, type };
+
+    const vectors = []; // TODO: implement
+
+    const config = {
+        a: vectors[0],
+        b: vectors[1],
+        c: vectors[2],
+        alat,
+        type,
+        units,
+    };
+
+    return new Lattice(config);
 }
 
 /**
@@ -373,7 +246,8 @@ function fromEspressoFormat(fileContent) {
     // const nSpecies = data.system.ntyp;
     // const nAtoms = data.system.nat;
     const { ibrav } = data.system;
-    let cell = null;
+    let cell = {};
+    let lattice = {};
 
     // Check for errors
     if (data.system === undefined) {
@@ -384,20 +258,21 @@ function fromEspressoFormat(fileContent) {
     }
 
     if (ibrav === 0) {
-        cell = getCellParameters(data.cell);
+        // Create unit cell from given cell parameters in CELL_PARAMETERS card
+        cell = getCellParameters(data.cards);
+        lattice = Lattice.fromVectors({
+            a: cell.vectors[0],
+            b: cell.vectors[1],
+            c: cell.vectors[2],
+            alat: cell.alat,
+            units: cell.units,
+            type: cell.type,
+        });
     } else {
         // Create unit cell from given ibrav (and celldm(i) or A) with algorithm
-        cell = ibravToCell(data.system);
+        lattice = ibravToCell(data.system);
     }
-    const { elements, coordinates, constraints, units } = data;
-    const lattice = Lattice.fromVectors({
-        a: cell.cell[0],
-        b: cell.cell[1],
-        c: cell.cell[2],
-        alat: cell.alat,
-        units: cell.units,
-    });
-    if (cell.type) lattice.type = cell.type;
+    const { elements, coordinates, constraints, units } = getAtomicPositions(data.cards);
 
     const basis = new ConstrainedBasis({
         elements,
