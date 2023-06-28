083fe02836c381e3f8f0ece59ccee1518775db2e
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-06-28 00:38:07 -0400

    update: adjust import

--- src/parsers/parsers.js
+++ src/parsers/parsers.js
@@ -1,5 +1,5 @@
 import cif from "./cif";
-import espresso from "./espresso/espresso";
+import espresso from "./espresso";
 import nativeFormatParsers from "./native_format_parsers";
 import poscar from "./poscar";
 import xyz from "./xyz";
