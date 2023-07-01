7726c57de83481dedb7ad7841ee833696e169beb
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-06-30 22:14:26 -0400

    update: simplify fortran parser

--- src/utils/parsers/fortran.js
+++ src/utils/parsers/fortran.js
@@ -1,39 +1,54 @@
 import { regex } from "./settings";
 
 /**
+ * Extracts pairs from a string data using provided regex pattern and type.
+ * If isArray is set to true, treats the value as an array.
+ *
+ * @param {String} data - The string data to extract pairs from.
+ * @param {RegExp} regexPattern - The regex pattern to use for extracting pairs.
+ * @param {Function | NumberConstructor} type - The type of the value.
+ * @param {Boolean} [isArray=false] - Whether to treat the value as an array.
+ *
+ * @returns {Array} The extracted pairs. Each pair is represented as an array,
+ *                  where the first element is the key and the second element is the value.
+ *                  If isArray is true, the value is an array where the first element
+ *                  is the index of the Fortran array element and the second element is the value.
+ * @throws {Error} If an invalid type is provided.
+ */
+function extractPairs(data, regexPattern, type, isArray) {
+    return Array.from(data.matchAll(regexPattern)).map((match) => {
+        switch (type) {
+            case Number:
+                return isArray
+                    ? [match[1], parseInt(match[2], 10), parseFloat(match[3])]
+                    : [match[1], parseFloat(match[2])];
+            case String:
+                return isArray
+                    ? [match[1], parseInt(match[2], 10), match[3]]
+                    : [match[1], match[2]];
+            case Boolean:
+                return isArray
+                    ? [match[1], parseInt(match[2], 10), match[3] === "true"]
+                    : [match[1], match[2] === "true"];
+            default:
+                throw new Error("Invalid type");
+        }
+    });
+}
+
+/**
  * @summary Extracts an array of the key value pairs from a Fortran namelist.
  * @param {String} data
  * @returns {Object[]}
  */
 function extractKeyValuePairs(data) {
     const output = {};
-    const numberPairs = Array.from(data.matchAll(regex.numberKeyValue)).map((match) => [
-        match[1],
-        parseFloat(match[2]),
-    ]);
-    const stringPairs = Array.from(data.matchAll(regex.stringKeyValue)).map((match) => [
-        match[1],
-        match[2],
-    ]);
-    const booleanPairs = Array.from(data.matchAll(regex.booleanKeyValue)).map((match) => [
-        match[1],
-        match[2] === "true",
-    ]);
-    const numberArrayPairs = Array.from(data.matchAll(regex.numberArrayKeyValue)).map((match) => [
-        match[1],
-        parseInt(match[2], 10), // get the index of Fortran array element
-        parseFloat(match[3]), // get the value of the Fortran array element
-    ]);
-    const stringArrayPairs = Array.from(data.matchAll(regex.stringArrayKeyValue)).map((match) => [
-        match[1],
-        parseInt(match[2], 10), // get the index of Fortran array element
-        match[3], // get the value of the Fortran array element
-    ]);
-    const booleanArrayPairs = Array.from(data.matchAll(regex.booleanArrayKeyValue)).map((match) => [
-        match[1],
-        parseInt(match[2], 10), // get the index of Fortran array element
-        match[3] === "true", // get the value of the Fortran array element
-    ]);
+    const numberPairs = extractPairs(data, regex.numberKeyValue, Number);
+    const stringPairs = extractPairs(data, regex.stringKeyValue, String);
+    const booleanPairs = extractPairs(data, regex.booleanKeyValue, Boolean);
+    const numberArrayPairs = extractPairs(data, regex.numberArrayKeyValue, Number, true);
+    const stringArrayPairs = extractPairs(data, regex.stringArrayKeyValue, String, true);
+    const booleanArrayPairs = extractPairs(data, regex.booleanArrayKeyValue, Boolean, true);
 
     [...numberPairs, ...stringPairs, ...booleanPairs].forEach((pair) => {
         // eslint-disable-next-line prefer-destructuring
--- src/utils/parsers/settings.js
+++ src/utils/parsers/settings.js
@@ -1,13 +1,65 @@
 import s from "underscore.string";
 
-const fortranDoubleRegex = "([-+]?\\d*\\.?\\d*(?:[Eed][+-]?\\d+)?)";
-const fortranNamelistRegex = "&(%s)((?:\\s|\\S)*?)\\/"; // capturing group: 1 - namelist name, 2 - data
-const fortranCardsRegex = "^\\s*\\/(?![\\s\\S]*^\\/)([\\s\\S]*)"; // capturing group: 1 - cards section string
-const keyValueRegex = "^\\s*%s\\s*=\\s*%s\\s*\\n"; // 0 - string value name, 1 - str|bool|number
-const stringRegex = "([+\\w.\\-\\/]*)"; // capturing group: 1 - string value
-const fortranStringRegex = "'([\\w.\\-\\+\\/ ]*)'"; // capturing group: string value
-const fortranArrayRegex = "^\\s*%s\\(%s\\)\\s*=\\s*%s\\s*\\n"; // capturing groups: 0 - array name, 1 - array index, 2 - value
-const fortranBooleanRegex = "\\.(true|false)\\."; // capturing group: boolean value
+// const fortranDoubleRegex = "([-+]?\\d*\\.?\\d*(?:[Eed][+-]?\\d+)?)";
+// const fortranNamelistRegex = "&(%s)((?:\\s|\\S)*?)\\/"; // capturing group: 1 - namelist name, 2 - data
+// const fortranCardsRegex = "^\\s*\\/(?![\\s\\S]*^\\/)([\\s\\S]*)"; // capturing group: 1 - cards section string
+// const keyValueRegex = "^\\s*%s\\s*=\\s*%s\\s*\\n"; // 0 - string value name, 1 - str|bool|number
+// const stringRegex = "([+\\w.\\-\\/]*)"; // capturing group: 1 - string value
+// const fortranStringRegex = "'([\\w.\\-\\+\\/ ]*)'"; // capturing group: string value
+// const fortranArrayRegex = "^\\s*%s\\(%s\\)\\s*=\\s*%s\\s*\\n"; // capturing groups: 0 - array name, 1 - array index, 2 - value
+// const fortranBooleanRegex = "\\.(true|false)\\."; // capturing group: boolean value
+const fortranDoubleRegex =
+    "([-+]?" + // Optional leading sign
+    "\\d*" + // Zero or more digits before the decimal point
+    "\\.?" + // Optional decimal point
+    "\\d*" + // Zero or more digits after the decimal point
+    "(?:[Eed][+-]?\\d+)?" + // Optional exponent part
+    ")";
+
+const fortranNamelistRegex =
+    "&" + // Start with an ampersand
+    "%s" + // Namelist name
+    "((?:\\s|\\S)*?)" + // Matches any sequence of space or non-space characters
+    "\\/"; // Ends with a slash
+
+const fortranCardsRegex =
+    "^\\s*\\" + // A slash at the beginning of a line with any leading spaces
+    "/" + // Negative lookahead
+    "(?![\\s\\S]*^\\/)" + // Not followed by any characters then a line starting with a slash
+    "([\\s\\S]*)"; // Capture all characters till end
+
+const keyValueRegex =
+    "^\\s*" + // Key name at the start of a line with any leading spaces
+    "%s" + // Key name
+    "\\s*=" + // Equal sign
+    "\\s*" + // Value with any leading and trailing spaces
+    "%s" + // Value
+    "\\s*" +
+    "\\n"; // Ends with a newline character
+
+const stringRegex = "([+\\w.\\-\\/]*)"; // Matches alphanumeric, plus, period, hyphen, and slash characters
+
+const fortranStringRegex =
+    "'" + // Starting single quote
+    "([\\w.\\-\\+\\/ ]*)" + // Matches alphanumeric, period, hyphen, plus, slash, and space characters
+    "'"; // Ending single quote
+
+const fortranArrayRegex =
+    "^\\s*" + // Array name at the start of a line with any leading spaces
+    "%s" + // Array name
+    "\\(" + // Array index enclosed in parentheses
+    "%s" + // Array index
+    "\\)" +
+    "\\s*=" + // Equal sign with any leading spaces
+    "\\s*" + // Value with any leading and trailing spaces
+    "%s" + // Value
+    "\\s*" +
+    "\\n"; // Ends with a newline character
+
+const fortranBooleanRegex =
+    "\\." + // Starting period
+    "(true|false)" + // Matches either "true" or "false"
+    "\\."; // Ending period
 
 export const regex = {
     fortranDouble: fortranDoubleRegex,
@@ -35,6 +87,6 @@ export const regex = {
         "gm",
     ),
     namelists: (namelistName) =>
-        new RegExp(s.sprintf(fortranNamelistRegex, namelistName.toUpperCase())),
+        new RegExp(s.sprintf(fortranNamelistRegex, `(${namelistName.toUpperCase()})`)),
     cards: new RegExp(fortranCardsRegex, "m"),
 };
