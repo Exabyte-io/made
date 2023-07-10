c6f7be8b49fbc52a2927f52dd2427fe9b63fd363
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-07-10 10:26:14 -0700

    update: fix for tests

--- tests/parsers/espresso.js
+++ tests/parsers/espresso.js
@@ -3,7 +3,7 @@ import { expect } from "chai";
 import { Material } from "../../src/material";
 import { EspressoParser } from "../../src/parsers/espresso/7.2/parser";
 import parsers from "../../src/parsers/parsers";
-import { NiCub, NiCubAPWSCFInput, Si, SiPWSCFInput } from "../enums";
+import { NiCub, NiCubPWSCFInput, Si, SiPWSCFInput } from "../enums";
 
 describe("Parsers:Espresso", () => {
     it("should return textual representation of a material according to QE pw.x input format", () => {
@@ -11,8 +11,8 @@ describe("Parsers:Espresso", () => {
         expect(parsers.espresso.toEspressoFormat(material)).to.be.equal(SiPWSCFInput);
     });
 
-    it("should return inetrmediate format", () => {
-        const parser = new EspressoParser(NiCubAPWSCFInput);
+    it("should return inetrmediate format for Ni ", () => {
+        const parser = new EspressoParser(NiCubPWSCFInput);
         expect(parser.getIntermediateFormat()).to.be.equal(NiCub);
     });
 });
