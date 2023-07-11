7877ab4e08c0a2a00bb10b3726552608bc508fd1
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-07-10 22:29:22 -0700

    update: fix the method called in fortranParserMixin test

--- tests/parsers/fortran.js
+++ tests/parsers/fortran.js
@@ -5,11 +5,11 @@ import { BaseParser } from "../../src/parsers/init";
 import { FortranParserMixin } from "../../src/parsers/utils/fortran";
 import { FortranFile1, FortranFile1JSON } from "../enums";
 
-// describe("Parsers:Fortran", () => {
-//     class TestParser extends mix(BaseParser).with(FortranParserMixin) {} // Test class
-//     it("should return intermediate format of parsed input file", () => {
-//         const parser = new TestParser({});
-//         const data = parser.parse(FortranFile1);
-//         expect(data).to.be.deep.equal(FortranFile1JSON);
-//     });
-// });
+describe("Parsers:Fortran", () => {
+    class TestParser extends mix(BaseParser).with(FortranParserMixin) {} // Test class
+    it("should return intermediate format of parsed input file", () => {
+        const parser = new TestParser({});
+        const data = parser.parseNamelists(FortranFile1);
+        expect(data).to.be.deep.equal(FortranFile1JSON);
+    });
+});
