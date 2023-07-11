8acc823109477a928f974c7c351278474e96f8ca
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-07-10 23:30:08 -0700

    update: change fortran tests directpry

--- tests/parsers/utils/fortran.js
+++ tests/parsers/utils/fortran.js
@@ -1,9 +1,9 @@
 import { expect } from "chai";
 import { mix } from "mixwith";
 
-import { BaseParser } from "../../src/parsers/init";
-import { FortranParserMixin } from "../../src/parsers/utils/fortran";
-import { FortranFile1, FortranFile1JSON } from "../enums";
+import { BaseParser } from "../../../src/parsers/init";
+import { FortranParserMixin } from "../../../src/parsers/utils/fortran";
+import { FortranFile1, FortranFile1JSON } from "../../enums";
 
 describe("Parsers:Fortran", () => {
     class TestParser extends mix(BaseParser).with(FortranParserMixin) {} // Test class
