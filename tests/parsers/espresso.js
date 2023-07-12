f4c2322dcd64f2e3a407a44b885d66ec2db268bf
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2023-07-11 17:33:40 -0700

    feat: add more tests to cover more functions

--- tests/parsers/espresso.js
+++ tests/parsers/espresso.js
@@ -3,7 +3,15 @@ import { expect } from "chai";
 import { Material } from "../../src/material";
 import { ESPRESSOMaterialParser } from "../../src/parsers/espresso/parser";
 import parsers from "../../src/parsers/parsers";
-import { BNHex, BNHexIbravPWSCFInput, Si, SiPWSCFInput } from "../enums";
+import {
+    BNHex,
+    BNHexIbravPWSCFInput,
+    BNHexPWSCF,
+    NiCub,
+    NiCubIbravAPWSCFInput,
+    Si,
+    SiPWSCFInput,
+} from "../enums";
 import { assertDeepAlmostEqual } from "../utils";
 
 describe("Parsers:Espresso", () => {
@@ -12,9 +20,22 @@ describe("Parsers:Espresso", () => {
         expect(parsers.espresso.toEspressoFormat(material)).to.be.equal(SiPWSCFInput);
     });
 
-    it("should return a material config from QE input file for BN", () => {
+    it("should return a material config from QE input file for BN Hex with specified ibrav and celldm parameter", () => {
         const parser = new ESPRESSOMaterialParser();
         const materialConfig = parser.parse(BNHexIbravPWSCFInput, "material");
         assertDeepAlmostEqual(materialConfig, BNHex, ["name"]);
+    });
+
+    it("should return a material config from QE input file for BN Hex with cell parameters given", () => {
+        const parser = new ESPRESSOMaterialParser();
+        const materialConfig = parser.parse(BNHexPWSCF, "material");
+        assertDeepAlmostEqual(materialConfig, BNHex, ["name", "lattice"]); // lattice.type is not detected, defaults to TRI, skipping it in tests
+        assertDeepAlmostEqual(materialConfig.lattice, BNHex.lattice, ["type"]);
+    });
+
+    it("should return a material config from QE input file for Ni Cub with specified ibrav and A parameter", () => {
+        const parser = new ESPRESSOMaterialParser();
+        const materialConfig = parser.parse(NiCubIbravAPWSCFInput, "material");
+        assertDeepAlmostEqual(materialConfig, NiCub, ["name"]);
     });
 });
