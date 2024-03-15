29345afe46f9dad648e777b602dc74bd9e1155f4
Sasha Firsov <suns@firsov.net>
2024-03-14 21:59:01 -0700

    SOF-7293: Parsers:XYZ unit tests fix

--- tests/parsers/xyz.js
+++ tests/parsers/xyz.js
@@ -1,14 +1,14 @@
+import { expect } from "chai";
+
 import parsers from "../../src/parsers/parsers";
-import { Si } from "../enums";
-import { assertDeepAlmostEqual } from "../utils";
+import Si from "../fixtures/Si.json";
 
 describe("Parsers:XYZ", () => {
     it("should extract basis from XYZ text", () => {
         const text = "Si 0 0 0 \n Si 0.25 0.25 0.25";
-        assertDeepAlmostEqual(parsers.xyz.toBasisConfig(text), Si.basis, [
-            "constraints",
-            "cell",
-            "units",
-        ]);
+        const basis = parsers.xyz.toBasisConfig(text);
+        expect(basis.elements.length).to.equal(Si.basis.elements.length);
+        expect(basis.elements[0]).to.contains(Si.basis.elements[0]);
+        expect(basis.elements[1]).to.contains(Si.basis.elements[1]);
     });
 });
