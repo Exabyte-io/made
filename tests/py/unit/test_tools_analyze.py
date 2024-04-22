443b66d1f8ea7917233477c2ab7a300858f0ffc8
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-04-22 13:26:28 -0700

    update: add test for area

--- tests/py/unit/test_tools_analyze.py
+++ tests/py/unit/test_tools_analyze.py
@@ -1,6 +1,7 @@
 import numpy as np
-from ase.build import bulk
+from ase.build import bulk, surface
 from mat3ra.made.tools.analyze import calculate_average_interlayer_distance
+from mat3ra.made.tools.analyze import calculate_surface_area
 
 
 def test_calculate_average_interlayer_distance():
@@ -10,3 +11,9 @@ def test_calculate_average_interlayer_distance():
     interface_atoms.set_tags([1] * len(substrate) + [2] * len(film))
     distance = calculate_average_interlayer_distance(interface_atoms, 1, 2)
     assert np.isclose(distance, 4.0725)
+
+
+def test_calculate_surface_area():
+    atoms = bulk("Si", cubic=False)
+    area = calculate_surface_area(atoms)
+    assert np.isclose(area, 12.7673)
