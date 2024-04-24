aad9e86d29c98b151f50f2e72030b2d6757fa0ca
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-04-24 16:08:07 -0700

    update: add tests 2

--- tests/py/unit/test_tools_calculate.py
+++ tests/py/unit/test_tools_calculate.py
@@ -1,7 +1,12 @@
 import numpy as np
-from ase.build import bulk
+from ase.build import bulk, surface
 from ase.calculators import emt
-from mat3ra.made.tools.calculate import calculate_total_energy
+from mat3ra.made.tools.calculate import (
+    calculate_total_energy,
+    calculate_total_energy_per_atom,
+    calculate_surface_energy,
+    calculate_adhesion_energy,
+)
 
 
 def test_calculate_total_energy():
@@ -9,3 +14,19 @@ def test_calculate_total_energy():
     calculator = emt.EMT()
     energy = calculate_total_energy(atoms, calculator)
     assert np.isclose(energy, 1.3612647524769237)
+
+
+def test_calculate_total_energy_per_atom():
+    atoms = bulk("C", cubic=True)
+    calculator = emt.EMT()
+    print(atoms.get_global_number_of_atoms())
+    energy_per_atom = calculate_total_energy_per_atom(atoms, calculator)
+    assert np.isclose(energy_per_atom, 0.1701580940596)
+
+
+def test_calculate_surface_energy():
+    atoms_slab = surface("C", (1, 1, 1), 3, vacuum=10)
+    atoms_bulk = bulk("C", cubic=True)
+    calculator = emt.EMT()
+    surface_energy = calculate_surface_energy(atoms_slab, atoms_bulk, calculator)
+    assert np.isclose(surface_energy, 0.148845)
