8f2e9f57ec3c6d7ce546cc4eb59d1dd2fa7159a5
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-01 12:07:51 -0700

    chore: move from utils to fixtures

--- tests/py/unit/utils.py
+++ tests/py/unit/utils.py
@@ -1,9 +1,10 @@
-from ase.build import bulk
+from typing import Dict
+
 from pymatgen.io.ase import AseAtomsAdaptor
 from pymatgen.core.interface import Interface
 
-tags_to_labels = {1: "substrate", 2: "film"}
-labels_to_tags = {v: k for k, v in tags_to_labels.items()}
+tags_to_labels: Dict = {1: "substrate", 2: "film"}
+labels_to_tags: Dict = {v: k for k, v in tags_to_labels.items()}
 
 
 def atoms_to_interface_structure(atoms):
@@ -21,15 +22,3 @@ def atoms_to_interface_structure(atoms):
         "interface_label", [tags_to_labels[tag] for tag in interface_structure.site_properties["tags"]]
     )
     return interface_structure
-
-
-substrate = bulk("Si", cubic=True)
-film = bulk("Cu", cubic=True)
-INTERFACE_ATOMS = substrate + film
-INTERFACE_ATOMS.set_tags([1] * len(substrate) + [2] * len(film))
-
-interface_structure = atoms_to_interface_structure(INTERFACE_ATOMS)
-
-temp = interface_structure.as_dict()
-
-INTERFACE_STRUCTURE = Interface.from_dict(temp)
