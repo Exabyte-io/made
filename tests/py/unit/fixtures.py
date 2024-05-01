06f892750f2072b619ffd93ecc2db319ffc036e4
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-01 16:17:24 -0700

    update: minor fixes

--- tests/py/unit/fixtures.py
+++ tests/py/unit/fixtures.py
@@ -3,7 +3,7 @@ from typing import Tuple
 from ase.build import bulk
 from pymatgen.core.interface import Interface
 from .utils import atoms_to_interface_structure
-from mat3ra.made.tools.build.interface import patch_interface_with_mean_abs_strain, StrainModes
+from mat3ra.made.tools.build.interface import patch_interface_with_mean_abs_strain
 
 # ASE Atoms fixtures
 substrate = bulk("Si", cubic=True)
@@ -12,12 +12,13 @@ INTERFACE_ATOMS = substrate + film
 INTERFACE_ATOMS.set_tags([1] * len(substrate) + [2] * len(film))
 
 # Pymatgen Interface fixtures
-TERMINATION: Tuple = ("Si_termination", "Cu_termination")
+INTERFACE_TERMINATION: Tuple = ("Si_termination", "Cu_termination")
 
 interface_structure = atoms_to_interface_structure(INTERFACE_ATOMS)
 dict = interface_structure.as_dict()
 
 INTERFACE_STRUCTURE = Interface.from_dict(dict)
-INTERFACE_STRUCTURE.interface_properties["termination"] = TERMINATION
-INTERFACE_STRUCTURE.interface_properties[StrainModes.strain] = 0.1
+# Add properties that are assigned during interface creation in ZSL algorithm
+INTERFACE_STRUCTURE.interface_properties["termination"] = INTERFACE_TERMINATION
+INTERFACE_STRUCTURE.interface_properties["strain"] = 0.1
 INTERFACE_STRUCTURE = patch_interface_with_mean_abs_strain(INTERFACE_STRUCTURE)
