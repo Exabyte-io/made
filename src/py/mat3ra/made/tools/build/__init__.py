4d3ca7191bd143ec4304864931f02fc43d9362f1
Timur Bazhirov <timur@exabyte.io>
2024-05-02 11:39:27 -0700

    Update __init__.py

--- src/py/mat3ra/made/tools/build/__init__.py
+++ src/py/mat3ra/made/tools/build/__init__.py
@@ -9,7 +9,6 @@ from ..utils import translate_to_bottom_pymatgen_structure
 from ..convert import decorator_convert_material_args_kwargs_to_structure
 
 
-# TODO: Refactor later when Interface Settings defined in ESSE
 class Settings(TypedDict):
     SUBSTRATE_PARAMETERS: TypedDict[Tuple[int, int, int], int]
     LAYER_PARAMETERS: TypedDict[Tuple[int, int, int], int]
