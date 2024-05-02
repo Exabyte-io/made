5906d99c6af1aaaaed296afddcd29d51c78e8d30
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-01 21:05:06 -0700

    chore: rename decorator

--- src/py/mat3ra/made/tools/modify.py
+++ src/py/mat3ra/made/tools/modify.py
@@ -2,10 +2,10 @@ from typing import Union
 
 from ase import Atoms
 
-from .convert import convert_material_args_kwargs_to_atoms
+from .convert import decorator_convert_material_args_kwargs_to_atoms
 
 
-@convert_material_args_kwargs_to_atoms
+@decorator_convert_material_args_kwargs_to_atoms
 def filter_by_label(atoms: Atoms, label: Union[int, str]):
     """
     Filter out only atoms corresponding to the label/tag.
