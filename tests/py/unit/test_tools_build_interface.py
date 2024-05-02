420814974ac465918c9b08f1d43d667a4936dce2
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-02 11:58:59 -0700

    update: add test fro remove duplicates

--- tests/py/unit/test_tools_build_interface.py
+++ tests/py/unit/test_tools_build_interface.py
@@ -13,3 +13,11 @@ def test_get_interfaces_for_termination():
     interfaces_data = InterfaceDataHolder()
     interfaces_data.add_data_entries([INTERFACE_STRUCTURE])
     assert interfaces_data.get_interfaces_for_termination(0)[0] == INTERFACE_STRUCTURE
+
+
+def test_remove_duplicate_interfaces():
+    interfaces_data = InterfaceDataHolder()
+    interfaces_data.add_data_entries([INTERFACE_STRUCTURE, INTERFACE_STRUCTURE], remove_duplicates=False)
+    assert len(interfaces_data.get_interfaces_for_termination(INTERFACE_TERMINATION)) == 2
+    interfaces_data.remove_duplicate_interfaces()
+    assert len(interfaces_data.get_interfaces_for_termination(INTERFACE_TERMINATION)) == 1
