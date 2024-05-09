7fb8c4b662a74afd606346330c4b193301876232
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-09 11:48:10 -0700

    fix: tests

--- tests/py/unit/test_tools_build.py
+++ tests/py/unit/test_tools_build.py
@@ -1,20 +1,13 @@
 import platform
-
 from mat3ra.made.tools.build import create_interfaces
-from mat3ra.made.tools.build.interface import InterfaceSettings
+from mat3ra.made.tools.build.interface import InterfaceSettings, ZSLParameters
 
 from .fixtures import LAYER_MATERIAL, SUBSTRATE_MATERIAL
 
 MAX_AREA = 200
 # pymatgen `2023.6.23` supporting py3.8 returns 1 interface instead of 2
 EXPECTED_NUMBER_OF_INTERFACES = 1 if platform.python_version().startswith("3.8") else 2
-settings = InterfaceSettings(
-    USE_CONVENTIONAL_CELL=True,
-    INTERFACE_PARAMETERS={"DISTANCE_Z": 3.0, "MAX_AREA": MAX_AREA},
-    ZSL_PARAMETERS={"MAX_AREA": MAX_AREA, "MAX_AREA_TOL": 0.09, "MAX_LENGTH_TOL": 0.03, "MAX_ANGLE_TOL": 0.01},
-    SUBSTRATE_PARAMETERS={"MILLER_INDICES": (1, 1, 1), "THICKNESS": 3},
-    LAYER_PARAMETERS={"MILLER_INDICES": (0, 0, 1), "THICKNESS": 1},
-)
+settings = InterfaceSettings(use_conventional_cell=True, ZSLParameters=ZSLParameters(max_area=MAX_AREA))
 
 
 def test_create_interfaces():
