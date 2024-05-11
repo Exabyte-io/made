6384e18b0101628a91f7908794a3b7700b0d0122
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-10 19:14:18 -0700

    feat: add relaxation function

--- src/py/mat3ra/made/tools/modify.py
+++ src/py/mat3ra/made/tools/modify.py
@@ -1,14 +1,25 @@
+from enum import Enum
 from typing import Union
 
 from ase import Atoms
+from ase.optimize import BFGS
 from pymatgen.analysis.structure_analyzer import SpacegroupAnalyzer
 from pymatgen.core.structure import Structure
 
+from .calculate import CalculatorEnum, calculator_by_name_map
 from .convert import (
     decorator_convert_material_args_kwargs_to_atoms,
     decorator_convert_material_args_kwargs_to_structure,
 )
 from .utils import translate_to_bottom_pymatgen_structure
+
+
+# TODO: ASE related enums and maps should be placed close to each other
+class OptimizerEnum(Enum):
+    BFGS = "BFGS"
+
+
+optimizer_by_name_map = {"BFGS": BFGS}
 
 
 @decorator_convert_material_args_kwargs_to_atoms
@@ -56,3 +67,27 @@ def wrap_to_unit_cell(structure: Structure):
     """
     structure.make_supercell((1, 1, 1), to_unit_cell=True)
     return structure
+
+
+@decorator_convert_material_args_kwargs_to_atoms
+def relax_atoms(atoms: Atoms, calculator: CalculatorEnum, optimizer: OptimizerEnum, **kwargs):
+    """
+    Relax the atoms using the calculator.
+
+    Args:
+        atoms (ase.Atoms): The Atoms object to relax.
+        calculator (CalculatorEnum): The calculator name to use for the relaxation.
+        optimizer (OptimizerEnum): The optimizer name to use for the relaxation.
+        **kwargs: Additional keyword arguments to pass to the relaxation method.
+
+    Returns:
+        ase.Atoms: The relaxed Atoms object.
+    """
+    calculator_object = calculator_by_name_map[calculator.value]()
+    atoms.set_calculator(calculator_object)
+    atoms.get_potential_energy()
+
+    optimizer_object = optimizer_by_name_map[optimizer.value]
+    dyn = optimizer_object(atoms, **kwargs)
+    dyn.run(fmax=0.01)
+    return atoms
