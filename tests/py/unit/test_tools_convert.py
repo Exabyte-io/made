71cc8c3ad45ab2474e0777a894fcb25f2d293720
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-13 21:20:17 -0700

    update: add test for interface properties conversion

--- tests/py/unit/test_tools_convert.py
+++ tests/py/unit/test_tools_convert.py
@@ -1,12 +1,16 @@
+import json
+
 import numpy as np
 from ase import Atoms
 from ase.build import bulk
 from mat3ra.made.material import Material
 from mat3ra.made.tools.convert import from_ase, from_poscar, from_pymatgen, to_ase, to_poscar, to_pymatgen
+from mat3ra.utils import object as object_utils
 from pymatgen.core.structure import Element, Lattice, Structure
 
 PYMATGEN_LATTICE = Lattice.from_parameters(a=3.84, b=3.84, c=3.84, alpha=120, beta=90, gamma=60)
 PYMATGEN_STRUCTURE = Structure(PYMATGEN_LATTICE, ["Si", "Si"], [[0, 0, 0], [0.75, 0.5, 0.75]])
+from .fixtures import INTERFACE_PROPERTIES_JSON, INTERFACE_STRUCTURE
 
 POSCAR_CONTENT = """Si2
 1.0
@@ -34,6 +38,9 @@ def test_from_pymatgen():
     material_data = from_pymatgen(PYMATGEN_STRUCTURE)
     assert material_data["lattice"]["a"] == 3.84
     assert material_data["lattice"]["alpha"] == 120
+    interface_data = from_pymatgen(INTERFACE_STRUCTURE)
+    actual_properties = interface_data["metadata"]["interface_properties"]
+    object_utils.assert_deep_almost_equal(INTERFACE_PROPERTIES_JSON, actual_properties)
 
 
 def test_to_poscar():
