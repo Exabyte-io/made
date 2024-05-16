980c8193d2f81d1efa1e19fa1f5a2230ecf17bc4
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-16 11:18:53 -0700

    update: return builder based on name + test

--- tests/py/unit/test_tools_build.py
+++ tests/py/unit/test_tools_build.py
@@ -1,17 +1,17 @@
 import platform
 
 from mat3ra.made.tools.build import create_interfaces
-from mat3ra.made.tools.build.interface import InterfaceSettings, SlabParameters, ZSLParameters
+from mat3ra.made.tools.build.interface import InterfaceBuilderSettings, SlabParameters, StrainMatchingParameters
 
 from .fixtures import LAYER_MATERIAL, SUBSTRATE_MATERIAL
 
 MAX_AREA = 200
 # pymatgen `2023.6.23` supporting py3.8 returns 1 interface instead of 2
 EXPECTED_NUMBER_OF_INTERFACES = 1 if platform.python_version().startswith("3.8") else 2
-settings = InterfaceSettings(
+settings = InterfaceBuilderSettings(
     use_conventional_cell=True,
     substrate_parameters=SlabParameters(miller_indices=(1, 1, 1)),
-    zsl_parameters=ZSLParameters(max_area=MAX_AREA),
+    strain_matching_parameters=StrainMatchingParameters(max_area=MAX_AREA),
 )
 
 
