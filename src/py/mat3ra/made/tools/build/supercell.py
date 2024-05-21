0a3d353d1f687ac3d9c9c6924ecfaecf33cbae47
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-20 20:22:35 -0700

    chore: typing

--- src/py/mat3ra/made/tools/build/supercell.py
+++ src/py/mat3ra/made/tools/build/supercell.py
@@ -1,19 +1,20 @@
-from typing import TypeVar
+from typing import TypeVar, List
 from mat3ra.utils.matrix import convert_2x2_to_3x3
 
 from ...material import Material
 from ..convert import from_ase, to_ase
 
+# Used to allow parameters based on Material (Slab, Interface etc)
 MaterialBased = TypeVar("MaterialBased", bound=Material)
 
 
-def create_supercell(material: Material, supercell_matrix: list[list[int]]) -> Material:
+def create_supercell(material: Material, supercell_matrix: List[List[int]]) -> Material:
     """
     Create a supercell of the material.
 
     Args:
         material (Material): The material to create a supercell of.
-        supercell_matrix (list[list[int]]): The supercell matrix.
+        supercell_matrix (List[List[int]]): The supercell matrix.
 
     Returns:
         Material: The supercell of the material.
