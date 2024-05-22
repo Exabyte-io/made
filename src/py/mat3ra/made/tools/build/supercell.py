ca7fbe77aeae6e00b23b60395762e87511d0a21c
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-22 13:08:56 -0700

    update; decorate supercell

--- src/py/mat3ra/made/tools/build/supercell.py
+++ src/py/mat3ra/made/tools/build/supercell.py
@@ -1,29 +1,25 @@
-from typing import TypeVar, List
-from mat3ra.utils.matrix import convert_2x2_to_3x3
+from typing import List
+from ase import Atoms
+from ..utils import decorator_convert_2x2_to_3x3
 
 from ...material import Material
-from ..convert import from_ase, to_ase
-
-# Used to allow parameters based on Material (Slab, Interface etc)
-MaterialBased = TypeVar("MaterialBased", bound=Material)
+from ..convert import from_ase, decorator_convert_material_args_kwargs_to_atoms
 
 
-def create_supercell(material: Material, supercell_matrix: List[List[int]]) -> Material:
+@decorator_convert_2x2_to_3x3
+@decorator_convert_material_args_kwargs_to_atoms
+def create_supercell(atoms: Atoms, supercell_matrix: List[List[int]]) -> Material:
     """
-    Create a supercell of the material.
+    Create a supercell of the atoms.
 
     Args:
-        material (Material): The material to create a supercell of.
+        atoms (Material): The atoms to create a supercell of.
         supercell_matrix (List[List[int]]): The supercell matrix.
 
     Returns:
-        Material: The supercell of the material.
+        Material: The supercell of the atoms.
     """
     from ase.build.supercells import make_supercell
 
-    # if xy_supercell provided, create 3x3 matrix
-    if len(supercell_matrix) == 2:
-        supercell_matrix = convert_2x2_to_3x3(supercell_matrix)
-
-    supercell_atoms = make_supercell(to_ase(material), supercell_matrix)
+    supercell_atoms = make_supercell(atoms, supercell_matrix)
     return Material(from_ase(supercell_atoms))
