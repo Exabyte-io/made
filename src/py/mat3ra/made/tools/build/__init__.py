c8235c2bb20d950b5c3762125de4c4c71be3d21d
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-23 22:25:14 -0700

    update: cleanup

--- src/py/mat3ra/made/tools/build/__init__.py
+++ src/py/mat3ra/made/tools/build/__init__.py
@@ -7,7 +7,6 @@ from .interface import (
     TerminationPair,
     InterfaceConfiguration,
     InterfaceConfigurationStrainMatcher,
-    InterfaceBuilderSettings as Settings,
     interface_patch_with_mean_abs_strain,
 )
 from ..convert import (
@@ -18,79 +17,79 @@ from ..convert import (
 )
 from ..modify import translate_to_bottom, wrap_to_unit_cell
 
-
-@decorator_convert_material_args_kwargs_to_structure
-def create_interfaces(
-    substrate: Optional[Material] = None,
-    layer: Optional[Material] = None,
-    settings: Settings = Settings(),
-    sort_by_strain_and_size: bool = True,
-    remove_duplicates: bool = True,
-    is_logging_enabled: bool = True,
-    interface_builder: Optional[InterfaceConfiguration] = None,
-    termination: Optional[TerminationPair] = None,
-) -> InterfaceDataHolder:
-    """
-    Create all interfaces between the substrate and layer structures using ZSL algorithm provided by pymatgen.
-
-    Args:
-        substrate (Material): The substrate structure.
-        layer (Material): The layer structure.
-        settings: The settings for the interface generation.
-        sort_by_strain_and_size (bool): Whether to sort the interfaces by strain and size.
-        remove_duplicates (bool): Whether to remove duplicate interfaces.
-        is_logging_enabled (bool): Whether to enable debug print.
-    Returns:
-        InterfaceDataHolder.
-    """
-    if is_logging_enabled:
-        print("Creating interfaces...")
-
-    builder = interface_builder or init_interface_builder(substrate, layer, settings)
-    interfaces_data = InterfaceDataHolder()
-
-    if termination is not None:
-        builder.terminations = [termination]
-
-    for termination in builder.terminations:
-        all_interfaces_for_termination = builder.get_interfaces(
-            termination,
-            gap=settings.distance_z,
-            film_thickness=settings.LayerParameters.thickness,
-            substrate_thickness=settings.SubstrateParameters.thickness,
-            in_layers=True,
-        )
-
-        all_interfaces_for_termination_patched_wrapped = list(
-            map(
-                lambda i: wrap_to_unit_cell(interface_patch_with_mean_abs_strain(i)),
-                all_interfaces_for_termination,
-            )
-        )
-
-        interfaces_data.add_data_entries(
-            all_interfaces_for_termination_patched_wrapped,
-            sort_interfaces_by_strain_and_size=sort_by_strain_and_size,
-            remove_duplicates=remove_duplicates,
-        )
-
-    if is_logging_enabled:
-        unique_str = "unique" if remove_duplicates else ""
-        print(f"Found {len(interfaces_data.get_interfaces_for_termination(0))} {unique_str} interfaces.")
-
-    return interfaces_data
-
-
-@decorator_convert_material_args_kwargs_to_structure
-def init_interface_builder(
-    substrate: Material,
-    film: Material,
-    settings: Settings,
-) -> InterfaceConfiguration:
-    substrate = translate_to_bottom(substrate, settings.use_conventional_cell)
-    film = translate_to_bottom(film, settings.use_conventional_cell)
-    return InterfaceConfiguration(
-        substrate_slab=substrate,
-        film_slab=film,
-        settings=settings,
-    )
+#
+# @decorator_convert_material_args_kwargs_to_structure
+# def create_interfaces(
+#     substrate: Optional[Material] = None,
+#     layer: Optional[Material] = None,
+#     settings: Settings = Settings(),
+#     sort_by_strain_and_size: bool = True,
+#     remove_duplicates: bool = True,
+#     is_logging_enabled: bool = True,
+#     interface_builder: Optional[InterfaceConfiguration] = None,
+#     termination: Optional[TerminationPair] = None,
+# ) -> InterfaceDataHolder:
+#     """
+#     Create all interfaces between the substrate and layer structures using ZSL algorithm provided by pymatgen.
+#
+#     Args:
+#         substrate (Material): The substrate structure.
+#         layer (Material): The layer structure.
+#         settings: The settings for the interface generation.
+#         sort_by_strain_and_size (bool): Whether to sort the interfaces by strain and size.
+#         remove_duplicates (bool): Whether to remove duplicate interfaces.
+#         is_logging_enabled (bool): Whether to enable debug print.
+#     Returns:
+#         InterfaceDataHolder.
+#     """
+#     if is_logging_enabled:
+#         print("Creating interfaces...")
+#
+#     builder = interface_builder or init_interface_builder(substrate, layer, settings)
+#     interfaces_data = InterfaceDataHolder()
+#
+#     if termination is not None:
+#         builder.terminations = [termination]
+#
+#     for termination in builder.terminations:
+#         all_interfaces_for_termination = builder.get_interfaces(
+#             termination,
+#             gap=settings.distance_z,
+#             film_thickness=settings.LayerParameters.thickness,
+#             substrate_thickness=settings.SubstrateParameters.thickness,
+#             in_layers=True,
+#         )
+#
+#         all_interfaces_for_termination_patched_wrapped = list(
+#             map(
+#                 lambda i: wrap_to_unit_cell(interface_patch_with_mean_abs_strain(i)),
+#                 all_interfaces_for_termination,
+#             )
+#         )
+#
+#         interfaces_data.add_data_entries(
+#             all_interfaces_for_termination_patched_wrapped,
+#             sort_interfaces_by_strain_and_size=sort_by_strain_and_size,
+#             remove_duplicates=remove_duplicates,
+#         )
+#
+#     if is_logging_enabled:
+#         unique_str = "unique" if remove_duplicates else ""
+#         print(f"Found {len(interfaces_data.get_interfaces_for_termination(0))} {unique_str} interfaces.")
+#
+#     return interfaces_data
+#
+#
+# @decorator_convert_material_args_kwargs_to_structure
+# def init_interface_builder(
+#     substrate: Material,
+#     film: Material,
+#     settings: Settings,
+# ) -> InterfaceConfiguration:
+#     substrate = translate_to_bottom(substrate, settings.use_conventional_cell)
+#     film = translate_to_bottom(film, settings.use_conventional_cell)
+#     return InterfaceConfiguration(
+#         substrate_slab=substrate,
+#         film_slab=film,
+#         settings=settings,
+#     )
