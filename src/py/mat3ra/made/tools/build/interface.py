20c38bb13776919ccc2fa3f79d57fa9756008691
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-28 18:12:38 -0700

    update: use base builder

--- src/py/mat3ra/made/tools/build/interface.py
+++ src/py/mat3ra/made/tools/build/interface.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel
 from pymatgen.analysis.interfaces.coherent_interfaces import CoherentInterfaceBuilder, ZSLGenerator, Interface
 from ase.build.tools import niggli_reduce
 
+from . import BaseBuilder
 from ..convert import to_ase, from_ase, to_pymatgen, from_pymatgen
 from .slab import BaseSlabConfiguration, SlabConfiguration
 from ...material import Material
@@ -20,11 +21,19 @@ class StrainModes(str, Enum):
     mean_abs_strain = "mean_abs_strain"
 
 
+class SimpleInterfaceBuilderParameters(BaseModel):
+    scale_film: bool = True
+
+
 class ZSLStrainMatchingParameters(BaseModel):
     max_area: float = 50.0
     max_area_ratio_tol: float = 0.09
     max_length_tol: float = 0.03
     max_angle_tol: float = 0.01
+
+
+class CSLStrainMatchingParameters(BaseModel):
+    pass
 
 
 class InterfaceConfiguration(BaseSlabConfiguration):
@@ -54,39 +63,21 @@ class InterfaceConfiguration(BaseSlabConfiguration):
 
     def get_material(self, scale_film_to_fit: bool = False):
         interface_builder = SimpleInterfaceBuilder(
-            strain_matching_parameters=NoStrainMatchingParameters(scale_film_to_fit=scale_film_to_fit)
+            build_parameters=SimpleInterfaceBuilderParameters(scale_film_to_fit=scale_film_to_fit)
         )
         return interface_builder.get_material(self)
 
 
-class NoStrainMatchingParameters(BaseModel):
-    scale_film: bool = True
-
-
-class BaseInterfaceBuilder:
-    def __init__(
-        self,
-        strain_matching_parameters: Optional[Union[NoStrainMatchingParameters, ZSLStrainMatchingParameters]] = None,
-    ):
-        self.strain_matching_parameters = strain_matching_parameters
-        pass
-
-    def get_materials(self, interface_configuration: InterfaceConfiguration) -> List[Material]:
-        raise NotImplementedError
-
-    def get_material(self, interface_configuration: InterfaceConfiguration) -> Material:
-        raise NotImplementedError
-
-
-class SimpleInterfaceBuilder(BaseInterfaceBuilder):
+class SimpleInterfaceBuilder(BaseBuilder):
     """
     Creates matching interface between substrate and film by straining the film to match the substrate.
     """
 
-    def __init__(self, strain_matching_parameters: Optional[NoStrainMatchingParameters] = None):
-        super().__init__(strain_matching_parameters=strain_matching_parameters)
+    def __init__(self, build_parameters: Optional[SimpleInterfaceBuilderParameters] = None):
+        super().__init__()
+        self.build_parameters = build_parameters
 
-    def get_material(self, interface_configuration: InterfaceConfiguration) -> Material:
+    def get_material(self, interface_configuration: InterfaceConfiguration, **kwargs) -> Material:
         vacuum = interface_configuration.vacuum
         substrate_slab = interface_configuration.substrate_configuration.get_material(
             termination=interface_configuration.termination_pair[1]
@@ -102,7 +93,7 @@ class SimpleInterfaceBuilder(BaseInterfaceBuilder):
         niggli_reduce(substrate_slab_ase)
         niggli_reduce(film_slab_ase)
 
-        if self.strain_matching_parameters.scale_film:
+        if self.build_parameters.scale_film:
             film_slab_ase.set_cell(substrate_slab_ase.cell, scale_atoms=True)
             film_slab_ase.wrap()
 
@@ -123,7 +114,16 @@ class SimpleInterfaceBuilder(BaseInterfaceBuilder):
         return Material(material_dict)
 
 
-class ZSLInterfaceBuilder(BaseInterfaceBuilder):
+class StrainMatchingInterfaceBuilder(BaseBuilder):
+    def __init__(
+        self,
+        strain_matching_parameters: Optional[Union[CSLStrainMatchingParameters, ZSLStrainMatchingParameters]] = None,
+    ):
+        self.strain_matching_parameters = strain_matching_parameters
+        pass
+
+
+class ZSLInterfaceBuilder(StrainMatchingInterfaceBuilder):
     """
     Creates matching interface between substrate and film using the ZSL algorithm.
     """
@@ -132,33 +132,36 @@ class ZSLInterfaceBuilder(BaseInterfaceBuilder):
         super().__init__(strain_matching_parameters=strain_matching_parameters)
         self.strain_matching_parameters = strain_matching_parameters.dict()
 
-    def get_material(self, interface_configuration: InterfaceConfiguration) -> Material:
-        interface = self.get_sorted_interfaces()[0]
-        interface_ase = to_ase(interface)
-        material_dict = from_ase(interface_ase)
-        return Material(material_dict)
+    def get_material(self, configuration: InterfaceConfiguration, **kwargs):
+        interface = self.get_sorted_interfaces(configuration)[0]
 
-    def get_sorted_interfaces(self):
-        interfaces = self.get_interface_structures()
-        interfaces = sorted(interfaces, key=lambda x: np.mean(np.abs(x.interface_properties["strain"])))
+        return Material(from_pymatgen(interface))
+
+    def get_materials(self, configuration: InterfaceConfiguration, **kwargs):
+        interfaces = self.get_sorted_interfaces(configuration)
         return [Material(from_pymatgen(interface)) for interface in interfaces]
 
-    def get_interface_structures(self, interface_configuration: InterfaceConfiguration) -> InterfacesType:
-        interfaces = self.generator(interface_configuration).get_interfaces(
-            termination=interface_configuration.termination_pair,
-            gap=interface_configuration.distance_z,
-            film_thickness=interface_configuration.film_configuration.thickness,
-            substrate_thickness=interface_configuration.substrate_configuration.thickness,
+    def get_sorted_interfaces(self, configuration: InterfaceConfiguration):
+        interfaces = self.get_interface_structures(configuration)
+        interfaces = sorted(interfaces, key=lambda x: np.mean(np.abs(x.interface_properties["strain"])))
+        return interfaces
+
+    def get_interface_structures(self, configuration: InterfaceConfiguration) -> InterfacesType:
+        interfaces = self.__generator(configuration).get_interfaces(
+            termination=configuration.termination_pair,
+            gap=configuration.distance_z,
+            film_thickness=configuration.film_configuration.thickness,
+            substrate_thickness=configuration.substrate_configuration.thickness,
             in_layers=True,
         )
         return list(interfaces)
 
-    def generator(self, interface_configuration: InterfaceConfiguration) -> CoherentInterfaceBuilder:
+    def __generator(self, configuration: InterfaceConfiguration) -> CoherentInterfaceBuilder:
         generator = ZSLGenerator(**self.strain_matching_parameters)
         return CoherentInterfaceBuilder(
-            substrate_structure=to_pymatgen(interface_configuration.substrate_configuration.bulk),
-            film_structure=to_pymatgen(interface_configuration.film_configuration.bulk),
-            substrate_miller=interface_configuration.substrate_configuration.miller_indices,
-            film_miller=interface_configuration.film_configuration.miller_indices,
+            substrate_structure=to_pymatgen(configuration.substrate_configuration.bulk),
+            film_structure=to_pymatgen(configuration.film_configuration.bulk),
+            substrate_miller=configuration.substrate_configuration.miller_indices,
+            film_miller=configuration.film_configuration.miller_indices,
             zslgen=generator,
         )
