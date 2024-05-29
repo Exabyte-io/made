d15a0457a7316416d0fcc343dcddbad5e4f040f7
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-05-28 21:14:40 -0700

    update: fix tests

--- tests/py/unit/fixtures.py
+++ tests/py/unit/fixtures.py
@@ -2,7 +2,7 @@ from typing import Tuple
 
 from ase.build import bulk
 from mat3ra.made.material import Material
-from mat3ra.made.tools.build.interface import interface_patch_with_mean_abs_strain
+from mat3ra.made.tools.build.slab import SlabConfiguration
 from mat3ra.made.tools.convert import from_ase
 from pymatgen.analysis.elasticity.strain import Strain
 from pymatgen.core.interface import Interface
@@ -19,9 +19,15 @@ INTERFACE_ATOMS.set_tags([1] * len(substrate) + [2] * len(film))
 SUBSTRATE_MATERIAL = Material(from_ase(substrate))
 LAYER_MATERIAL = Material(from_ase(film))
 
+SUBSTRATE_CONFIGURATION = SlabConfiguration(bulk=SUBSTRATE_MATERIAL, thickness=3)
+LAYER_CONFIGURATION = SlabConfiguration(bulk=LAYER_MATERIAL)
+
 # Pymatgen Interface fixtures
-INTERFACE_TERMINATION: Tuple = ("Si_termination", "Cu_termination")
-INTERFACE_TERMINATION_AS_STR = str(INTERFACE_TERMINATION)
+INTERFACE_TERMINATION_PAIR: Tuple[str, str] = (
+    LAYER_CONFIGURATION.terminations[0],
+    SUBSTRATE_CONFIGURATION.terminations[0],
+)
+INTERFACE_TERMINATION_AS_STR = str(INTERFACE_TERMINATION_PAIR)
 
 interface_structure = atoms_to_interface_structure(INTERFACE_ATOMS)
 dict = interface_structure.as_dict()
@@ -33,7 +39,7 @@ INTERFACE_PROPERTIES_MOCK = {
     "substrate_transformation": [[1.0, 0.0], [0.0, 1.0]],
     "strain": Strain([[0.004746364, -0.0, -0.0], [-0.0, 0.004746364, 0.0], [-0.0, 0.0, -0.0]]),
     "von_mises_strain": 0.001,
-    "termination": INTERFACE_TERMINATION,
+    "termination": INTERFACE_TERMINATION_PAIR,
 }
 INTERFACE_PROPERTIES_JSON = {
     "film_transformation": [[2.0, 0.0], [0.0, 2.0]],
@@ -41,10 +47,9 @@ INTERFACE_PROPERTIES_JSON = {
     "strain": [[0.004746364, -0.0, -0.0], [-0.0, 0.004746364, 0.0], [-0.0, 0.0, -0.0]],
     "von_mises_strain": 0.001,
     "termination": INTERFACE_TERMINATION_AS_STR,
-    "mean_abs_strain": 0.00105,
+    # "mean_abs_strain": 0.00105,
 }
 
 
 # Add properties to interface structure
 INTERFACE_STRUCTURE.interface_properties = INTERFACE_PROPERTIES_MOCK
-INTERFACE_STRUCTURE = interface_patch_with_mean_abs_strain(INTERFACE_STRUCTURE)
