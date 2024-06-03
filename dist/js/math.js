c92d2128e2ad2dd9964eeb24b21f60f1b6fca019
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2024-06-03 22:12:33 +0300

    chore: AnyObject from esse + update code.js

--- dist/js/math.js
+++ dist/js/math.js
@@ -2,6 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // TODO: adjust the imports and remove the need for re-exporting
 const math_1 = require("@mat3ra/code/dist/js/math");
-exports.default = {
-    ...math_1.math,
-};
+exports.default = math_1.math;
--- src/js/math.js
+++
@@ -1,6 +0,0 @@
-// TODO: adjust the imports and remove the need for re-exporting
-import { math as codeJSMath } from "@mat3ra/code/dist/js/math";
-
-export default {
-    ...codeJSMath,
-};
