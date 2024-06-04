c0094813599f6f6c31adba249604d578da7cbd2c
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2024-06-04 13:08:07 +0300

    chore: add mathjs types

--- dist/js/math.js
+++ dist/js/math.js
@@ -2,4 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // TODO: adjust the imports and remove the need for re-exporting
 const math_1 = require("@mat3ra/code/dist/js/math");
-exports.default = math_1.math;
+exports.default = {
+    ...math_1.math,
+};
--- src/js/math.ts
+++ src/js/math.ts
@@ -1,4 +1,6 @@
 // TODO: adjust the imports and remove the need for re-exporting
 import { math } from "@mat3ra/code/dist/js/math";
 
-export default math;
+export default {
+    ...math,
+};
