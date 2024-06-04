f5850f79f124f0610e3282d8bd11e57e941c0942
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-06-04 13:14:45 -0700

    chore: fix lint

--- src/py/mat3ra/made/tools/convert/utils.py
+++ src/py/mat3ra/made/tools/convert/utils.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Dict, List, Union
 
 from ase import Atoms
 from pymatgen.core.interface import Interface, label_termination
@@ -15,7 +15,9 @@ label_pymatgen_slab_termination = label_termination
 INTERFACE_LABELS_MAP = {"substrate": 0, "film": 1}
 
 
-def extract_interface_labels_from_pymatgen(structure: type(PymatgenInterface)) -> list[dict[str, int | Any]] | None:  # type: ignore
+def extract_interface_labels_from_pymatgen(
+    structure: type(PymatgenInterface),  # type: ignore
+) -> Union[List[Dict[str, int]], None]:
     interface_labels = None
     if any("interface_label" in site.properties for site in structure.sites):
         interface_labels = [
