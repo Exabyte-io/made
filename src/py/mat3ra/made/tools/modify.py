fbd53b34c1e00018ba054acdb6bbf357542c20e4
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-06-04 18:23:47 -0700

    chore: remove unused relaxer

--- src/py/mat3ra/made/tools/modify.py
+++ src/py/mat3ra/made/tools/modify.py
@@ -1,54 +1,16 @@
-import json
-from enum import Enum
 from typing import Union
 
-from ase import Atoms
-from ase.optimize import BFGS
-from mat3ra.made.material import Material
 from pymatgen.analysis.structure_analyzer import SpacegroupAnalyzer
 from pymatgen.core.structure import Structure
 
-from .calculate import CalculatorEnum, calculator_by_name_map
+from mat3ra.made.material import Material
 from .convert import (
-    decorator_convert_material_args_kwargs_to_atoms,
     decorator_convert_material_args_kwargs_to_structure,
-    from_ase,
 )
 from .utils import translate_to_bottom_pymatgen_structure
 
 
-# TODO: ASE related enums and maps should be placed close to each other
-class OptimizerEnum(Enum):
-    BFGS = "BFGS"
-
-
-optimizer_by_name_map = {"BFGS": BFGS}
-
-
-class RelaxationSettings:
-    def __init__(
-        self,
-        calculator: CalculatorEnum = CalculatorEnum.EMT,
-        optimizer: OptimizerEnum = OptimizerEnum.BFGS,
-        fmax: float = 0.05,
-        **kwargs,
-    ):
-        self.calculator = calculator
-        self.optimizer = optimizer
-        self.fmax = fmax
-        self.kwargs = kwargs
-
-    def __repr__(self):
-        return json.dumps(self.__dict__)
-
-    def __str__(self):
-        return json.dumps(
-            {"calculator": self.calculator, "optimizer": self.optimizer, "fmax": self.fmax, "kwargs": self.kwargs},
-            indent=4,
-        )
-
-
-def filter_by_label(material: Material, label: Union[int, str]):
+def filter_by_label(material: Material, label: Union[int, str]) -> Material:
     """
     Filter out only atoms corresponding to the label.
 
@@ -60,12 +22,16 @@ def filter_by_label(material: Material, label: Union[int, str]):
         Material: The filtered material object.
     """
     labels = material.basis["labels"]
-
+    new_material = material.clone()
     indices = [idx for idx, _label in enumerate(labels) if _label["value"] == label]
-    material.basis["coordinates"] = [coord for idx, coord in enumerate(material.basis["coordinates"]) if idx in indices]
-    material.basis["elements"] = [element for idx, element in enumerate(material.basis["elements"]) if idx in indices]
-    material.basis["labels"] = [label for idx, label in enumerate(labels) if idx in indices]
-    return material
+    new_material.basis["coordinates"] = [
+        coord for idx, coord in enumerate(material.basis["coordinates"]) if idx in indices
+    ]
+    new_material.basis["elements"] = [
+        element for idx, element in enumerate(material.basis["elements"]) if idx in indices
+    ]
+    new_material.basis["labels"] = [label for idx, label in enumerate(labels) if idx in indices]
+    return new_material
 
 
 @decorator_convert_material_args_kwargs_to_structure
@@ -98,25 +64,3 @@ def wrap_to_unit_cell(structure: Structure):
     """
     structure.make_supercell((1, 1, 1), to_unit_cell=True)
     return structure
-
-
-@decorator_convert_material_args_kwargs_to_atoms
-def relax_atoms(atoms: Atoms, relaxation_settings: RelaxationSettings = RelaxationSettings()):
-    """
-    Relax the atoms using the calculator.
-
-    Args:
-        atoms (ase.Atoms): The Atoms object to relax.
-        relaxation_settings (RelaxationSettings): The settings for the relaxation.
-
-    Returns:
-        ase.Atoms: The relaxed Atoms object.
-    """
-    calculator_object = calculator_by_name_map[relaxation_settings.calculator.value]()
-    atoms.set_calculator(calculator_object)
-    atoms.get_potential_energy()
-
-    optimizer_object = optimizer_by_name_map[relaxation_settings.optimizer.value]
-    dyn = optimizer_object(atoms, **relaxation_settings.kwargs)
-    dyn.run(fmax=relaxation_settings.fmax)
-    return from_ase(atoms)
