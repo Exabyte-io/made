352b3275bdf4acb68e3ad16ebef85b767cdce5f6
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-06-05 19:50:16 -0700

    feat: add point defects generator with pymatgen

--- src/py/mat3ra/made/tools/build/defect/__init__.py
+++ src/py/mat3ra/made/tools/build/defect/__init__.py
@@ -5,6 +5,9 @@ from pydantic import BaseModel
 
 from src.py.mat3ra.made.material import Material
 from ...build import BaseBuilder
+from ...convert import PymatgenStructure, to_pymatgen
+from pymatgen.analysis.defects.core import Substitution, Vacancy, Interstitial
+from pymatgen.core import PeriodicSite, Species, Structure
 
 
 class PointDefectTypeEnum(str, Enum):
@@ -41,7 +44,28 @@ class PointDefectBuilder(BaseBuilder):
 
     _BuildParametersType = PointDefectBuilderParameters
     _DefaultBuildParameters = PointDefectBuilderParameters()
-    _GeneratedItemType = ...
+    _GeneratedItemType = PymatgenStructure
     _ConfigurationType = PointDefectConfiguration
     _SelectorParametersType = None
     _PostProcessParametersType = None
+
+    def _generate(self, configuration: PointDefectConfiguration) -> List[_GeneratedItemType]:
+        site_index = self.build_parameters.target_site
+        pymatgen_structure = to_pymatgen(configuration.material)
+        pymatgen_site = pymatgen_structure[site_index]
+        pymatgen_periodic_site = PeriodicSite(
+            species=pymatgen_structure.species[site_index],
+            coords=pymatgen_site.frac_coords,
+            lattice=pymatgen_structure.lattice,
+        )
+
+        if configuration.defect_type == PointDefectTypeEnum.VACANCY:
+            defect = Vacancy(pymatgen_structure, pymatgen_periodic_site)
+        elif configuration.defect_type == PointDefectTypeEnum.SUBSTITUTION:
+            defect = Substitution(pymatgen_structure, pymatgen_periodic_site, configuration.specie)
+        elif configuration.defect_type == PointDefectTypeEnum.INTERSTITIAL:
+            defect = Interstitial(pymatgen_structure, pymatgen_periodic_site)
+        else:
+            raise ValueError(f"Unknown defect type: {configuration.defect_type}")
+
+        return [defect.defect_structure]
