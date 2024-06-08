ab9c8778b27514b837f7fcc6304201f750f63309
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-06-07 17:12:35 -0700

    update: add tests for each defect type

--- tests/py/unit/test_tools_build_defect.py
+++ tests/py/unit/test_tools_build_defect.py
@@ -1,11 +1,37 @@
 from mat3ra.made.material import Material
-from mat3ra.made.tools.build.defect import PointDefectConfiguration, create_defect
+from mat3ra.made.tools.build.defect import (
+    create_defect,
+    VacancyConfiguration,
+    SubstitutionConfiguration,
+    InterstitialConfiguration,
+)
 
 
-def test_create_defect():
+def test_create_vacancy():
     # vacancy in place of 0 element
-    configuration = PointDefectConfiguration()
+    configuration = VacancyConfiguration()
     material = Material.create(Material.default_config)
     defect = create_defect(material, configuration)
 
     assert len(defect.basis["elements"]) == 1
+
+
+def test_create_substitution():
+    # Substitution of Ge in place of Si at 0 element
+    configuration = SubstitutionConfiguration(element="Ge")
+    material = Material.create(Material.default_config)
+    defect = create_defect(material, configuration)
+
+    assert defect.basis["elements"] == [{"id": 0, "value": "Ge"}, {"id": 1, "value": "Si0+"}]
+
+
+def test_create_interstitial():
+    configuration = InterstitialConfiguration(element="Ge", position=[0.5, 0.5, 0.5])
+    material = Material.create(Material.default_config)
+    defect = create_defect(material, configuration)
+
+    assert defect.basis["elements"] == [
+        {"id": 0, "value": "Ge3+"},
+        {"id": 1, "value": "Si0+"},
+        {"id": 2, "value": "Si0+"},
+    ]
