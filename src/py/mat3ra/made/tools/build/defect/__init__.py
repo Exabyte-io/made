090bc8f4c68afc77370d671accef33d828cd98dd
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-06-07 17:12:18 -0700

    update: adjust create defect function

--- src/py/mat3ra/made/tools/build/defect/__init__.py
+++ src/py/mat3ra/made/tools/build/defect/__init__.py
@@ -1,11 +1,26 @@
+from typing import Union, Optional
+
 from mat3ra.made.material import Material
-from .builders import PointDefectBuilder, PointDefectBuilderParameters
-from .configuration import PointDefectConfiguration
+from .builders import VacancyBuilder, SubstitutionBuilder, InterstitialBuilder, PointDefectBuilderParameters
+from .configuration import VacancyConfiguration, SubstitutionConfiguration, InterstitialConfiguration
+from .enums import PointDefectTypeEnum
 
 
-def create_defect(material: Material, configuration: PointDefectConfiguration) -> Material:
+def DEFECT_BUILDER_MAP(builder_parameters):
+    return {
+        PointDefectTypeEnum.VACANCY: VacancyBuilder(builder_parameters),
+        PointDefectTypeEnum.SUBSTITUTION: SubstitutionBuilder(builder_parameters),
+        PointDefectTypeEnum.INTERSTITIAL: InterstitialBuilder(builder_parameters),
+    }
+
+
+def create_defect(
+    material: Material,
+    configuration: Union[VacancyConfiguration, SubstitutionConfiguration, InterstitialConfiguration],
+    builder_parameters: Optional[PointDefectBuilderParameters] = None,
+) -> Material:
     """
-    Return a material with a defect added.
+    Return a material with a selected defect added.
 
     Args:
         material: The material to which the defect will be added.
@@ -14,6 +29,7 @@ def create_defect(material: Material, configuration: PointDefectConfiguration) -
     Returns:
         The material with the defect added.
     """
-    builder = PointDefectBuilder(build_parameters=PointDefectBuilderParameters())
-    configuration.material = material
-    return builder.get_material(configuration)
+    builder = DEFECT_BUILDER_MAP(builder_parameters)[configuration.defect_type]
+
+    configuration.crystal = material
+    return builder.get_material(configuration) if builder else material
