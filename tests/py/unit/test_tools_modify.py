65ef1c294cea897c712a08bb426d835580b5d661
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-06-13 18:45:23 -0700

    update: adjust tests

--- tests/py/unit/test_tools_modify.py
+++ tests/py/unit/test_tools_modify.py
@@ -1,7 +1,79 @@
 from ase.build import bulk
+
+from mat3ra.utils import assertion as assertion_utils
 from mat3ra.made.material import Material
 from mat3ra.made.tools.convert import from_ase
-from mat3ra.made.tools.modify import filter_by_label
+from mat3ra.made.tools.modify import filter_by_label, filter_by_layers, filter_by_sphere
+from .fixtures import SI_CONVENTIONAL_CELL
+
+
+COMMON_PART = {
+    "units": "crystal",
+    "cell": [[5.468763846, 0.0, 0.0], [-0.0, 5.468763846, 0.0], [0.0, 0.0, 5.468763846]],
+    "constraints": [],
+    "labels": [],
+}
+
+expected_layers_section_basis = {
+    "elements": [
+        {"id": 0, "value": "Si"},
+        {"id": 3, "value": "Si"},
+        {"id": 5, "value": "Si"},
+        {"id": 6, "value": "Si"},
+    ],
+    "coordinates": [
+        {"id": 0, "value": [0.5, 0.0, 0.0]},
+        {"id": 3, "value": [0.25, 0.75, 0.25]},
+        {"id": 5, "value": [0.75, 0.25, 0.25]},
+        {"id": 6, "value": [0.0, 0.5, 0.0]},
+    ],
+    **COMMON_PART,
+}
+
+expected_layers_cavity_basis = {
+    "elements": [
+        {"id": 1, "value": "Si"},
+        {"id": 2, "value": "Si"},
+        {"id": 4, "value": "Si"},
+        {"id": 7, "value": "Si"},
+    ],
+    "coordinates": [
+        {"id": 1, "value": [0.25, 0.25, 0.75]},
+        {"id": 2, "value": [0.5, 0.5, 0.5]},
+        {"id": 4, "value": [0.0, 0.0, 0.5]},
+        {"id": 7, "value": [0.75, 0.75, 0.75]},
+    ],
+    **COMMON_PART,
+}
+
+
+expected_sphere_cluster_basis = {
+    "elements": [{"id": 0, "value": "Si"}],
+    "coordinates": [{"id": 0, "value": [0.5, 0.0, 0.0]}],
+    **COMMON_PART,
+}
+
+expected_sphere_cavity_basis = {
+    "elements": [
+        {"id": 1, "value": "Si"},
+        {"id": 2, "value": "Si"},
+        {"id": 3, "value": "Si"},
+        {"id": 4, "value": "Si"},
+        {"id": 5, "value": "Si"},
+        {"id": 6, "value": "Si"},
+        {"id": 7, "value": "Si"},
+    ],
+    "coordinates": [
+        {"id": 1, "value": [0.25, 0.25, 0.75]},
+        {"id": 2, "value": [0.5, 0.5, 0.5]},
+        {"id": 3, "value": [0.25, 0.75, 0.25]},
+        {"id": 4, "value": [0.0, 0.0, 0.5]},
+        {"id": 5, "value": [0.75, 0.25, 0.25]},
+        {"id": 6, "value": [0.0, 0.5, 0.0]},
+        {"id": 7, "value": [0.75, 0.75, 0.75]},
+    ],
+    **COMMON_PART,
+}
 
 
 def test_filter_by_label():
@@ -17,3 +89,19 @@ def test_filter_by_label():
     assert [el["value"] for el in film_material.basis["elements"]] == [
         el["value"] for el in film_extracted.basis["elements"]
     ]
+
+
+def test_filter_by_layers():
+    material = Material(SI_CONVENTIONAL_CELL)
+    section = filter_by_layers(material, 0, 3.0)
+    cavity = filter_by_layers(material, 0, 3.0, invert=True)
+    assertion_utils.assert_deep_almost_equal(expected_layers_section_basis, section.basis)
+    assertion_utils.assert_deep_almost_equal(expected_layers_cavity_basis, cavity.basis)
+
+
+def test_filter_by_sphere():
+    material = Material(SI_CONVENTIONAL_CELL)
+    cluster = filter_by_sphere(material, 0, 2.0)
+    cavity = filter_by_sphere(material, 0, 2.0, invert=True)
+    assertion_utils.assert_deep_almost_equal(expected_sphere_cluster_basis, cluster.basis)
+    assertion_utils.assert_deep_almost_equal(expected_sphere_cavity_basis, cavity.basis)
