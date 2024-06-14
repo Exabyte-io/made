bc2cce9bff3f286191c9d899a41277a9338876d5
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-06-14 15:48:41 -0700

    update: add cell class

--- src/py/mat3ra/made/cell/cell.py
+++ src/py/mat3ra/made/cell/cell.py
@@ -1,42 +1,34 @@
+from typing import List
+
 import numpy as np
 
 
 class Cell:
     tolerance = 1
 
-    def __init__(self, nestedArray):
-        self.vector1, self.vector2, self.vector3 = nestedArray
+    def __init__(self, nested_array):
+        self.vector1, self.vector2, self.vector3 = nested_array
 
     @property
-    def vectorsAsArray(self):
-        return [
-            [0 if abs(c) < self.tolerance else c for c in v]
-            for v in [self.vector1, self.vector2, self.vector3]
-        ]
+    def vectors_as_array(self) -> List[List[float]]:
+        return [[0 if abs(c) < self.tolerance else c for c in v] for v in [self.vector1, self.vector2, self.vector3]]
 
     def clone(self):
-        return self.__class__(self.vectorsAsArray)
+        return self.__class__(self.vectors_as_array)
 
-    def cloneAndScaleByMatrix(self, matrix):
+    def clone_and_scale_by_matrix(self, matrix):
         newCell = self.clone()
-        newCell.scaleByMatrix(matrix)
+        newCell.scale_by_matrix(matrix)
         return newCell
 
-    def convertPointToCartesian(self, point):
-        return np.dot(point, self.vectorsAsArray)
+    def convert_point_to_cartesian(self, point):
+        np_vector = np.array(self.vectors_as_array)
+        return np.dot(point, np_vector)
 
-    def convertPointToFractional(self, point):
-        return np.dot(point, np.linalg.inv(self.vectorsAsArray))
+    def convert_point_to_fractional(self, point):
+        np_vector = np.array(self.vectors_as_array)
+        return np.dot(point, np.linalg.inv(np_vector))
 
-    def isPointInsideCell(self, point, tolerance=1):
-        fractional_point = self.convertPointToFractional(point)
-        return all(0 <= c <= 1 for c in fractional_point)
-
-    def getMostCollinearVectorIndex(self, testVector):
-        angles = [np.arccos(np.dot(v, testVector) / (np.linalg.norm(v) * np.linalg.norm(testVector))) for v in self.vectorsAsArray]
-        return angles.index(min(angles))
-
-    def scaleByMatrix(self, matrix):
-        self.vector1, self.vector2, self.vector3 = np.dot(matrix, self.vectorsAsArray).tolist()
-
-
+    def scale_by_matrix(self, matrix):
+        np_vector = np.array(self.vectors_as_array)
+        self.vector1, self.vector2, self.vector3 = np.dot(matrix, np_vector).tolist()
