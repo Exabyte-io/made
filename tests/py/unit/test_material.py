7076822c25b1d7b59e279491a6b39031c0453f1c
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-06-17 13:26:37 -0700

    update: material test

--- tests/py/unit/test_material.py
+++ tests/py/unit/test_material.py
@@ -1,3 +1,5 @@
+from mat3ra.made.basis.basis import Basis
+from mat3ra.made.lattice.lattice import Lattice
 from mat3ra.made.material import Material
 
 REFERENCE_OBJECT_1 = {"key1": "value1", "key2": "value2"}
@@ -5,5 +7,7 @@ REFERENCE_OBJECT_1 = {"key1": "value1", "key2": "value2"}
 
 def test_create():
     material = Material.create(Material.default_config)
+    assert isinstance(material.basis, Basis)
+    assert isinstance(material.lattice, Lattice)
     assert material.to_json() == Material.default_config
     assert material.name == Material.default_config["name"]
