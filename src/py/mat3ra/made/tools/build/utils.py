811b7ea92258cde45e309d7df027b7282b63d073
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-06-21 18:24:39 -0700

    update: cleanup

--- src/py/mat3ra/made/tools/build/utils.py
+++ src/py/mat3ra/made/tools/build/utils.py
@@ -12,7 +12,7 @@ def resolve_close_coordinates_basis(basis: Basis, distance_tolerance: float = 0.
     Find all atoms that are within distance tolerance and only keep the last one, remove other sites
     """
     coordinates = basis.coordinates.to_array_of_values_with_ids()
-    ids = set([coord.id for coord in coordinates])
+    ids = set(basis.coordinates.ids)
     ids_to_remove = set()
 
     for i in range(1, len(coordinates)):
@@ -25,7 +25,37 @@ def resolve_close_coordinates_basis(basis: Basis, distance_tolerance: float = 0.
     return basis
 
 
-def merge_two_materials(material1: Material, material2: Material, distance_tolerance: float = 0.01) -> Material:
+def merge_two_bases(basis1: Basis, basis2: Basis, distance_tolerance: float) -> Basis:
+    basis1 = convert_basis_to_crystal(basis1)
+    basis2 = convert_basis_to_crystal(basis2)
+
+    merged_elements = basis1.elements
+    merged_coordinates = basis1.coordinates
+    merged_labels = basis1.labels
+
+    for coordinate in basis2.coordinates.values:
+        merged_coordinates.add_item(coordinate)
+
+    for element in basis2.elements.values:
+        merged_elements.add_item(element)
+
+    if basis2.labels:
+        for label in basis2.labels.values:
+            merged_labels.add_item(label)
+
+    merged_basis = Basis(
+        elements=merged_elements,
+        coordinates=merged_coordinates,
+        units=basis1.units,
+        cell=basis1.cell,
+        labels=merged_labels,
+    )
+    resolved_basis = resolve_close_coordinates_basis(merged_basis, distance_tolerance)
+
+    return resolved_basis
+
+
+def merge_two_materials(material1: Material, material2: Material, distance_tolerance: float) -> Material:
     """
     Merge two materials with the same lattice into a single material,
     replacing colliding atoms with the latest material's atoms.
@@ -35,32 +65,8 @@ def merge_two_materials(material1: Material, material2: Material, distance_toler
     material2 = material2.clone()
     if material1.lattice != material2.lattice:
         raise ValueError("Lattices of the two materials must be the same.")
-    material1.basis = convert_basis_to_crystal(material1.basis)
-    material2.basis = convert_basis_to_crystal(material2.basis)
-
     merged_lattice = material1.lattice
-    merged_elements = material1.basis.elements
-    merged_coordinates = material1.basis.coordinates
-    merged_labels = material1.basis.labels
-
-    for coordinate in material2.basis.coordinates.values:
-        merged_coordinates.add_item(coordinate)
-
-    for element in material2.basis.elements.values:
-        merged_elements.add_item(element)
-
-    if material2.basis.labels:
-        for label in material2.basis.labels.values:
-            merged_labels.add_item(label)
-
-    merged_basis = Basis(
-        elements=merged_elements,
-        coordinates=merged_coordinates,
-        units=material1.basis.units,
-        cell=material1.basis.cell,
-        labels=merged_labels,
-    )
-    resolved_basis = resolve_close_coordinates_basis(merged_basis, distance_tolerance)
+    resolved_basis = merge_two_bases(material1.basis, material2.basis, distance_tolerance)
 
     name = "Merged Material"
     new_material = Material.create(
