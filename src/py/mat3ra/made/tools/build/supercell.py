b338b29da3fe6cae4860a4dcd0b3018f3c9b254f
VsevolodX <vsevolodbiryukov@icloud.com>
2024-06-28 18:25:09 -0400

    update: adjust more imports

--- src/py/mat3ra/made/tools/build/supercell.py
+++ src/py/mat3ra/made/tools/build/supercell.py
@@ -1,15 +1,14 @@
 from typing import List
-from ase import Atoms
-from ase.build.supercells import make_supercell
 
 from mat3ra.made.material import Material
+from ..third_party import ASEAtoms, ase_make_supercell
 from ..utils import decorator_convert_2x2_to_3x3
 from ..convert import from_ase, decorator_convert_material_args_kwargs_to_atoms
 
 
 @decorator_convert_2x2_to_3x3
 @decorator_convert_material_args_kwargs_to_atoms
-def create_supercell(atoms: Atoms, supercell_matrix: List[List[int]]) -> Material:
+def create_supercell(atoms: ASEAtoms, supercell_matrix: List[List[int]]) -> Material:
     """
     Create a supercell of the atoms.
 
@@ -21,5 +20,5 @@ def create_supercell(atoms: Atoms, supercell_matrix: List[List[int]]) -> Materia
         Material: The supercell of the atoms.
     """
 
-    supercell_atoms = make_supercell(atoms, supercell_matrix)
+    supercell_atoms = ase_make_supercell(atoms, supercell_matrix)
     return Material(from_ase(supercell_atoms))
--- src/py/mat3ra/made/tools/utils.py
+++ src/py/mat3ra/made/tools/utils.py
@@ -4,18 +4,19 @@ from typing import Callable, List
 import numpy as np
 from mat3ra.made.basis import Basis
 from mat3ra.utils.matrix import convert_2x2_to_3x3
-from pymatgen.core.structure import Structure
+
+from .third_party import PymatgenStructure
 
 
 # TODO: convert to accept ASE Atoms object
-def translate_to_bottom_pymatgen_structure(structure: Structure):
+def translate_to_bottom_pymatgen_structure(structure: PymatgenStructure):
     """
     Translate the structure to the bottom of the cell.
     Args:
-        structure (Structure): The pymatgen Structure object to translate.
+        structure (PymatgenStructure): The pymatgen Structure object to translate.
 
     Returns:
-        Structure: The translated pymatgen Structure object.
+        PymatgenStructure: The translated pymatgen Structure object.
     """
     min_c = min(site.c for site in structure)
     translation_vector = [0, 0, -min_c]
