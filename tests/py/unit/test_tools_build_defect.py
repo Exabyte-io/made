d05f1919e44b9ef170051a8c6f8145a0141bda37
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-07-09 20:47:53 -0700

    fix: tests on gha

--- tests/py/unit/test_tools_build_defect.py
+++ tests/py/unit/test_tools_build_defect.py
@@ -93,7 +93,7 @@ def test_create_adatom_equidistant():
     assert defect.basis.elements.values[-1] == "Si"
     # We expect adatom to shift from provided position
     assertion_utils.assert_deep_almost_equal(
-        [0.472222222, 0.472222222, 0.450843412], defect.basis.coordinates.values[-1]
+        [0.388888889, 0.638888889, 0.450843412], defect.basis.coordinates.values[-1]
     )
 
 
@@ -107,5 +107,5 @@ def test_create_crystal_site_adatom():
 
     assert defect.basis.elements.values[-1] == "Si"
     assertion_utils.assert_deep_almost_equal(
-        [0.972222222, 0.972222222, 0.352272727], defect.basis.coordinates.values[-1]
+        [0.72222222, 0.972222222, 0.352272727], defect.basis.coordinates.values[-1]
     )
