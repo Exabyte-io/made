15a99d0b192b20ba2d97aee6416c5d9c8e00a5ba
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-07-25 19:45:12 -0700

    update: rename to deformation

--- src/py/mat3ra/made/tools/build/deformation/builders.py
+++ src/py/mat3ra/made/tools/build/deformation/builders.py
@@ -3,22 +3,21 @@ from typing import List, Optional, Any
 from mat3ra.made.material import Material
 from mat3ra.made.tools.build import BaseBuilder
 
-from .configuration import PerturbationConfiguration
+from .configuration import DeformationConfiguration
 
 
-class PerturbationBuilder(BaseBuilder):
-    _ConfigurationType: type(PerturbationConfiguration) = PerturbationConfiguration  # type: ignore
+class DeformationBuilder(BaseBuilder):
+    _ConfigurationType: type(DeformationConfiguration) = DeformationConfiguration  # type: ignore
     _GeneratedItemType: Material = Material
 
     def _generate(self, configuration: _ConfigurationType) -> List[_GeneratedItemType]:
-        """Generate materials with applied perturbations based on the given configuration."""
+        """Generate materials with applied deformation based on the given configuration."""
         new_material = configuration.slab.clone()
         new_material.to_cartesian()
         new_coordinates = []
         for coord in new_material.basis.coordinates.values:
-            x, y, z = coord
-            perturbed_z = z + configuration.amplitude * configuration.perturbation_func(x, y)
-            new_coordinates.append([x, y, perturbed_z])
+            perturbed_coord = configuration.deformation_function[0](coord)
+            new_coordinates.append(perturbed_coord)
         new_basis = new_material.basis.copy()
         new_basis.coordinates.values = new_coordinates
         new_basis.to_crystal()
@@ -26,7 +25,6 @@ class PerturbationBuilder(BaseBuilder):
         return [new_material]
 
     def _update_material_name(self, material: Material, configuration: _ConfigurationType) -> Material:
-        """Update material name based on the perturbation details."""
-        perturbation_details = f"perturbed_amplitude_{configuration.amplitude}"
-        material.name = f"{material.name} ({perturbation_details})"
+        deformation_details = f"Deformation: {configuration.deformation_function[0].__name__}"
+        material.name = f"{material.name} ({deformation_details})"
         return material
---
+++ src/py/mat3ra/made/tools/build/deformation/configuration.py
@@ -0,0 +1,24 @@
+from typing import Callable, List, Dict, Tuple
+
+from mat3ra.code.entity import InMemoryEntity
+from mat3ra.made.material import Material
+from pydantic import BaseModel
+
+from ...utils import DeformationFunctionBuilder
+
+
+class DeformationConfiguration(BaseModel, InMemoryEntity):
+    slab: Material
+    deformation_function: Tuple[Callable[[List[float]], float], Dict] = DeformationFunctionBuilder().sine_wave()
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    @property
+    def _json(self):
+        deformation_function_json = self.deformation_function[1]
+        return {
+            "type": self.get_cls_name(),
+            "slab": self.slab.to_json(),
+            "deformation_function": deformation_function_json,
+        }
