7279daa38e9078aae241fed2542bd0ce97e53ddd
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-07-25 20:09:30 -0700

    update: prepare for continuos deformation

--- src/py/mat3ra/made/tools/build/deformation/builders.py
+++ src/py/mat3ra/made/tools/build/deformation/builders.py
@@ -10,8 +10,8 @@ class DeformationBuilder(BaseBuilder):
     _ConfigurationType: type(DeformationConfiguration) = DeformationConfiguration  # type: ignore
     _GeneratedItemType: Material = Material
 
-    def _generate(self, configuration: _ConfigurationType) -> List[_GeneratedItemType]:
-        """Generate materials with applied deformation based on the given configuration."""
+    @staticmethod
+    def deform_slab(configuration):
         new_material = configuration.slab.clone()
         new_material.to_cartesian()
         new_coordinates = []
@@ -22,9 +22,19 @@ class DeformationBuilder(BaseBuilder):
         new_basis.coordinates.values = new_coordinates
         new_basis.to_crystal()
         new_material.basis = new_basis
+        return new_material
+
+    def _generate(self, configuration: _ConfigurationType) -> List[_GeneratedItemType]:
+        """Generate materials with applied deformation based on the given configuration."""
+        new_material = self.deform_slab(configuration)
         return [new_material]
 
     def _update_material_name(self, material: Material, configuration: _ConfigurationType) -> Material:
         deformation_details = f"Deformation: {configuration.deformation_function[0].__name__}"
         material.name = f"{material.name} ({deformation_details})"
         return material
+
+
+class ContinuousDeformationBuilder(DeformationBuilder):
+    pass
+
