9edfab5cb9505fecc633eea7f857e99d405281e6
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-07-30 12:03:11 -0700

    update: explain

--- src/py/mat3ra/made/tools/build/defect/enums.py
+++ src/py/mat3ra/made/tools/build/defect/enums.py
@@ -6,7 +6,6 @@ class PointDefectTypeEnum(str, Enum):
     SUBSTITUTION = "substitution"
     INTERSTITIAL = "interstitial"
     ADATOM = "adatom"
-    PAIR = "pair"
 
 
 class SlabDefectTypeEnum(str, Enum):
@@ -14,8 +13,16 @@ class SlabDefectTypeEnum(str, Enum):
     TERRACE = "terrace"
 
 
+class ComplexDefectTypeEnum(str, Enum):
+    PAIR = "pair"
+
+
 class AtomPlacementMethodEnum(str, Enum):
+    # Places the atom at the exact given coordinate.
     COORDINATE = "coordinate"
+    # Among existing atoms, selects the closest one to the given coordinate.
     CLOSEST_SITE = "closest_site"
+    # Places the atom at the equal distance from the closest atoms to the given coordinate.
     EQUIDISTANT = "equidistant"
+    # Places the atom at the existing or extrapolated crystal site closest to the given coordinate.
     CRYSTAL_SITE = "crystal_site"
