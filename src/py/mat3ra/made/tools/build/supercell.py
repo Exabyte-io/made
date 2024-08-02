830140a5a981ece358f31d396afdab3cd33a51ad
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-08-02 16:17:40 -0700

    update: noticed and issue with name and metadata

--- src/py/mat3ra/made/tools/build/supercell.py
+++ src/py/mat3ra/made/tools/build/supercell.py
@@ -4,26 +4,30 @@ import numpy as np
 from mat3ra.made.material import Material
 from ..third_party import ASEAtoms, ase_make_supercell
 from ..utils import decorator_convert_2x2_to_3x3
-from ..convert import from_ase, decorator_convert_material_args_kwargs_to_atoms
+from ..convert import from_ase, decorator_convert_material_args_kwargs_to_atoms, to_ase
 
 
 @decorator_convert_2x2_to_3x3
-@decorator_convert_material_args_kwargs_to_atoms
 def create_supercell(
-    atoms: ASEAtoms, supercell_matrix: Optional[List[List[int]]] = None, scaling_factor: Optional[List[int]] = None
+    material: Material, supercell_matrix: Optional[List[List[int]]] = None, scaling_factor: Optional[List[int]] = None
 ) -> Material:
     """
     Create a supercell of the atoms.
 
     Args:
-        atoms (Material): The atoms to create a supercell of.
+        material (Material): The atoms to create a supercell of.
         supercell_matrix (List[List[int]]): The supercell matrix (e.g. [[3,0,0],[0,3,0],[0,0,1]]).
         scaling_factor (List[int], optional): The scaling factor instead of matrix (e.g. [3,3,1]).
 
     Returns:
         Material: The supercell of the atoms.
     """
+    atoms = to_ase(material)
     if scaling_factor is not None:
         supercell_matrix = np.multiply(scaling_factor, np.eye(3)).tolist()
     supercell_atoms = ase_make_supercell(atoms, supercell_matrix)
-    return Material(from_ase(supercell_atoms))
+    new_material = Material(from_ase(supercell_atoms))
+    if material.metadata:
+        new_material.metadata = material.metadata
+    new_material.name = material.name
+    return new_material
