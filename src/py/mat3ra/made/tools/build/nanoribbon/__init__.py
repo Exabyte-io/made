ac258182596fb7876c785daa45bb6db90d5c62c5
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-08-13 17:22:14 -0700

    update: wrap in a builder

--- src/py/mat3ra/made/tools/build/nanoribbon/__init__.py
+++ src/py/mat3ra/made/tools/build/nanoribbon/__init__.py
@@ -1,8 +1,9 @@
-from typing import Literal
+from typing import Literal, List, Optional, Any
 
 import numpy as np
+from mat3ra.made.tools.build import BaseBuilder
 from mat3ra.made.tools.build.supercell import create_supercell
-from mat3ra.made.tools.modify import filter_by_rectangle_projection
+from mat3ra.made.tools.modify import filter_by_rectangle_projection, wrap_to_unit_cell
 from pydantic import BaseModel
 
 from mat3ra.made.material import Material
@@ -18,19 +19,40 @@ class NanoribbonConfiguration(BaseModel):
         arbitrary_types_allowed = True
 
 
-def build_nanoribbon(config: NanoribbonConfiguration) -> Material:
-    n = max(config.width, config.length)
-    scaling_matrix = np.diag([n, n, 1])
-    supercell = create_supercell(config.material, scaling_matrix)
-    width_crystal = config.width / config.length
-    min_coordinate = [0, 0, 0]
-    max_coordinate = [1, width_crystal, 1]
-    if config.edge_type == "armchair":
-        rotation_matrix = [[1, -1, 0], [1, 1, 0], [0, 0, 1]]
-        supercell = create_supercell(supercell, supercell_matrix=rotation_matrix)
+class NanoribbonBuilder(BaseBuilder):
+    _ConfigurationType: type(NanoribbonConfiguration) = NanoribbonConfiguration  # type: ignore
+    _GeneratedItemType: Material = Material
+    _PostProcessParametersType: Any = None
 
-    nanoribbon = filter_by_rectangle_projection(
-        supercell, min_coordinate=min_coordinate, max_coordinate=max_coordinate, use_cartesian_coordinates=False
-    )
+    def build_nanoribbon(self, config: NanoribbonConfiguration) -> Material:
+        n = max(config.width, config.length)
+        scaling_matrix = np.diag([n, n, 1])
+        supercell = create_supercell(config.material, scaling_matrix)
+        width_crystal = config.width / config.length
+        min_coordinate = [0, 0, 0]
+        max_coordinate = [1, width_crystal, 1]
+        if config.edge_type == "armchair":
+            rotation_matrix = [[1, -1, 0], [1, 1, 0], [0, 0, 1]]
+            supercell = create_supercell(supercell, supercell_matrix=rotation_matrix)
 
-    return nanoribbon
+        nanoribbon = filter_by_rectangle_projection(
+            supercell, min_coordinate=min_coordinate, max_coordinate=max_coordinate, use_cartesian_coordinates=False
+        )
+
+        return nanoribbon
+
+    def _generate(self, configuration: NanoribbonConfiguration) -> List[_GeneratedItemType]:
+        nanoribbon = self.build_nanoribbon(configuration)
+        return [nanoribbon]
+
+    def _post_process(
+        self,
+        items: List[_GeneratedItemType],
+        post_process_parameters: Optional[_PostProcessParametersType],
+    ) -> List[Material]:
+        return [wrap_to_unit_cell(item) for item in items]
+
+    def _update_material_name(self, material: Material, configuration: NanoribbonConfiguration) -> Material:
+        edge_type = configuration.edge_type.capitalize()
+        material.name = f"{material.name} ({edge_type} nanoribbon)"
+        return material
