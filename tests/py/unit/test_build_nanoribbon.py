7786f68b6a689cf43c9fc4d055a7a41b719f2b58
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-08-19 17:12:35 -0700

    update: add a test for armchair

--- tests/py/unit/test_build_nanoribbon.py
+++ tests/py/unit/test_build_nanoribbon.py
@@ -2,10 +2,10 @@ from mat3ra.made.material import Material
 from mat3ra.made.tools.build.nanoribbon import NanoribbonConfiguration, build_nanoribbon
 from mat3ra.utils import assertion as assertion_utils
 
-from .fixtures import GRAPHENE, GRAPHENE_ZIGZAG_NANORIBBON
+from .fixtures import GRAPHENE, GRAPHENE_ZIGZAG_NANORIBBON, GRAPHENE_ARMCHAIR_NANORIBBON
 
 
-def test_build_nanoribbon():
+def test_build_zigzag_nanoribbon():
     config = NanoribbonConfiguration(
         material=Material(GRAPHENE),
         width=2,
@@ -15,5 +15,17 @@ def test_build_nanoribbon():
     )
 
     nanoribbon = build_nanoribbon(config)
-    print(nanoribbon.to_json())
     assertion_utils.assert_deep_almost_equal(GRAPHENE_ZIGZAG_NANORIBBON, nanoribbon.to_json())
+
+
+def test_build_armchair_nanoribbon():
+    config = NanoribbonConfiguration(
+        material=Material(GRAPHENE),
+        width=2,
+        length=4,
+        vacuum_width=3,
+        edge_type="armchair",
+    )
+
+    nanoribbon = build_nanoribbon(config)
+    assertion_utils.assert_deep_almost_equal(GRAPHENE_ARMCHAIR_NANORIBBON, nanoribbon.to_json())
