a6f3768fc72a40d6c0ee9655d493f350dbfd1219
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-08-19 17:06:35 -0700

    update: add a test for zigzag

---
+++ tests/py/unit/test_build_nanoribbon.py
@@ -0,0 +1,22 @@
+from mat3ra.made.material import Material
+from mat3ra.utils import assertion as assertion_utils
+
+from mat3ra.made.tools.build.nanoribbon import NanoribbonConfiguration
+
+from mat3ra.made.tools.build.nanoribbon import build_nanoribbon
+
+from .fixtures import GRAPHENE, GRAPHENE_ZIGZAG_NANORIBBON
+
+
+def test_build_nanoribbon():
+    config = NanoribbonConfiguration(
+        material=Material(GRAPHENE),
+        width=2,
+        length=4,
+        vacuum_width=3,
+        edge_type="zigzag",
+    )
+
+    nanoribbon = build_nanoribbon(config)
+    print(nanoribbon.to_json())
+    assertion_utils.assert_deep_almost_equal(GRAPHENE_ZIGZAG_NANORIBBON, nanoribbon.to_json())
