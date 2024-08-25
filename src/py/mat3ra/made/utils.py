8db314a4742ee656bcbeecc5d50b3ca91422b7c6
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-08-25 16:45:53 -0700

    feat: add get id by value

--- src/py/mat3ra/made/utils.py
+++ src/py/mat3ra/made/utils.py
@@ -94,6 +94,12 @@ class ArrayWithIds(RoundNumericValuesMixin, BaseModel):
     def get_element_value_by_index(self, index: int) -> Any:
         return self.values[index] if index < len(self.values) else None
 
+    def get_element_id_by_value(self, value: Any) -> Optional[int]:
+        try:
+            return self.ids[self.values.index(value)]
+        except ValueError:
+            return None
+
     def filter_by_values(self, values: Union[List[Any], Any]):
         def make_hashable(value):
             return tuple(value) if isinstance(value, list) else value
