ab9f2dcd4469137465ec51f135a031ba51d4c30d
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-08-26 10:46:22 -0700

    feat: add surface and edge configs

---
+++ src/py/mat3ra/made/tools/build/passivation/configuration.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+from mat3ra.made.material import Material
+from ...build import BaseConfiguration
+
+
+class PassivationConfiguration(BaseConfiguration):
+    """
+    Configuration for a passivation.
+
+    Args:
+        slab (Material): The Material object.
+        passivant (str): The passivant element.
+        bond_length (float): The bond length.
+    """
+
+    slab: Material
+    passivant: str = "H"
+    bond_length: float = 1.0
+
+    @property
+    def _json(self):
+        return {
+            "type": self.get_cls_name(),
+            "slab": self.slab.to_json(),
+            "passivant": self.passivant,
+            "bond_length": self.bond_length,
+        }
+
+
+class SurfacePassivationConfiguration(PassivationConfiguration):
+    """
+    Configuration for a passivation.
+    """
+
+    surface: SurfaceTypes = SurfaceTypes.BOTH
+
+
+class EdgePassivationConfiguration(PassivationConfiguration):
+    """
+    Configuration for a passivation.
+    """
+
+    edge: EdgeTypes = EdgeTypes.BOTH
