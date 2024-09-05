4a0d0bb81f0f1bab4792a1f081b5b9d94431eb02
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-09-05 12:46:38 -0700

    update: optimize performance

--- src/py/mat3ra/made/tools/build/utils.py
+++ src/py/mat3ra/made/tools/build/utils.py
@@ -1,26 +1,27 @@
+import numpy as np
+from scipy.spatial import cKDTree
 from typing import List, Optional
-
 from mat3ra.made.basis import Basis
 from mat3ra.made.material import Material
-
-from ..utils import get_distance_between_coordinates
+from ...utils import ArrayWithIds
 
 
 def resolve_close_coordinates_basis(basis: Basis, distance_tolerance: float = 0.01) -> Basis:
     """
     Find all atoms that are within distance tolerance and only keep the last one, remove other sites
     """
-    coordinates = basis.coordinates.to_array_of_values_with_ids()
-    ids = set(basis.coordinates.ids)
+    coordinates = np.array(basis.coordinates.values)
+    tree = cKDTree(coordinates)
+    colliding_pairs = tree.query_pairs(r=distance_tolerance)
+
     ids_to_remove = set()
+    if len(colliding_pairs) == 0:
+        return basis
+    for index_1, index_2 in colliding_pairs:
+        ids_to_remove.add(index_1)
 
-    for i in range(1, len(coordinates)):
-        for j in range(i):
-            if get_distance_between_coordinates(coordinates[i].value, coordinates[j].value) < distance_tolerance:
-                ids_to_remove.add(coordinates[j].id)
-
-    ids_to_keep = list(ids - ids_to_remove)
-    basis.filter_atoms_by_ids(ids_to_keep)
+    ids_to_keep = [id for id in basis.coordinates.ids if id not in ids_to_remove]
+    basis = basis.filter_atoms_by_ids(ids_to_keep)
     return basis
 
 
@@ -28,26 +29,16 @@ def merge_two_bases(basis1: Basis, basis2: Basis, distance_tolerance: float) -> 
     basis1.to_crystal()
     basis2.to_crystal()
 
-    merged_elements = basis1.elements
-    merged_coordinates = basis1.coordinates
-    merged_labels = basis1.labels
-
-    for coordinate in basis2.coordinates.values:
-        merged_coordinates.add_item(coordinate)
-
-    for element in basis2.elements.values:
-        merged_elements.add_item(element)
-
-    if basis2.labels:
-        for label in basis2.labels.values:
-            merged_labels.add_item(label)
+    merged_elements_values = basis1.elements.values + basis2.elements.values
+    merged_coordinates_values = basis1.coordinates.values + basis2.coordinates.values
+    merged_labels_values = basis1.labels.values + basis2.labels.values if basis1.labels and basis2.labels else []
 
     merged_basis = Basis(
-        elements=merged_elements,
-        coordinates=merged_coordinates,
+        elements=ArrayWithIds.from_values(values=merged_elements_values),
+        coordinates=ArrayWithIds.from_values(values=merged_coordinates_values),
         units=basis1.units,
         cell=basis1.cell,
-        labels=merged_labels,
+        labels=ArrayWithIds.from_values(values=merged_labels_values),
     )
     resolved_basis = resolve_close_coordinates_basis(merged_basis, distance_tolerance)
 
@@ -61,20 +52,6 @@ def merge_two_materials(
     distance_tolerance: float,
     merge_dangerously=False,
 ) -> Material:
-    """
-    Merge two materials with the same lattice into a single material,
-    replacing colliding atoms with the latest material's atoms.
-
-    Args:
-        material1 (Material): First material to merge.
-        material2 (Material): Second material to merge.
-        material_name (str): Name of the merged material.
-        distance_tolerance (float): Distance tolerance for merging atoms.
-        merge_dangerously (bool): If True, merge materials with different lattices.
-    """
-
-    material1 = material1.clone()
-    material2 = material2.clone()
     if material1.lattice != material2.lattice and not merge_dangerously:
         raise ValueError("Lattices of the two materials must be the same.")
     merged_lattice = material1.lattice
