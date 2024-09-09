13d85382215972d3f82a08c685982f4e13a90638
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-09-09 11:35:36 -0700

    update: add top level function to create passivation

---
+++ src/py/mat3ra/made/tools/build/passivation/__init__.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+from mat3ra.made.material import Material
+from .configuration import PassivationConfiguration
+from .builders import PassivationBuilder, SurfacePassivationBuilder, UndercoordinationPassivationBuilder
+
+
+def create_passivation(
+    configuration: PassivationConfiguration,
+    builder: Union[SurfacePassivationBuilder, UndercoordinationPassivationBuilder] = SurfacePassivationBuilder(),
+) -> Material:
+    return builder.get_material(configuration)
