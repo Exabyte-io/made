72c9bdd7f2dde68bdc43f51368fc5d4151278c46
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-09-25 16:13:03 -0700

    chore: docstring 2

--- src/py/mat3ra/made/tools/build/grain_boundary/__init__.py
+++ src/py/mat3ra/made/tools/build/grain_boundary/__init__.py
@@ -10,6 +10,15 @@ def create_grain_boundary(
     configuration: GrainBoundaryConfiguration,
     builder: Optional[GrainBoundaryBuilder] = None,
 ) -> Material:
+    """
+    Create a grain boundary according to provided configuration with selected builder.
+    Args:
+        configuration (GrainBoundaryConfiguration): The configuration of the grain boundary.
+        builder (Optional[GrainBoundaryBuilder]): The builder to use for creating the grain boundary.
+    Returns:
+        Material: The material with the grain boundary.
+
+    """
     if builder is None:
         builder = GrainBoundaryBuilder()
     return builder.get_material(configuration)
