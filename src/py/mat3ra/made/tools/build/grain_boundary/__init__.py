6f5d7dba3d12ea03efe9edaf5d9758a17508b09c
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-09-27 20:21:33 -0700

    update: add top level function to create grain boundary

--- src/py/mat3ra/made/tools/build/grain_boundary/__init__.py
+++ src/py/mat3ra/made/tools/build/grain_boundary/__init__.py
@@ -1,2 +1,27 @@
-from .configuration import SurfaceGrainBoundaryConfiguration
-from .builders import SurfaceGrainBoundaryBuilder, SurfaceGrainBoundaryBuilderParameters
+from typing import Union
+
+from .configuration import SurfaceGrainBoundaryConfiguration, SlabGrainBoundaryConfiguration
+from .builders import (
+    SurfaceGrainBoundaryBuilder,
+    SurfaceGrainBoundaryBuilderParameters,
+    SlabGrainBoundaryBuilder,
+    SlabGrainBoundaryBuilderParameters,
+)
+
+
+def create_grain_boundary(
+    configuration: Union[SurfaceGrainBoundaryConfiguration, SlabGrainBoundaryConfiguration],
+    builder_parameters: Union[SurfaceGrainBoundaryBuilderParameters, SlabGrainBoundaryBuilderParameters, None] = None,
+):
+    """
+    Create a grain boundary between two surface phases.
+
+    Args:
+        configuration: The configuration of the grain boundary to be created.
+        builder_parameters: The parameters to be used by the grain boundary builder.
+
+    Returns:
+        The material with the grain boundary added.
+    """
+    builder = SurfaceGrainBoundaryBuilder(build_parameters=builder_parameters)
+    return builder.get_materials(configuration)
