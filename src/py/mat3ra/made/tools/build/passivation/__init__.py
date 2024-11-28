d39273a8c01b98145f3771f16aca90244a712a02
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-11-27 17:31:22 -0800

    update: add ability to pass builder params to function

--- src/py/mat3ra/made/tools/build/passivation/__init__.py
+++ src/py/mat3ra/made/tools/build/passivation/__init__.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Optional
 
 from mat3ra.made.material import Material
 from .configuration import PassivationConfiguration
@@ -6,6 +6,7 @@ from .builders import (
     SurfacePassivationBuilder,
     CoordinationBasedPassivationBuilder,
     SurfacePassivationBuilderParameters,
+    CoordinationBasedPassivationBuilderParameters,
 )
 
 
@@ -18,8 +19,19 @@ def create_passivation(
     return builder.get_material(configuration)
 
 
-def get_unique_coordination_numbers(configuration: PassivationConfiguration) -> set:
+def get_unique_coordination_numbers(
+    configuration: PassivationConfiguration,
+    builder_parameters: Optional[CoordinationBasedPassivationBuilderParameters] = None,
+) -> set:
     """
     Get the unique coordination numbers for the provided configuration as a set type.
+
+    Args:
+        configuration (PassivationConfiguration): The configuration object.
+        builder_parameters (CoordinationBasedPassivationBuilderParameters): The builder parameters.
     """
-    return CoordinationBasedPassivationBuilder().get_unique_coordination_numbers(material=configuration.slab)
+    if builder_parameters is None:
+        builder_parameters = CoordinationBasedPassivationBuilderParameters()
+    return CoordinationBasedPassivationBuilder(builder_parameters).get_unique_coordination_numbers(
+        material=configuration.slab
+    )
