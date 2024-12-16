4de1a60e365201180a5a68bd0302bf5e1a6c552a
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-12-15 18:16:31 -0800

    update: add cutting nanoparticle builder

--- src/py/mat3ra/made/tools/build/nanoparticle/builders.py
+++ src/py/mat3ra/made/tools/build/nanoparticle/builders.py
@@ -9,24 +9,59 @@ from ase.cluster import (
     HexagonalClosedPacked,
 )
 from ase.cluster.wulff import wulff_construction
+from mat3ra.made.material import Material
 from mat3ra.made.tools.build import BaseBuilder
 from mat3ra.made.tools.build.mixins import ConvertGeneratedItemsASEAtomsMixin
+from mat3ra.made.tools.build.slab import SlabConfiguration
+from mat3ra.made.tools.modify import filter_by_condition_on_coordinates
 
-from .configuration import NanoparticleConfiguration
+from .configuration import ASENanoparticleConfiguration, NanoparticleConfiguration
 from .enums import NanoparticleShapes
+from ..slab import create_slab
 from ...third_party import ASEAtoms
 
 
 class NanoparticleBuilder(ConvertGeneratedItemsASEAtomsMixin, BaseBuilder):
     """
+    Builder for creating nanoparticles by cutting from bulk materials supercells.
+    """
+
+    _ConfigurationType: type(NanoparticleConfiguration) = NanoparticleConfiguration  # type: ignore
+    _GeneratedItemType: type(Material) = Material  # type: ignore
+
+    def create_nanoparticle(self, config: _ConfigurationType) -> _GeneratedItemType:
+        material = config.material
+        # shape = config.shape
+        orientation_z = config.orientation_z
+        radius = config.radius
+
+        # Get the conventional structure
+        conventional_material = self._ConfigurationType.convert_to_conventional(material)
+        thickness_in_layers = (2 * radius + config.vacuum_padding) / conventional_material.lattice_constant
+        slab_config = SlabConfiguration(
+            bulk=conventional_material.structure,
+            miller_indices=orientation_z,
+            thickness=thickness_in_layers,
+            use_conventional_cell=True,
+            use_orthogonal_z=True,
+            make_primitive=False,
+        )
+        slab = create_slab(slab_config)
+        # TODO: switch condition based on shape
+        nanoparticle = filter_by_condition_on_coordinates(slab, lambda vector: vector.norm() <= radius)
+        return nanoparticle
+
+
+class ASEBasedNanoparticleBuilder(ConvertGeneratedItemsASEAtomsMixin, NanoparticleBuilder):
+    """
     Generalized builder for creating nanoparticles based on ASE cluster tools.
     Passes configuration parameters directly to the ASE constructors.
     """
 
-    _ConfigurationType: type(NanoparticleConfiguration) = NanoparticleConfiguration  # type: ignore
+    _ConfigurationType: type(ASENanoparticleConfiguration) = ASENanoparticleConfiguration  # type: ignore
     _GeneratedItemType: type(ASEAtoms) = ASEAtoms  # type: ignore
 
-    def create_nanoparticle(self, config: NanoparticleConfiguration) -> _GeneratedItemType:
+    def create_nanoparticle(self, config: ASENanoparticleConfiguration) -> _GeneratedItemType:
         shape = config.shape
         element = config.element
 
@@ -39,7 +74,7 @@ class NanoparticleBuilder(ConvertGeneratedItemsASEAtomsMixin, BaseBuilder):
         parameters["symbol"] = element
         if "latticeconstant" not in parameters:
             parameters["latticeconstant"] = lattice_constant
-
+        # TODO: adjust parameters for octahedron based on type (cuboctahedron, regular octahedron, etc.)
         # Shape-specific factory logic
         if shape == NanoparticleShapes.CUBOCTAHEDRON:
             nanoparticle = FaceCenteredCubic(**parameters)
@@ -64,6 +99,6 @@ class NanoparticleBuilder(ConvertGeneratedItemsASEAtomsMixin, BaseBuilder):
         nanoparticle.center()
         return nanoparticle
 
-    def _generate(self, configuration: NanoparticleConfiguration) -> List[_GeneratedItemType]:
+    def _generate(self, configuration: ASENanoparticleConfiguration) -> List[_GeneratedItemType]:
         nanoparticle = self.create_nanoparticle(configuration)
         return [nanoparticle]
