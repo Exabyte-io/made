7ea6f2ce74de1a180c798e8e4938e89ecda41b38
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-12-24 14:44:08 -0800

    chore: slab with 8 atoms

--- tests/py/unit/test_tools_build_slab.py
+++ tests/py/unit/test_tools_build_slab.py
@@ -1,5 +1,10 @@
 from mat3ra.made.material import Material
-from mat3ra.made.tools.build.slab import SlabConfiguration, create_slab, get_terminations
+from mat3ra.made.tools.build.slab import (
+    PymatgenSlabGeneratorParameters,
+    SlabConfiguration,
+    create_slab,
+    get_terminations,
+)
 from mat3ra.utils import assertion as assertion_utils
 
 from .fixtures import SI_SLAB
@@ -11,12 +16,16 @@ def test_build_slab():
     slab_config = SlabConfiguration(
         bulk=material,
         miller_indices=(0, 0, 1),
-        thickness=1,
+        thickness=2,
         vacuum=5.0,
         xy_supercell_matrix=[[1, 0], [0, 1]],
         use_orthogonal_z=True,
+        use_conventional_cell=True,
         make_primitive=True,
     )
-    termination = get_terminations(slab_config)[0]
-    slab = create_slab(slab_config, termination)
+    params = PymatgenSlabGeneratorParameters(
+        min_vacuum_size=1, in_unit_planes=True, reorient_lattice=True, symmetrize=True
+    )
+    termination = get_terminations(slab_config, build_parameters=params)[0]
+    slab = create_slab(slab_config, termination, params)
     assertion_utils.assert_deep_almost_equal(SI_SLAB, slab.to_json())
