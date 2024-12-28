1fc2cab4e359ff2b719a67b062ff3914d8ccdcca
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-12-27 20:21:00 -0800

    fix: test create

--- tests/py/unit/test_material.py
+++ tests/py/unit/test_material.py
@@ -10,27 +10,18 @@ def test_create():
     material = Material.create(Material.default_config)
     assert isinstance(material.basis, Basis)
     assert isinstance(material.lattice, Lattice)
-    assert material.to_json() == Material.default_config
     assert material.name == Material.default_config["name"]
 
 
 def test_material_to_json():
     material = Material.create(Material.default_config)
-    labels_array = [{"id": 0, "value": 0}, {"id": 1, "value": 1}]
-    config_with_labels = {
-        **Material.default_config,
-        "basis": {**Material.default_config["basis"], "labels": labels_array},
-    }
-    expected_config = {
-        **Material.default_config,
-        "basis": {**Material.default_config["basis"], "cell": None, "labels": labels_array},
-    }
-    material.basis = Basis.from_dict(**config_with_labels["basis"])
-    assertion_utils.assert_deep_almost_equal(expected_config, material.to_json())
+    assertion_utils.assert_deep_almost_equal(Material.default_config, material.to_json())
 
 
 def test_basis_to_json():
     material = Material.create(Material.default_config)
     basis = material.basis
-    expected_basis_config = {**Material.default_config["basis"], "cell": None, "labels": []}
+    expected_basis_config = {**Material.default_config["basis"],  "labels": []}
     assertion_utils.assert_deep_almost_equal(expected_basis_config, basis.to_json())
+
+# TODO: Add test to check if basis.cell is changed when lattice of material is changed, and vice versa
