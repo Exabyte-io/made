0c02d04cb1ca5fb66bb5c71454deafd2bcc89dcb
VsevolodX <vsevolodbiryukov@icloud.com>
2025-01-10 19:07:10 -0800

    update: remove unused

--- src/py/mat3ra/made/tools/utils/__init__.py
+++ src/py/mat3ra/made/tools/utils/__init__.py
@@ -9,25 +9,6 @@ from ..third_party import PymatgenStructure
 DEFAULT_SCALING_FACTOR = np.array([3, 3, 3])
 DEFAULT_TRANSLATION_VECTOR = 1 / DEFAULT_SCALING_FACTOR
 
-
-# TODO: convert to accept ASE Atoms object
-def translate_to_bottom_pymatgen_structure(structure: PymatgenStructure):
-    """
-    Translate the structure to the bottom of the cell.
-    Args:
-        structure (PymatgenStructure): The pymatgen Structure object to translate.
-
-    Returns:
-        PymatgenStructure: The translated pymatgen Structure object.
-    """
-    min_c = min(site.c for site in structure)
-    translation_vector = [0, 0, -min_c]
-    translated_structure = structure.copy()
-    for site in translated_structure:
-        site.coords += translation_vector
-    return translated_structure
-
-
 def decorator_convert_2x2_to_3x3(func: Callable) -> Callable:
     """
     Decorator to convert a 2x2 matrix to a 3x3 matrix.
