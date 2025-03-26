d451d9419cc5523d13d8c099db9790934438b08a
VsevolodX <vsevolodbiryukov@icloud.com>
2025-03-26 12:29:55 -0700

    wip: use slab cinfig schema

--- src/py/mat3ra/made/tools/build/slab/configuration.py
+++ src/py/mat3ra/made/tools/build/slab/configuration.py
@@ -1,6 +1,9 @@
 from typing import List, Tuple
 
 import numpy as np
+from mat3ra.esse.models.materials_category.single_material.two_dimensional.slab.configuration import (
+    SlabConfigurationSchema,
+)
 
 from mat3ra.made.material import Material
 
@@ -9,40 +12,17 @@ from ...third_party import PymatgenSpacegroupAnalyzer
 from ...convert import to_pymatgen, from_pymatgen
 
 
-class SlabConfiguration(BaseConfiguration):
-    """
-    Configuration for building a slab.
-
-    Args:
-        bulk (Material): The bulk material.
-        miller_indices (Tuple[int, int, int]): The Miller indices of the slab.
-        thickness (int): The thickness of the slab.
-        vacuum (float): The vacuum thickness, in Angstroms.
-        xy_supercell_matrix (List[List[int]]): The supercell matrix for the xy plane.
-        use_conventional_cell (bool): Whether to use the conventional cell.
-        use_orthogonal_z (bool): Whether to use orthogonal z.
-        make_primitive (bool): Whether to try to find primitive cell for the created slab.
-    """
-
-    bulk: Material
-    miller_indices: Tuple[int, int, int] = (0, 0, 1)
-    thickness: int = 1
-    vacuum: float = 5.0
-    xy_supercell_matrix: List[List[int]] = np.eye(2).tolist()
-    use_conventional_cell: bool = True
-    use_orthogonal_z: bool = False
-    make_primitive: bool = False
-
+class SlabConfiguration(BaseConfiguration, SlabConfigurationSchema):
     def __init__(
         self,
-        bulk=None,
-        miller_indices=miller_indices,
-        thickness=thickness,
-        vacuum=vacuum,
-        xy_supercell_matrix=None,
-        use_conventional_cell=use_conventional_cell,
-        use_orthogonal_z=use_orthogonal_z,
-        make_primitive=make_primitive,
+        bulk=SlabConfigurationSchema.bulk,
+        miller_indices=SlabConfigurationSchema.miller_indices,
+        thickness=SlabConfigurationSchema.thickness,
+        vacuum=SlabConfigurationSchema.vacuum,
+        xy_supercell_matrix=SlabConfigurationSchema.xy_supercell_matrix,
+        use_conventional_cell=SlabConfigurationSchema.use_conventional_cell,
+        use_orthogonal_z=SlabConfigurationSchema.use_orthogonal_z,
+        make_primitive=SlabConfigurationSchema.make_primitive,
     ):
         if xy_supercell_matrix is None:
             xy_supercell_matrix = np.eye(2).tolist()
