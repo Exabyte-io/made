b98ab337b651985b3c52660420e71c9ee026c0ce
VsevolodX <vsevolodbiryukov@icloud.com>
2025-03-31 14:28:14 -0700

    update: add assertion with diff

--- tests/py/unit/utils.py
+++ tests/py/unit/utils.py
@@ -1,9 +1,10 @@
+import difflib
 import json
-from typing import Dict
+from typing import Any, Dict
 
+from mat3ra.utils import assertion as assertion_utils
 from pymatgen.core.structure import Structure
 from pymatgen.io.ase import AseAtomsAdaptor
-from mat3ra.utils import assertion as assertion_utils
 
 ATOMS_TAGS_TO_INTERFACE_STRUCTURE_LABELS: Dict = {1: "substrate", 2: "film"}
 INTERFACE_STRUCTURE_LABELS_TO_ATOMS_TAGS: Dict = {v: k for k, v in ATOMS_TAGS_TO_INTERFACE_STRUCTURE_LABELS.items()}
@@ -27,12 +28,48 @@ def atoms_to_interface_structure(atoms) -> Structure:
     return interface_structure
 
 
+def prune_extra_keys(data: Any, reference: Any) -> Any:
+    if isinstance(data, dict) and isinstance(reference, dict):
+        return {key: prune_extra_keys(data[key], reference[key]) for key in data if key in reference}
+    elif isinstance(data, list) and isinstance(reference, list):
+        return [prune_extra_keys(d_item, r_item) for d_item, r_item in zip(data, reference)]
+    else:
+        return data
+
+
+def sort_dict_recursively(d):
+    """Sort dictionary keys recursively."""
+    if isinstance(d, dict):
+        return {k: sort_dict_recursively(v) for k, v in sorted(d.items())}
+    elif isinstance(d, list):
+        return [sort_dict_recursively(x) for x in d]
+    else:
+        return d
+
+
+def show_difference(expected_data, actual_data):
+    diff = difflib.ndiff(
+        json.dumps(expected_data, indent=4).splitlines(),
+        json.dumps(actual_data, indent=4).splitlines(),
+    )
+    diff_str = "\n".join(diff)
+    if diff_str:
+        raise AssertionError(f"Entities differ:\n{diff_str}")
+
+
 def assert_two_entities_deep_almost_equal(entity1, entity2):
-    dict_1 = json.loads(entity1.to_json())
-    dict_1_copy = dict_1.copy()
-    for key in dict_1:
-        if key not in entity2:
-            dict_1_copy.pop(key)
-    actual_data = json.dumps(dict_1_copy)
-    expected_data = json.dumps(entity2)
+    dict_1 = entity1 if isinstance(entity1, dict) else json.loads(entity1.to_json())
+    dict_2 = entity2 if isinstance(entity2, dict) else json.loads(entity2.to_json())
+
+    cleaned_dict_1 = prune_extra_keys(dict_1, dict_2)
+
+    sorted_dict_1 = sort_dict_recursively(cleaned_dict_1)
+    sorted_dict_2 = sort_dict_recursively(dict_2)
+
+    actual_data = json.loads(json.dumps(sorted_dict_1))
+    expected_data = json.loads(json.dumps(sorted_dict_2))
+
+    if actual_data != expected_data:
+        show_difference(expected_data, actual_data)
+
     assertion_utils.assert_deep_almost_equal(expected_data, actual_data)
