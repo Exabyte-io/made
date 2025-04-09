1c7fb2ade365ceb8c74fd7a2f038e565c55fcac1
VsevolodX <vsevolodbiryukov@icloud.com>
2025-04-08 19:58:34 -0700

    update: compare Materials of configs

--- tests/js/tools/surface.js
+++ tests/js/tools/surface.js
@@ -1,23 +1,27 @@
 import { Material } from "../../../src/js/material";
 import tools from "../../../src/js/tools";
 import SiSlab100 from "../../fixtures/si-slab-100.json";
-import SiSlab111 from "../../fixtures/si-slab-111-0.5-vacuum.json";
+import SiSlab111 from "../../fixtures/si-slab-111-0.5-vacuum-ratio.json";
 import SiSlab111NoVacuum from "../../fixtures/si-slab-111-0-vacuum.json";
 import Si from "../../fixtures/si-standata.json";
 import { assertDeepAlmostEqual } from "../utils";
 
 describe("Tools:Surface", () => {
-    it("should return slab", () => {
+    it("should return slab (100)", () => {
         const material = new Material(Si);
         const slabConfig = tools.surface.generateConfig(material, [1, 0, 0], 3, 1, 1);
         assertDeepAlmostEqual(SiSlab100, slabConfig);
     });
 
-    it("should return slab", () => {
+    it("should return slab (111)", () => {
         const material = new Material(Si);
         const slabConfig = tools.surface.generateConfig(material, [1, 1, 1], 3, 1, 1);
-
         assertDeepAlmostEqual(SiSlab111NoVacuum, slabConfig);
+    });
+
+    it("should return slab (111) with vacuum", () => {
+        const material = new Material(Si);
+        const slabConfig = tools.surface.generateConfig(material, [1, 1, 1], 3, 1, 1);
         const vacuumRatio = 0.5;
         const slabMaterial = new Material(slabConfig);
         const { outOfPlaneAxisIndex } = slabConfig;
@@ -27,6 +31,7 @@ describe("Tools:Surface", () => {
             ["a", "b", "c"][outOfPlaneAxisIndex],
             1 / (1 - vacuumRatio),
         );
-        assertDeepAlmostEqual(SiSlab111, slabMaterial.toJSON());
+        const expectedSlabMaterial = new Material(SiSlab111);
+        assertDeepAlmostEqual(expectedSlabMaterial.toJSON(), slabMaterial.toJSON());
     });
 });
