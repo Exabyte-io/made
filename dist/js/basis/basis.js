39d12d92340701f28cad1745503a920811cecf93
VsevolodX <vsevolodbiryukov@icloud.com>
2025-04-22 18:59:39 -0700

    chore: remove underscore

--- dist/js/basis/basis.js
+++ dist/js/basis/basis.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -31,7 +8,6 @@ exports.Basis = void 0;
 const periodic_table_js_1 = require("@exabyte-io/periodic-table.js");
 const entity_1 = require("@mat3ra/code/dist/js/entity");
 const lodash_1 = require("lodash");
-const s = __importStar(require("underscore.string"));
 const cell_1 = require("../cell/cell");
 const constants_1 = require("../constants");
 const lattice_1 = require("../lattice/lattice");
@@ -351,7 +327,7 @@ class Basis extends entity_1.InMemoryEntity {
             const coordinate = entry[1];
             const atomicLabel = this.atomicLabelsArray[idx];
             return `${element}${atomicLabel} ${coordinate.value
-                .map((x) => s.sprintf("%14.9f", x).trim())
+                .map((x) => (0, lodash_1.padStart)(x.toFixed(9), 14).trim())
                 .join(" ")}`;
         });
     }
