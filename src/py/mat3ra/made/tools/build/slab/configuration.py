574396b783558870e6484a45f44cb8212083a1fe
VsevolodX <vsevolodbiryukov@icloud.com>
2025-05-22 19:49:52 -0700

    wip: slab configurations

--- src/py/mat3ra/made/tools/build/slab/configuration.py
+++ src/py/mat3ra/made/tools/build/slab/configuration.py
@@ -1,6 +1,12 @@
 from typing import List, Union
 
 from mat3ra.esse.models.materials_category.pristine_structures.two_dimensional.atomic_layers import AtomicLayersSchema
+from mat3ra.esse.models.materials_category.pristine_structures.two_dimensional.atomic_layers_unique import (
+    AtomicLayersUniqueSchema,
+)
+from mat3ra.esse.models.materials_category.pristine_structures.two_dimensional.atomic_layers_unique_repeated import (
+    AtomicLayersUniqueRepeatedSchema,
+)
 from mat3ra.esse.models.materials_category.pristine_structures.two_dimensional.crystal_lattice_planes import (
     CrystalLatticePlanesSchema,
 )
@@ -9,21 +15,60 @@ from mat3ra.esse.models.materials_category.pristine_structures.two_dimensional.s
     VacuumSchema,
     AxisEnum,
 )
-
-
+from ...third_party import PymatgenSlabGenerator, label_pymatgen_slab_termination
 from mat3ra.made.material import Material
 from .termination import Termination
 from .. import BaseConfigurationPydantic
 from ...convert import to_pymatgen, from_pymatgen
-from ...third_party import PymatgenSpacegroupAnalyzer
+from ...third_party import PymatgenSlab, PymatgenSpacegroupAnalyzer
 
 
 class CrystalLatticePlanes(CrystalLatticePlanesSchema):
+
     crystal: Material
-    miller_indices: List[int] = [1, 1, 1]
+    _pymatgen_slabs: List[PymatgenSlab]
+
+    def __init__(self, **kwargs):
+        crystal = kwargs.pop("crystal", None) or Material.create_default()
+        use_conventional = kwargs.get(
+            "use_conventional_cell", CrystalLatticePlanesSchema.model_fields["use_conventional_cell"].default
+        )
+
+        # TODO: use LatticeAnalyzer to get the conventional cell
+        pymatgen_structure = to_pymatgen(crystal)
+        if use_conventional:
+            pymatgen_structure = PymatgenSpacegroupAnalyzer(pymatgen_structure).get_conventional_standard_structure()
+
+        crystal_config = from_pymatgen(pymatgen_structure)
+        kwargs["crystal"] = Material.create(crystal_config)
+        super().__init__(**kwargs)
+        self._pymatgen_slabs = self._get_pymatgen_slabs(crystal)
+
+    # TODO: add specific parameters
+    def _generate_pymatgen_slabs(self, symmetrize) -> List[PymatgenSlab]:  # type: ignore
+        generator = PymatgenSlabGenerator(
+            initial_structure=to_pymatgen(self.crystal),
+            miller_index=self.miller_indices,
+            min_slab_size=1,  # TODO: change
+            min_vacuum_size=1,  # TODO: change
+            in_unit_planes=True,
+            primitive=False,
+        )
+        raw_slabs = generator.get_slabs(
+            # We need to preserve symmetric slabs for different terminations at the surface
+            symmetrize=symmetrize
+        )
+
+        return raw_slabs
+
+    def get_terminations(self):
+        """
+        Get the terminations for the slab.
+        """
+        return [Termination.from_string(label_pymatgen_slab_termination(slab)) for slab in self._pymatgen_slabs]
 
 
-# termination = CrystalLatticePlanesSchema.termination
+# termination = CrystalLatticePlanesSchema.get_terminations()
 
 
 class AtomicLayers(AtomicLayersSchema, CrystalLatticePlanes):
@@ -31,40 +76,21 @@ class AtomicLayers(AtomicLayersSchema, CrystalLatticePlanes):
     pass
 
 
+class AtomicLayersUnique(AtomicLayersUniqueSchema, CrystalLatticePlanes):
+    # terminations: List[Termination]
+    pass
+
+
+class AtomicLayersUniqueRepeated(AtomicLayersUniqueRepeatedSchema, CrystalLatticePlanes):
+    termination_top: Termination
+
+
 class VacuumConfiguration(VacuumSchema):
-    direction: float
-    size: float
+    pass
 
 
 class SlabConfiguration(SlabSchema, BaseConfigurationPydantic):
-    """
-    Configuration for building a slab.
-
-    Args:
-        bulk (Material): The bulk material.
-        miller_indices (Tuple[int, int, int]): The Miller indices of the slab.
-        thickness (int): The thickness of the slab.
-        vacuum (float): The vacuum thickness, in Angstroms.
-        xy_supercell_matrix (List[List[int]]): The supercell matrix for the xy plane.
-        use_conventional_cell (bool): Whether to use the conventional cell.
-        use_orthogonal_z (bool): Whether to use orthogonal z.
-        make_primitive (bool): Whether to try to find primitive cell for the created slab.
-    """
-
     type: str = "SlabConfiguration"
-    stack_components: List[Union[AtomicLayers, VacuumConfiguration]]
+    stack_components: List[Union[AtomicLayersUniqueRepeated, VacuumConfiguration]]
     supercell_matrix: List[List[int]]
-    direction: AxisEnum = "z"
-
-    def __init__(self, **kwargs):
-        bulk = kwargs.pop("bulk", None) or Material.create_default()
-        use_conventional = kwargs.get("use_conventional_cell", True)
-
-        bulk_pmg = to_pymatgen(bulk)
-        if use_conventional:
-            bulk_pmg = PymatgenSpacegroupAnalyzer(bulk_pmg).get_conventional_standard_structure()
-
-        bulk_config = from_pymatgen(bulk_pmg)
-        kwargs["bulk"] = Material.create(bulk_config)
-
-        super().__init__(**kwargs)
+    direction: AxisEnum = AxisEnum.z
