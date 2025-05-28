f1c49aad121cee4c02fe848c1a49368a9710d852
VsevolodX <vsevolodbiryukov@icloud.com>
2025-05-27 21:47:28 -0700

    chore: address pr comments

--- tests/py/unit/test_tools_build_slab.py
+++ tests/py/unit/test_tools_build_slab.py
@@ -16,28 +16,8 @@ from unit.fixtures.slab import SI_SLAB_100, SI_SLAB_DEFAULT_PARAMETERS
 
 from .utils import assert_two_entities_deep_almost_equal
 
-# Create a default material with correct configuration
-material_config = {
-    "name": "Silicon FCC",
-    "lattice": {
-        "a": 3.867,
-        "b": 3.867,
-        "c": 3.867,
-        "alpha": 60.0,
-        "beta": 60.0,
-        "gamma": 60.0,
-        "units": {"length": "angstrom", "angle": "degree"},
-        "type": "FCC",
-    },
-    "basis": {
-        "elements": [{"id": 0, "value": "Si"}, {"id": 1, "value": "Si"}],
-        "coordinates": [{"id": 0, "value": [0.0, 0.0, 0.0]}, {"id": 1, "value": [0.25, 0.25, 0.25]}],
-        "units": "crystal",
-        "constraints": [],
-    },
-}
 
-material = Material.create(material_config)
+material = Material.create_default()
 
 
 def test_get_terminations():
@@ -51,6 +31,7 @@ def test_get_terminations():
 def test_build_slab():
     MILLER_INDICES = [0, 0, 1]
     USE_CONVENTIONAL_CELL = True
+
     crystal_lattice_planes = CrystalLatticePlanes(
         crystal=material, miller_indices=MILLER_INDICES, use_conventional_cell=USE_CONVENTIONAL_CELL
     )
@@ -65,24 +46,29 @@ def test_build_slab():
         number_of_repetitions=2,
         termination_top=terminations[0],
     )
-    # atomic_layers = AtomicLayersUniqueRepeated.from_parameters(
-    #     crystal_lattice_planes=crystal_lattice_planes
-    #     number_of_repetitions=2,
-    #     termination_top=terminations[0],
-    # )
-
     vacuum = VacuumConfiguration(direction=AxisEnum.z, size=5.0)
-
     slab_config = SlabConfiguration(
         supercell_xy=[[1, 0], [0, 1]], stack_components=[atomic_layers, vacuum], direction=AxisEnum.z
     )
-
     params = SlabBuilderParameters(min_vacuum_size=1, reorient_lattice=True, symmetrize=True, make_primitive=True)
 
     slab = create_slab(slab_config, build_parameters=params)
 
-    # Use SI_SLAB_100 as reference, but update if needed to match the new termination
-    # assert str(terminations[0]) == "Si_P4/mmm_2", "Test expects Si_P4/mmm_2 termination."
+    assert_two_entities_deep_almost_equal(slab, SI_SLAB_100)
+
+
+def test_build_slab_from_parameters():
+    MILLER_INDICES = [0, 0, 1]
+    USE_CONVENTIONAL_CELL = True
+
+    slab_config = SlabConfiguration.from_parameters(
+        bulk=material,
+        miller_indices=MILLER_INDICES,
+        number_of_layers=2,
+        vacuum=5.0,
+        use_conventional_cell=USE_CONVENTIONAL_CELL,
+    )
+    slab = create_slab(slab_config)
     assert_two_entities_deep_almost_equal(slab, SI_SLAB_100)
 
 
