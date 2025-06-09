fa533d065aa8e1008a2f43a64c4be0d51f79a856
VsevolodX <vsevolodbiryukov@icloud.com>
2025-06-08 21:17:32 -0700

    update: adjsut tests

--- tests/py/unit/test_tools_build_slab.py
+++ tests/py/unit/test_tools_build_slab.py
@@ -1,20 +1,23 @@
 from mat3ra.esse.models.core.reusable.axis_enum import AxisEnum
 
 from mat3ra.made.material import Material
-from mat3ra.made.tools.build.slab import SlabConfiguration, create_slab, SlabBuilder
+from mat3ra.made.tools.analyze.crystal_planes import CrystalLatticePlanesMaterialAnalyzer
+from mat3ra.made.tools.build.slab import (
+    SlabConfiguration,
+    create_slab,
+    SlabBuilder,
+    select_slab_termination,
+    get_slab_terminations,
+)
 from mat3ra.made.tools.build.slab.builders import (
-    ConventionalCellBuilder,
     AtomicLayersUniqueRepeatedBuilder,
     SlabBuilderParameters,
 )
 from mat3ra.made.tools.build.slab.configuration import (
     AtomicLayersUniqueRepeatedConfiguration,
     VacuumConfiguration,
-    get_terminations,
     CrystalLatticePlanesConfiguration,
-    ConventionalCellConfiguration,
 )
-from mat3ra.made.tools.build.slab.utils import select_termination
 from unit.fixtures.slab import SI_SLAB_001, SI_SLAB_001_CONFIGURATION
 from .utils import assert_two_entities_deep_almost_equal
 
@@ -28,23 +31,23 @@ material = Material.create_default()
 
 
 def test_build_slab():
-    conventional_cell_config = ConventionalCellConfiguration(crystal=material)
-    conventional_cell = (
-        ConventionalCellBuilder().get_material(conventional_cell_config) if USE_CONVENTIONAL_CELL else material
-    )
-    crystal_lattice_planes = CrystalLatticePlanesConfiguration(crystal=conventional_cell, miller_indices=MILLER_INDICES)
-    terminations = crystal_lattice_planes.terminations
-    termination = select_termination(terminations, "Si")
 
-    atomic_layers_repeated_config = AtomicLayersUniqueRepeatedConfiguration(
-        crystal=crystal_lattice_planes.crystal,
+    # Explicitly create the material from the configuration
+    crystal_lattice_planes_analyzer = CrystalLatticePlanesMaterialAnalyzer(
+        material=material, miller_indices=MILLER_INDICES
+    )
+    terminations = crystal_lattice_planes_analyzer.terminations
+    termination = select_slab_termination(terminations, "Si")
+
+    atomic_layers_repeated_configuration = AtomicLayersUniqueRepeatedConfiguration(
+        crystal=material,
         miller_indices=MILLER_INDICES,
         termination_top=termination,
         number_of_repetitions=NUMBER_OF_LAYERS,
     )
 
     atomic_layers_repeated_orthogonal_c = AtomicLayersUniqueRepeatedBuilder().get_material(
-        atomic_layers_repeated_config
+        atomic_layers_repeated_configuration
     )
 
     vacuum_configuration = VacuumConfiguration(
@@ -53,7 +56,7 @@ def test_build_slab():
 
     build_params = SlabBuilderParameters(use_orthogonal_c=True, xy_supercell_matrix=XY_SUPERCELL_MATRIX)
     slab_configuration = SlabConfiguration(
-        stack_components=[atomic_layers_repeated_config, vacuum_configuration],
+        stack_components=[atomic_layers_repeated_configuration, vacuum_configuration],
         direction=AxisEnum.z,
     )
 
@@ -66,8 +69,8 @@ def test_build_slab():
 def test_create_slab():
     crystal = material
     hkl = MILLER_INDICES
-    terminations = get_terminations(crystal=crystal, miller_indices=hkl)
-    termination = select_termination(terminations, "Si")
+    terminations = get_slab_terminations(crystal=crystal, miller_indices=hkl)
+    termination = select_slab_termination(terminations, "Si")
     slab = create_slab(
         crystal=crystal,
         miller_indices=hkl,
