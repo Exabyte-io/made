c01c14102b0dbfda3ecf5f5635a58c1b47f01959
VsevolodX <vsevolodbiryukov@icloud.com>
2025-06-13 15:53:15 -0700

    update: add strain and mirror ops

--- src/py/mat3ra/made/tools/operations/core/unary.py
+++ src/py/mat3ra/made/tools/operations/core/unary.py
@@ -1,4 +1,8 @@
+import numpy as np
 from mat3ra.code.vector import Vector3D
+from mat3ra.esse.models.core.abstract.matrix_3x3 import Matrix3x3Schema
+from mat3ra.esse.models.core.reusable.axis_enum import AxisEnum
+
 from mat3ra.made.material import Material
 from mat3ra.made.tools.modify import translate_by_vector, wrap_to_unit_cell
 
@@ -31,3 +35,42 @@ def edit_cell(material: Material, lattice_vectors=None) -> Material:
         )
     wrapped_material = wrap_to_unit_cell(material)
     return wrapped_material
+
+
+def strain(material: Material, strain_matrix: Matrix3x3Schema) -> Material:
+    # Applies a strain to the material by modifying its lattice vectors while keeping the basis in crystal coordinates.
+
+    # The strain matrix is a 3x3 matrix that defines the strain to be applied.
+    # It should be a small perturbation to the identity matrix.
+    if not isinstance(strain_matrix, Matrix3x3Schema):
+        raise ValueError("strain_matrix must be an instance of Matrix3x3Schema")
+    if strain_matrix.is_identity():
+        return material
+
+    strain_matrix_np = np.array(strain_matrix)
+    lattice_vectors_np = np.array(material.lattice.vector_arrays)
+    new_lattice_vectors_np = lattice_vectors_np @ strain_matrix_np
+    new_lattice_vectors = new_lattice_vectors_np.tolist()
+
+    new_material = material.clone()
+    new_material.basis.to_crystal()
+
+    new_material.set_new_lattice_vectors(
+        lattice_vector1=new_lattice_vectors[0],
+        lattice_vector2=new_lattice_vectors[1],
+        lattice_vector3=new_lattice_vectors[2],
+    )
+
+    return new_material
+
+
+def mirror(material: Material, direction: AxisEnum = AxisEnum.z) -> Material:
+    # Flips the material along the specified axis by applying a right-handed supercell transformation.
+    supercell_matrix = {
+        AxisEnum.x: [[-1, 0, 0], [0, 0, 1], [0, 1, 0]],
+        AxisEnum.y: [[0, 0, 1], [0, -1, 0], [1, 0, 0]],
+        AxisEnum.z: [[0, 1, 0], [1, 0, 0], [0, 0, -1]],
+    }.get(direction)
+
+    mirrored_material = supercell(material, supercell_matrix)
+    return mirrored_material
