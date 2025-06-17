5cf2cdfce3582736884ee0b96fc7756935bd57e3
VsevolodX <vsevolodbiryukov@icloud.com>
2025-06-16 19:10:24 -0700

    update: parametrize tests perturbation

--- tests/py/unit/test_tools_build_perturbation.py
+++ tests/py/unit/test_tools_build_perturbation.py
@@ -1,3 +1,4 @@
+import pytest
 from mat3ra.made.material import Material
 from mat3ra.made.tools.build.perturbation import create_perturbation
 from mat3ra.made.tools.build.perturbation.builders import SlabPerturbationBuilder
@@ -9,41 +10,71 @@ from mat3ra.utils import assertion as assertion_utils
 from .fixtures.monolayer import GRAPHENE
 
 
-def test_sine_perturbation():
-    material = Material.create(GRAPHENE)
-    slab = create_supercell(material, [[10, 0, 0], [0, 10, 0], [0, 0, 1]])
+@pytest.mark.parametrize(
+    "material_config, supercell_matrix, perturbation_function_params, use_cartesian, coords_to_check",
+    [
+        (
+            GRAPHENE,
+            [[10, 0, 0], [0, 10, 0], [0, 0, 1]],
+            {"amplitude": 0.05, "wavelength": 1},
+            False,
+            {0: [0.0, 0.0, 0.5], 42: [0.2, 0.1, 0.547552826]},
+        ),
+    ],
+)
+def test_sine_perturbation(
+    material_config, supercell_matrix, perturbation_function_params, use_cartesian, coords_to_check
+):
+    material = Material.create(material_config)
+    slab = create_supercell(material, supercell_matrix)
 
     perturbation_config = PerturbationConfiguration(
         material=slab,
-        perturbation_function=SineWavePerturbationFunctionHolder(amplitude=0.05, wavelength=1),
-        use_cartesian_coordinates=False,
+        perturbation_function=SineWavePerturbationFunctionHolder(**perturbation_function_params),
+        use_cartesian_coordinates=use_cartesian,
     )
     builder = SlabPerturbationBuilder()
     perturbed_slab = builder.get_material(perturbation_config)
     # Check selected atoms to avoid using 100+ atoms fixture
-    assertion_utils.assert_deep_almost_equal([0.0, 0.0, 0.5], perturbed_slab.basis.coordinates.values[0])
-    assertion_utils.assert_deep_almost_equal([0.2, 0.1, 0.547552826], perturbed_slab.basis.coordinates.values[42])
+    for index, expected_coord in coords_to_check.items():
+        assertion_utils.assert_deep_almost_equal(expected_coord, perturbed_slab.basis.coordinates.values[index])
 
 
-def test_distance_preserved_sine_perturbation():
-    material = Material.create(GRAPHENE)
-    slab = create_supercell(material, [[10, 0, 0], [0, 10, 0], [0, 0, 1]])
+@pytest.mark.parametrize(
+    "material_config, supercell_matrix, perturbation_function_params, use_cartesian, preserve_distance,"
+    + " coords_to_check, expected_cell",
+    [
+        (
+            GRAPHENE,
+            [[10, 0, 0], [0, 10, 0], [0, 0, 1]],
+            {"amplitude": 0.05, "wavelength": 1, "phase": 0.25, "axis": "y"},
+            False,
+            True,
+            {0: [0.0, 0.0, 0.5], 42: [0.194051947, 0.1, 0.546942315]},
+            [[24.087442, 0.0, 0.0], [-12.043583, 21.367367, 0.0], [0.0, 0.0, 20.0]],
+        ),
+    ],
+)
+def test_distance_preserved_sine_perturbation(
+    material_config,
+    supercell_matrix,
+    perturbation_function_params,
+    use_cartesian,
+    preserve_distance,
+    coords_to_check,
+    expected_cell,
+):
+    material = Material.create(material_config)
+    slab = create_supercell(material, supercell_matrix)
 
     perturbation_config = PerturbationConfiguration(
         material=slab,
-        perturbation_function=SineWavePerturbationFunctionHolder(amplitude=0.05, wavelength=1, phase=0.25, axis="y"),
-        use_cartesian_coordinates=False,
+        perturbation_function=SineWavePerturbationFunctionHolder(**perturbation_function_params),
+        use_cartesian_coordinates=use_cartesian,
     )
-    perturbed_slab = create_perturbation(configuration=perturbation_config, preserve_distance=True)
+    perturbed_slab = create_perturbation(configuration=perturbation_config, preserve_distance=preserve_distance)
     # Check selected atoms to avoid using 100+ atoms fixture
-    assertion_utils.assert_deep_almost_equal([0.0, 0.0, 0.5], perturbed_slab.basis.coordinates.values[0])
-    assertion_utils.assert_deep_almost_equal(
-        [0.194051947, 0.1, 0.546942315], perturbed_slab.basis.coordinates.values[42]
-    )
+    for index, expected_coord in coords_to_check.items():
+        assertion_utils.assert_deep_almost_equal(expected_coord, perturbed_slab.basis.coordinates.values[index])
     # Value taken from visually inspected notebook
-    expected_cell = [
-        [24.087442, 0.0, 0.0],
-        [-12.043583, 21.367367, 0.0],
-        [0.0, 0.0, 20.0],
-    ]
     assertion_utils.assert_deep_almost_equal(expected_cell, perturbed_slab.lattice.vector_arrays)
