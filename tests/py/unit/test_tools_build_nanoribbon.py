fc42d03b605ddc2fdb02cf3d4aa3597e3395eac3
VsevolodX <vsevolodbiryukov@icloud.com>
2025-06-16 18:42:33 -0700

    update: parametrize tests nanoribbon

--- tests/py/unit/test_tools_build_nanoribbon.py
+++ tests/py/unit/test_tools_build_nanoribbon.py
@@ -1,33 +1,28 @@
 from mat3ra.made.material import Material
 from mat3ra.made.tools.build.nanoribbon import NanoribbonConfiguration, create_nanoribbon
 from mat3ra.made.tools.build.nanoribbon.enums import EdgeTypes
+import pytest
 
 from .fixtures.monolayer import GRAPHENE
 from .fixtures.nanoribbon import GRAPHENE_ARMCHAIR_NANORIBBON, GRAPHENE_ZIGZAG_NANORIBBON
 from .utils import assert_two_entities_deep_almost_equal
 
 
-def test_build_zigzag_nanoribbon():
+@pytest.mark.parametrize(
+    "material_config, width, length, vacuum_width, edge_type, expected_nanoribbon",
+    [
+        (GRAPHENE, 2, 4, 3, EdgeTypes.zigzag, GRAPHENE_ZIGZAG_NANORIBBON),
+        (GRAPHENE, 2, 4, 3, EdgeTypes.armchair, GRAPHENE_ARMCHAIR_NANORIBBON),
+    ],
+)
+def test_build_nanoribbon(material_config, width, length, vacuum_width, edge_type, expected_nanoribbon):
     config = NanoribbonConfiguration(
-        material=Material.create(GRAPHENE),
-        width=2,
-        length=4,
-        vacuum_width=3,
-        edge_type=EdgeTypes.zigzag,
+        material=Material.create(material_config),
+        width=width,
+        length=length,
+        vacuum_width=vacuum_width,
+        edge_type=edge_type,
     )
 
     nanoribbon = create_nanoribbon(config)
-    assert_two_entities_deep_almost_equal(nanoribbon, GRAPHENE_ZIGZAG_NANORIBBON)
-
-
-def test_build_armchair_nanoribbon():
-    config = NanoribbonConfiguration(
-        material=Material.create(GRAPHENE),
-        width=2,
-        length=4,
-        vacuum_width=3,
-        edge_type=EdgeTypes.armchair,
-    )
-
-    nanoribbon = create_nanoribbon(config)
-    assert_two_entities_deep_almost_equal(nanoribbon, GRAPHENE_ARMCHAIR_NANORIBBON)
+    assert_two_entities_deep_almost_equal(nanoribbon, expected_nanoribbon)
