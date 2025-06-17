9e872c4e5c586ca8081974ae5dfb955191ad0602
VsevolodX <vsevolodbiryukov@icloud.com>
2025-06-17 10:43:38 -0700

    chore: parametrize flipping supercell

--- tests/py/unit/test_tools_build_supercell.py
+++ tests/py/unit/test_tools_build_supercell.py
@@ -3,7 +3,7 @@ from mat3ra.made.material import Material
 from mat3ra.made.tools.build.supercell import create_supercell
 from unit.fixtures.supercell import SI_SUPERCELL_2X2X1, SI_SUPERCELL_FLIPPED_Z
 
-from .fixtures.bulk import BULK_Si_PRIMITIVE
+from .fixtures.bulk import BULK_Si_CONVENTIONAL, BULK_Si_PRIMITIVE
 from .utils import assert_two_entities_deep_almost_equal
 
 
@@ -14,18 +14,15 @@ from .utils import assert_two_entities_deep_almost_equal
             BULK_Si_PRIMITIVE,
             [[2, 0, 0], [0, 2, 0], [0, 0, 1]],
             SI_SUPERCELL_2X2X1,
-        )
+        ),
+        (
+            BULK_Si_CONVENTIONAL,
+            [[0, 1, 0], [1, 0, 0], [0, 0, -1]],
+            SI_SUPERCELL_FLIPPED_Z,
+        ),
     ],
 )
 def test_create_supercell(material_config, supercell_matrix, expected_material_config):
     material = Material.create(material_config)
     supercell_material = create_supercell(material, supercell_matrix)
     assert_two_entities_deep_almost_equal(supercell_material, expected_material_config)
-
-
-def test_create_supercell_mirror():
-    material = Material.create_default()
-    supercell_matrix = [[0, 1, 0], [1, 0, 0], [0, 0, -1]]
-    z_flipped_material = create_supercell(material, supercell_matrix)
-
-    assert_two_entities_deep_almost_equal(z_flipped_material, SI_SUPERCELL_FLIPPED_Z)
