9eb763b71611d6e58174d852743d59df1dee51bf
VsevolodX <vsevolodbiryukov@icloud.com>
2025-06-17 20:31:18 -0700

    update: adjust test for strain

--- tests/py/unit/test_operations.py
+++ tests/py/unit/test_operations.py
@@ -1,24 +1,22 @@
-import numpy as np
+import pytest
 from mat3ra.esse.models.core.abstract.matrix_3x3 import Matrix3x3Schema
 from mat3ra.made.material import Material
 from mat3ra.made.tools.operations.core.unary import strain
-from mat3ra.utils import assertion as assertion_utils
 from unit.fixtures.bulk import BULK_Si_CONVENTIONAL
+from unit.fixtures.strain import BULK_Si_CONVENTIONAL_STRAINED
+from unit.utils import assert_two_entities_deep_almost_equal
+
+STRAIN_TEST_CASES = [
+    (BULK_Si_CONVENTIONAL, [[1.1, 0, 0], [0, 1.1, 0], [0, 0, 1.0]], BULK_Si_CONVENTIONAL_STRAINED),
+]
 
 
-def test_strain():
-    """
-    Tests that the strain operation correctly modifies the lattice vectors of a material.
-    """
-    material = Material.create(BULK_Si_CONVENTIONAL)
-    strain_matrix_data = [[1.01, 0, 0], [0, 1, 0], [0, 0, 1]]
-    strain_matrix = Matrix3x3Schema(root=strain_matrix_data)
+@pytest.mark.parametrize("material_config, strain_matrix, expected_material_config", STRAIN_TEST_CASES)
+def test_strain(material_config, strain_matrix, expected_material_config):
+    material = Material.create(material_config)
+    strain_matrix_obj = Matrix3x3Schema(root=strain_matrix)
+    expected_material = Material.create(expected_material_config)
 
-    strained_material = strain(material, strain_matrix)
+    strained_material = strain(material, strain_matrix_obj)
 
-    original_lattice_vectors = np.array(material.lattice.vector_arrays)
-    expected_strained_lattice_vectors = np.dot(original_lattice_vectors, np.array(strain_matrix_data))
-
-    assertion_utils.assert_deep_almost_equal(
-        strained_material.lattice.vector_arrays, expected_strained_lattice_vectors.tolist()
-    )
+    assert_two_entities_deep_almost_equal(strained_material, expected_material)
