6ea8f8af01b15db27d23cbf4e802a730f68c8091
VsevolodX <vsevolodbiryukov@icloud.com>
2025-07-09 19:54:30 -0700

    update: adjust slab analyzer

--- src/py/mat3ra/made/tools/analyze/slab.py
+++ src/py/mat3ra/made/tools/analyze/slab.py
@@ -1,17 +1,15 @@
-from typing import Union
-
 from mat3ra.code.entity import InMemoryEntityPydantic
 
 from ..analyze import BaseMaterialAnalyzer
 from ..build.metadata import MaterialMetadata
-from ..build.slab.builders import SlabWithAdditionalLayersBuilder
-from ..build.slab.configurations import SlabConfiguration, SlabWithAdditionalLayersConfiguration
+from ..build.slab.configurations import SlabConfiguration
+from ..build.vacuum.configuration import VacuumConfiguration
 
 
 class SlabMaterialAnalyzer(BaseMaterialAnalyzer):
     def get_slab_configuration(self) -> SlabConfiguration:
         metadata = MaterialMetadata(**self.material.metadata)
-        slab_build_configuration_dict = metadata.build.configuration
+        slab_build_configuration_dict = metadata.build[-1].configuration
         if slab_build_configuration_dict.get("type") != "SlabConfiguration":
             raise ValueError("Material is not a slab.")
 
@@ -31,3 +29,24 @@ class SlabMaterialAnalyzer(BaseMaterialAnalyzer):
     def vacuum_thickness_in_layers(self) -> float:
         slab_configuration = self.get_slab_configuration()
         return self.vacuum_ratio / (1 - self.vacuum_ratio) * self.number_of_layers
+
+    def get_slab_configuration_with_no_vacuum(self) -> SlabConfiguration:
+        """
+        Get the slab material without vacuum.
+        Returns:
+            InMemoryEntityPydantic: The slab material without vacuum.
+        """
+        slab_configuration = self.get_slab_configuration()
+        slab_configuration_with_no_vacuum = slab_configuration.clone()
+        slab_configuration_with_no_vacuum.set_vacuum(0.0)
+
+        return slab_configuration_with_no_vacuum
+
+    def get_slab_vacuum_configuration(self) -> VacuumConfiguration:
+        """
+        Get the vacuum configuration from the slab.
+        Returns:
+            VacuumConfiguration: The vacuum configuration.
+        """
+        slab_configuration = self.get_slab_configuration()
+        return slab_configuration.vacuum_configuration
