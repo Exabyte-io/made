f64bd2379f33424d1f0c52ac5b6beff7f4f74323
VsevolodX <vsevolodbiryukov@icloud.com>
2025-07-09 20:14:07 -0700

    update: fix an error

--- src/py/mat3ra/made/tools/operations/core/unary.py
+++ src/py/mat3ra/made/tools/operations/core/unary.py
@@ -1,9 +1,9 @@
 import numpy as np
 from mat3ra.code.vector import Vector3D
 from mat3ra.esse.models.core.abstract.matrix_3x3 import Matrix3x3Schema
+
 from mat3ra.made.material import Material
 from mat3ra.made.tools.modify import translate_by_vector, wrap_to_unit_cell
-
 from ...convert import from_ase, to_ase
 from ...third_party import ase_make_supercell
 from ...utils import decorator_convert_supercell_matrix_2x2_to_3x3
