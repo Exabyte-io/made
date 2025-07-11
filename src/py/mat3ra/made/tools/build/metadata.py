e31ac744c13f8d4f392edaccef7e69ab808c913e
VsevolodX <vsevolodbiryukov@icloud.com>
2025-07-11 12:40:57 -0700

    update: supercell adds metadata

--- src/py/mat3ra/made/tools/build/metadata.py
+++ src/py/mat3ra/made/tools/build/metadata.py
@@ -2,12 +2,24 @@ from typing import List, Optional
 
 from pydantic import Field
 
+from mat3ra.made.material import Material
 from mat3ra.made.metadata import BaseMetadata
 
 
 class BuildMetadata(BaseMetadata):
     configuration: dict = Field(default_factory=dict)
     build_parameters: dict = Field(default_factory=dict)
+
+    @staticmethod
+    def set_build_metadata(
+        material: Material,
+        configuration: dict,
+        build_parameters: Optional[dict] = None,
+    ) -> None:
+        metadata = MaterialMetadata(**material.metadata or {})
+        build_metadata = BuildMetadata(configuration=configuration, build_parameters=build_parameters or {})
+        metadata.build.append(build_metadata)
+        material.metadata = metadata.to_dict()
 
 
 class MaterialMetadata(BaseMetadata):
--- src/py/mat3ra/made/tools/build/supercell.py
+++ src/py/mat3ra/made/tools/build/supercell.py
@@ -1,9 +1,18 @@
 from typing import List, Optional
 
 import numpy as np
+from mat3ra.esse.models.materials_category_components.entities.auxiliary.three_dimensional.supercell_matrix_3d import (
+    SupercellMatrix3DSchema,
+)
 
 from mat3ra.made.material import Material
+from . import BuildMetadata, BaseConfigurationPydantic
 from ..operations.core.unary import supercell
+
+
+class SupercellConfiguration(BaseConfigurationPydantic):
+    type: str = "SupercellConfiguration"
+    supercell_matrix: SupercellMatrix3DSchema
 
 
 def create_supercell(
@@ -23,4 +32,8 @@ def create_supercell(
     if scaling_factor is not None:
         supercell_matrix = np.multiply(scaling_factor, np.eye(3)).tolist()
     new_material = supercell(material, supercell_matrix)
+    BuildMetadata.set_build_metadata(
+        new_material,
+        configuration=SupercellConfiguration(supercell_matrix=supercell_matrix),
+    )
     return new_material
