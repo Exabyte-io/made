77afcf96b899d34fa7be4bb8d33b21803e83b450
VsevolodX <vsevolodbiryukov@icloud.com>
2025-07-15 11:53:14 -0700

    update: remove duplication

--- src/py/mat3ra/made/tools/build/defect/terrace/analyzer.py
+++ src/py/mat3ra/made/tools/build/defect/terrace/analyzer.py
@@ -1,88 +1,57 @@
 from typing import List
-
 import numpy as np
 
 from mat3ra.made.tools.analyze.slab import SlabMaterialAnalyzer
-from mat3ra.made.tools.build import MaterialWithBuildMetadata
-from mat3ra.made.utils import get_atomic_coordinates_extremum
 
 
 class TerraceMaterialAnalyzer(SlabMaterialAnalyzer):
-    def calculate_cut_direction_vector(self, cut_direction: List[int]):
-        np_cut_direction = np.array(cut_direction)
-        direction_vector = np.dot(np.array(self.material.basis.cell.vector_arrays), np_cut_direction)
-        normalized_direction_vector = direction_vector / np.linalg.norm(direction_vector)
-        return normalized_direction_vector
+    cut_direction: List[int] = [0, 0, 1]
+    number_of_added_layers: float = 1.0
 
-    def get_rotation_axis(self, cut_direction: List[int]) -> List[int]:
-        normalized_rotation_axis = np.cross(cut_direction, [0, 0, 1])
-        norm = np.linalg.norm(normalized_rotation_axis)
-        normalized = normalized_rotation_axis / norm
-        return np.round(normalized).astype(int).tolist()
+    @property
+    def _lattice_matrix(self) -> np.ndarray:
+        return np.array(self.material.lattice.vector_arrays)
 
-    def get_angle(self, cut_direction, number_of_added_layers: float) -> float:
-        """
-        Calculate the angle of rotation based on the number of added layers.
+    @property
+    def cut_direction_vector(self) -> np.ndarray:
+        direction = np.dot(self._lattice_matrix, np.array(self.cut_direction))
+        return direction / np.linalg.norm(direction)
 
-        Args:
-            cut_direction: The direction of the cut in Miller indices.
-            number_of_added_layers: The number of layers added to the slab.
+    @property
+    def _height_cartesian(self) -> float:
+        return self.layer_thickness * self.number_of_added_layers
 
-        Returns:
-            The angle in degrees.
-        """
-        height_cartesian = self.layer_thickness * number_of_added_layers
-        cut_direction_xy_proj_cart = np.linalg.norm(
-            np.dot(np.array(self.material.lattice.vector_arrays), self.calculate_cut_direction_vector(cut_direction))
+    @property
+    def _cut_direction_xy_proj_cart(self) -> float:
+        return np.linalg.norm(np.dot(self._lattice_matrix, self.cut_direction_vector))
+
+    @property
+    def rotation_axis(self) -> List[int]:
+        axis = np.cross(np.array(self.cut_direction), [0, 0, 1])
+        return np.round(axis / np.linalg.norm(axis)).astype(int).tolist()
+
+    @property
+    def angle(self) -> float:
+        return np.degrees(np.arctan(self._height_cartesian / self._cut_direction_xy_proj_cart))
+
+    @property
+    def _length_increase(self) -> float:
+        return (
+            np.linalg.norm([self._height_cartesian, self._cut_direction_xy_proj_cart])
+            - self._cut_direction_xy_proj_cart
         )
-        angle = np.arctan(height_cartesian / cut_direction_xy_proj_cart) * 180 / np.pi
-        return angle
 
-    def get_length_increase(self, cut_direction: List[int], number_of_added_layers: float) -> float:
-        """
-        Calculate the increase in length based on the number of added layers.
+    @property
+    def new_lattice_vectors(self) -> np.ndarray:
+        dir_vec = self.cut_direction_vector
+        length_inc = self._length_increase
 
-        Args:
-            number_of_added_layers: The number of layers added to the slab.
+        vector_a = self.material.lattice.vectors.a
+        vector_b = self.material.lattice.vectors.b
 
-        Returns:
-            The increase in length.
-        """
-        height_cartesian = self.layer_thickness * number_of_added_layers
-        cut_direction_xy_proj_cart = np.linalg.norm(
-            np.dot(np.array(self.material.lattice.vector_arrays), self.calculate_cut_direction_vector(cut_direction))
-        )
-        hypotenuse = np.linalg.norm([height_cartesian, cut_direction_xy_proj_cart])
-        delta_length = hypotenuse - cut_direction_xy_proj_cart
-        return delta_length
-
-    def get_new_lattice_vectors(self, cut_direction, number_of_added_layers: float) -> List[List[float]]:
-        """
-        Increase the lattice size in a specific direction.
-
-        When the material is rotated to maintain periodic boundary conditions (PBC),
-        the periodicity in the X and Y directions changes.
-        Therefore, the lattice size must be increased to fit the new structure dimensions.
-
-        If the terrace plane is normal to the Z direction, it becomes larger than the previous XY plane of the material
-        because it forms a hypotenuse between PBC points.
-        This method adjusts the lattice vectors to accommodate this change.
-
-        Args:
-            cut_direction: The direction of the cut in Miller indices.
-            number_of_added_layers: The number of layers added to the slab.
-
-        Returns:
-            The material with the increased lattice size.
-        """
-
-        direction_of_increase = self.calculate_cut_direction_vector(cut_direction)
-        length_increase = self.get_length_increase(cut_direction, number_of_added_layers)
-        vector_a, vector_b = self.material.lattice.vectors.a, self.material.lattice.vectors.b
         norm_a, norm_b = vector_a.norm, vector_b.norm
-
-        delta_a_cart = np.dot(vector_a.value, np.array(direction_of_increase)) * length_increase / norm_a
-        delta_b_cart = np.dot(vector_b.value, np.array(direction_of_increase)) * length_increase / norm_b
+        delta_a_cart = np.dot(vector_a.value, dir_vec) * length_inc / norm_a
+        delta_b_cart = np.dot(vector_b.value, dir_vec) * length_inc / norm_b
 
         scaling_matrix = np.eye(3)
         scaling_matrix[0, 0] += delta_a_cart / norm_a
@@ -91,18 +60,3 @@ class TerraceMaterialAnalyzer(SlabMaterialAnalyzer):
         new_material = self.material.clone()
         new_lattice = new_material.lattice.get_scaled_by_matrix(scaling_matrix.tolist())
         return new_lattice.vector_arrays
-
-    def calculate_height_cartesian(self, new_material: MaterialWithBuildMetadata):
-        """
-        Calculate the height of the added layers in Cartesian coordinates.
-
-        Args:
-            new_material: The material with the added layers.
-
-        Returns:
-            The height of the added layers in Cartesian coordinates.
-        """
-        original_max_z = get_atomic_coordinates_extremum(self.material, use_cartesian_coordinates=True)
-        added_layers_max_z = get_atomic_coordinates_extremum(new_material, use_cartesian_coordinates=True)
-        height_cartesian = added_layers_max_z - original_max_z
-        return height_cartesian
