d32d017481240e01e6f821c8ed0843fc1abc9951
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2025-07-15 20:47:05 +0300

    chore: update code

--- dist/js/material.d.ts
+++ dist/js/material.d.ts
@@ -1,13 +1,8 @@
+import { HasConsistencyChecksHasMetadataNamedDefaultableInMemoryEntity } from "@mat3ra/code/dist/js/entity";
 import type { MaterialSchema } from "@mat3ra/esse/dist/js/types";
 import { type MaterialMixinConstructor, defaultMaterialConfig } from "./materialMixin";
 export { defaultMaterialConfig };
-declare const BaseInMemoryEntity: typeof import("@mat3ra/code/dist/js/entity").InMemoryEntity & import("@mat3ra/code/dist/js/entity/mixins/DefaultableMixin").DefaultableInMemoryEntityConstructor & {
-    createDefault<T extends import("@mat3ra/code/dist/js/utils/types").Constructor<import("@mat3ra/code/dist/js/entity").InMemoryEntity> & {
-        defaultConfig?: object | null | undefined;
-    }>(this: T): InstanceType<T> & {
-        isDefault: boolean;
-    };
-} & import("@mat3ra/code/dist/js/entity/mixins/NamedEntityMixin").NamedInMemoryEntityConstructor & import("@mat3ra/code/dist/js/entity/mixins/HasMetadataMixin").HasMetadataInMemoryEntityConstructor & import("@mat3ra/code/dist/js/entity/mixins/HasConsistencyChecksMixin").HasConsistencyChecksInMemoryEntityConstructor;
+declare const BaseInMemoryEntity: typeof HasConsistencyChecksHasMetadataNamedDefaultableInMemoryEntity;
 type BaseMaterial = MaterialMixinConstructor & typeof BaseInMemoryEntity;
 declare const Material_base: BaseMaterial;
 export declare class Material extends Material_base {
