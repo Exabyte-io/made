ad1b81fc51474e767f5b438d4e3fea93b1e79397
VsevolodX <vsevolodbiryukov@icloud.com>
2025-07-16 17:39:12 -0700

    chore: update to use pydantic

--- src/py/mat3ra/made/tools/utils/functions.py
+++ src/py/mat3ra/made/tools/utils/functions.py
@@ -1,9 +1,9 @@
 from typing import List
 
-from pydantic import BaseModel
+from mat3ra.code.entity import InMemoryEntityPydantic
 
 
-class FunctionHolder(BaseModel):
+class FunctionHolder(InMemoryEntityPydantic):
     def apply_function(self, coordinate: List[float]) -> float:
         """
         Get the value of the function at the given coordinate.
@@ -19,11 +19,5 @@ class FunctionHolder(BaseModel):
     def calculate_arc_length(self, a: float, b: float) -> float:
         """
         Get the arc length of the function between a and b.
-        """
-        raise NotImplementedError
-
-    def get_json(self) -> dict:
-        """
-        Get the json representation of the function holder.
         """
         raise NotImplementedError
