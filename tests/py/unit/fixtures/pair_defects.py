6fc9f5501af9d06a6ea4772d6c4d20c55b7a1dfe
VsevolodX <vsevolodbiryukov@icloud.com>
2025-07-22 10:15:29 -0700

    update: adapt more fixtures due to reset id

--- tests/py/unit/fixtures/pair_defects.py
+++ tests/py/unit/fixtures/pair_defects.py
@@ -11,8 +11,8 @@ PAIR_DEFECT_VACANCY_INTERSTITIAL_BULK_PRIMITIVE_Si = {
         "type": "FCC",
     },
     "basis": {
-        "elements": [{"id": 0, "value": "Si"}, {"id": 2, "value": "Ge"}],
-        "coordinates": [{"id": 0, "value": [0.25, 0.25, 0.25]}, {"id": 2, "value": [0.5, 0.5, 0.5]}],
+        "elements": [{"id": 0, "value": "Si"}, {"id": 1, "value": "Ge"}],
+        "coordinates": [{"id": 0, "value": [0.25, 0.25, 0.25]}, {"id": 1, "value": [0.5, 0.5, 0.5]}],
         "units": "crystal",
         "labels": [],
         "constraints": [],
@@ -32,8 +32,8 @@ PAIR_DEFECT_SUBSTITUTION_VACANCY_BULK_PRIMITIVE_Si = {
         "type": "FCC",
     },
     "basis": {
-        "elements": [{"id": 1, "value": "Ge"}],
-        "coordinates": [{"id": 1, "value": [0.0, 0.0, 0.0]}],
+        "elements": [{"id": 0, "value": "Ge"}],
+        "coordinates": [{"id": 0, "value": [0.0, 0.0, 0.0]}],
         "units": "crystal",
         "labels": [],
         "constraints": [],
