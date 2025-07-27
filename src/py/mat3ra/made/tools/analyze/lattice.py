427ec40e565c30f7a3a8a22edc03f1a2a00b1203
VsevolodX <vsevolodbiryukov@icloud.com>
2025-07-27 15:25:41 -0700

    update: cleanups

--- src/py/mat3ra/made/tools/analyze/lattice.py
+++ src/py/mat3ra/made/tools/analyze/lattice.py
@@ -34,6 +34,16 @@ def get_material_with_conventional_lattice(material: MaterialWithBuildMetadata) 
     return analyzer.material_with_conventional_lattice
 
 
-def get_material_with_primitive_lattice(material: MaterialWithBuildMetadata) -> MaterialWithBuildMetadata:
+def get_material_with_primitive_lattice(
+    material: MaterialWithBuildMetadata, return_original_if_not_reduced=False
+) -> MaterialWithBuildMetadata:
     analyzer = LatticeMaterialAnalyzer(material=material)
-    return analyzer.material_with_primitive_lattice
+    material_with_primitive_lattice = analyzer.material_with_primitive_lattice
+    original_number_of_atoms = material.basis.number_of_atoms
+    primitive_structure_number_of_atoms = material_with_primitive_lattice.basis.number_of_atoms
+    if original_number_of_atoms == primitive_structure_number_of_atoms:
+        # Not reduced, return original material if requested, to avoid unnecessary editions
+        if return_original_if_not_reduced:
+            return material
+    # Reduced, return the primitive structure
+    return material_with_primitive_lattice
