1567b37f6309b2f7dd403301a25d32edb4360230
VsevolodX <vsevolodbiryukov@icloud.com>
2025-07-28 16:10:19 -0700

    update: add name for adatom builder

--- src/py/mat3ra/made/tools/build/defect/adatom/builders.py
+++ src/py/mat3ra/made/tools/build/defect/adatom/builders.py
@@ -1,6 +1,11 @@
+from mat3ra.made.tools.analyze import BaseMaterialAnalyzer
 from .configuration import AdatomDefectConfiguration
 from ..slab.builders import SlabStackBuilder
 
 
 class AdatomDefectBuilder(SlabStackBuilder):
     _ConfigurationType = AdatomDefectConfiguration
+
+    def get_name_suffix(self, configuration: AdatomDefectConfiguration) -> str:
+        formula = BaseMaterialAnalyzer(material=configuration.added_component).formula
+        return f"{formula} Adatom"
