5bb34755a098836b86edbe015791a5f0721da68f
VsevolodX <vsevolodbiryukov@icloud.com>
2025-07-27 20:22:13 -0700

    update: add test fixture for multiple adatoms

--- tests/py/unit/test_tools_build_defect/test_adatom.py
+++ tests/py/unit/test_tools_build_defect/test_adatom.py
@@ -1,16 +1,22 @@
 import pytest
+from mat3ra.utils import assertion as assertion_utils
+
+from mat3ra.made.material import Material
 from mat3ra.made.tools.build import MaterialWithBuildMetadata
 from mat3ra.made.tools.build.defect.adatom.helpers import create_adatom_defect, create_multiple_adatom_defects
 from mat3ra.made.tools.build.defect.enums import AdatomPlacementMethodEnum
-from mat3ra.utils import assertion as assertion_utils
+from mat3ra.made.tools.build.slab.helpers import create_slab
+from unit.fixtures.adatom import SLAB_Si_3_ADATOMS
+from unit.fixtures.bulk import BULK_Si_CONVENTIONAL
 from unit.fixtures.slab import SI_CONVENTIONAL_SLAB_001
+from unit.utils import assert_two_entities_deep_almost_equal
 
 
 @pytest.mark.parametrize(
     "crystal_config, position_on_surface, distance_z, chemical_element, adatom_placement_method, expected_last_coord",
     [
         (
-            SI_CONVENTIONAL_SLAB_001,
+            BULK_Si_CONVENTIONAL,
             [0.5, 0.5],
             2.0,
             "Si",
@@ -18,7 +24,7 @@ from unit.fixtures.slab import SI_CONVENTIONAL_SLAB_001
             [0.5, 0.5, 0.5748],
         ),
         (
-            SI_CONVENTIONAL_SLAB_001,
+            BULK_Si_CONVENTIONAL,
             [0.55, 0.51],
             2.0,
             "Si",
@@ -35,14 +41,18 @@ def test_create_adatom(
     adatom_placement_method,
     expected_last_coord,
 ):
-    slab = MaterialWithBuildMetadata.create(crystal_config)
+    slab = create_slab(
+        crystal=Material.create(crystal_config),
+        number_of_layers=2,
+        xy_supercell_matrix=[[2, 0], [0, 2]],
+    )
     defect = create_adatom_defect(slab, position_on_surface, distance_z, adatom_placement_method, chemical_element)
 
     assertion_utils.assert_deep_almost_equal(expected_last_coord, defect.basis.coordinates.values[-1], atol=1e-4)
 
 
 @pytest.mark.parametrize(
-    "crystal_config, adatom_dicts, placement_method, expected_num_atoms",
+    "crystal_config, adatom_dicts, placement_method, expected_material_config",
     [
         (
             SI_CONVENTIONAL_SLAB_001,
@@ -52,17 +62,13 @@ def test_create_adatom(
                 {"element": "N", "coordinate": [0.75, 0.75], "distance_z": 1.0},
             ],
             AdatomPlacementMethodEnum.EXACT_COORDINATE.value,
-            19,  # 16 atoms in slab + 3 adatoms
+            SLAB_Si_3_ADATOMS,
         ),
     ],
 )
-def test_create_multiple_adatom_defects(crystal_config, adatom_dicts, placement_method, expected_num_atoms):
+def test_create_multiple_adatom_defects(crystal_config, adatom_dicts, placement_method, expected_material_config):
+
     slab = MaterialWithBuildMetadata.create(crystal_config)
     defects = create_multiple_adatom_defects(slab, adatom_dicts, placement_method)
-
-    assert len(defects.basis.elements.values) == expected_num_atoms
-
-    # Check that all expected elements are present
-    expected_elements = ["Si"] * 16 + ["Si", "C", "N"]  # Original slab + added adatoms
-    actual_elements = defects.basis.elements.values
-    assert sorted(actual_elements) == sorted(expected_elements)
+    defects.metadata.build = []
+    assert_two_entities_deep_almost_equal(defects, expected_material_config)
