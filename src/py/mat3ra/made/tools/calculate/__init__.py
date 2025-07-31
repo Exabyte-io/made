ec647bcd5d7f886e27c22c462e4d96afcf74a5b3
VsevolodX <vsevolodbiryukov@icloud.com>
2025-07-31 11:55:13 -0700

    update: get metadata for calculate

--- src/py/mat3ra/made/tools/calculate/__init__.py
+++ src/py/mat3ra/made/tools/calculate/__init__.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from .interaction_functions import sum_of_inverse_distances_squared
+from ...material import Material
 from ..analyze.other import get_surface_area
 from ..build import MaterialWithBuildMetadata
 from ..build.interface.configuration import InterfaceConfiguration
@@ -8,7 +8,7 @@ from ..build.interface.utils import get_slab
 from ..build.metadata import MaterialBuildMetadata
 from ..convert import decorator_convert_material_args_kwargs_to_atoms
 from ..third_party import ASEAtoms, ASECalculator, ASECalculatorEMT
-from ...material import Material
+from .interaction_functions import sum_of_inverse_distances_squared
 
 
 @decorator_convert_material_args_kwargs_to_atoms
