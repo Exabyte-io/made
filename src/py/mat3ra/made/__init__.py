32815de8c1ba62f9fbc7881b89d3f0cab168b4bf
VsevolodX <vsevolodbiryukov@icloud.com>
2025-08-02 20:06:08 -0700

    try: remove warnings on running pacakge spglib

--- src/py/mat3ra/made/__init__.py
+++ src/py/mat3ra/made/__init__.py
@@ -4,4 +4,5 @@ from pydantic.warnings import PydanticDeprecatedSince20
 warnings.filterwarnings("ignore", category=DeprecationWarning, module="typing_extensions")
 warnings.filterwarnings("ignore", category=UserWarning, module="pydantic")
 warnings.filterwarnings("ignore", category=RuntimeWarning, module="mat3ra.made")
+warnings.filterwarnings("ignore", module="spglib")
 warnings.filterwarnings("ignore", category=PydanticDeprecatedSince20)
