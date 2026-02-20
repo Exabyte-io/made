7ccc83197f8223e152ea76c2010032671407e818
VsevolodX <vsevolodbiryukov@icloud.com>
2026-02-20 13:46:24 -0800

    update: compare py and js hash

--- tests/js/material.test.ts
+++ tests/js/material.test.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 
 import { Material } from "../../src/js/material";
-import { Na4Cl4, Silicon } from "./fixtures";
+import { Graphene, Na4Cl4, Silicon } from "./fixtures";
 
 const newBasisXYZ = `Si     0.000000    0.000000    0.000000
 Ge     0.250000    0.250000    0.250000
@@ -18,5 +18,18 @@ describe("Material", () => {
         const clonedMaterial = material.clone();
         clonedMaterial.setBasis(newBasisXYZ, "xyz", clonedMaterial.Basis.units);
         expect(clonedMaterial.Basis.elements).to.have.lengthOf(2);
+    });
+
+    describe("calculateHash", () => {
+        [
+            { name: "Silicon", fixture: Silicon },
+            { name: "Graphene", fixture: Graphene },
+        ].forEach(({ name, fixture }) => {
+            it(`should match expected hash for ${name}`, () => {
+                const material = new Material(fixture);
+                expect(material.calculateHash()).to.equal((fixture as any).hash);
+                expect(material.scaledHash).to.equal((fixture as any).scaledHash);
+            });
+        });
     });
 });
